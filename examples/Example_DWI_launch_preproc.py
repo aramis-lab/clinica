a28fb806bb1b4f6c50d70d76348f187ab529c1be
michael.bacci <michael.bacci@inria.fr>
2016-03-24 16:13:48 +0100

    adding check: fsl > 5.0.5

--- examples/Example_DWI_launch_preproc.py
+++ examples/Example_DWI_launch_preproc.py
@@ -4,9 +4,20 @@ Created on Tue Mar 22 15:56:18 2016
 
 @author: jacquemont
 """
+from __future__ import absolute_import
 from clinica.pipeline.preproc.DWI_launch_preproc import launch
 import os
 from os.path import realpath,split,join
+import tempfile
+
+import nipype.interfaces.fsl as fsl
+
+try:
+    if fsl.Info.version() > ['5','0','5']:
+        raise
+except:
+    print("FSL version must be great then 5.0.5")
+    exit(0)
 
 data_path = join(split(realpath(__file__))[0], 'data/DWI_launch_preproc')
 
@@ -15,7 +26,12 @@ T1 = join(data_path, 'T1.nii')
 b_values = join(data_path, 'b_values.txt')
 b_vectors = join(data_path, 'b_vectors.txt')
 
-working_direct = os.getcwd()
-datasink_direct = os.getcwd()
+working_direct = tempfile.mkdtemp()
+datasink_direct = tempfile.mkdtemp()
 
+print("Working Directory -> %s" % working_direct)
+print("Datasink Directory -> %s" % datasink_direct)
+
+print("Running...")
 launch(DWI, T1, b_values, b_vectors, working_direct, datasink_direct)
+
