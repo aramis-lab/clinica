22b13838f8b5b232d23eadfa9b3a7063c66d8681
Thomas Jacquemont <thomas.jacquemont@ens.fr>
2016-06-14 18:04:48 +0200

    Add exempl efor the connectome construction. WARNING: the script is not working cause of lake of example data
    Former-commit-id: 3328352dedcdb1f4d3f9cc4061ce99cc97b54781

---
+++ examples/example_connectome_construction.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 15:56:18 2016
+
+@author: jacquemont
+"""
+from __future__ import absolute_import
+from clinica.pipeline.postprocessing.connectome_construction.py import Connectome_construction_pipeline
+import os
+from os.path import realpath,split,join
+import tempfile
+
+data_path = join(split(realpath(__file__))[0], 'data/DWI_postproc')
+
+in_parcellation = join('path to parc')
+configuration_file = join(data_path, 'config_file.nii')
+atlas_scalar_image = join(data_path, 'WM_atlas_FA.nii')
+lut_type = 'lut_type'
+lut_path = join('path to LUT')
+in_tracks = join(data_path, 'Tracks.tck')
+connectome_metric = 'count'
+zeros_diagonal=True
+
+working_direct = tempfile.mkdtemp()
+datasink_direct = tempfile.mkdtemp()
+
+print("Working Directory -> %s" % working_direct)
+print("Datasink Directory -> %s" % datasink_direct)
+
+print("Running connectome construction")
+connectome =  Connectome_construction_pipeline(in_parcellation, configuration_file, lut_type, lut_path, in_tracks, connectome_metric, working_directory, datasink_directory, in_scalar_image='', zeros_diagonal=True)
+connectome.run()
+print("connectome construction done")
+
+print("Working Directory -> %s" % working_direct)
+print("Datasink Directory -> %s" % datasink_direct)
+
