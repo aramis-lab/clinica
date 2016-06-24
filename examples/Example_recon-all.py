004414277ac755a4a83d4317f6377d091d163010
michael.bacci <michael.bacci@inria.fr>
2016-06-24 17:21:30 +0200

    for Hao, please fix ;)
    Former-commit-id: e1f9fb19c90cbf5b4aaa6052740970553e667301

--- examples/Example_recon-all.py
+++ examples/Example_recon-all.py
@@ -9,10 +9,10 @@ Created on Mon Apr 25 15:04:17 2016
 from __future__ import absolute_import
 from clinica.pipeline.preprocessing.t1_freesurfer_workflow import recon_all_pipeline
 import os.path
-import tmpfile
+import tempfile
 
 data_dir = os.path.abspath('data/Recon-all')
-output_dir = tmpfile.mkdtemp()
+output_dir = tempfile.mkdtemp()
 
 print("Data Directory -> %s" % data_dir)
 print("Output Directory -> %s" % output_dir)
