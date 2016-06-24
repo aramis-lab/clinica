b288d42a3732bef9c7610ac5ee05a8bcb3e30fde
guest <anbai106@hotmail.com>
2016-06-24 20:10:22 +0200

    reconall pipeline
    Former-commit-id: 14279a7ecb4d052e0050ac2035b9453e8a052ac7

--- clinica/pipeline/preprocessing/t1_freesurfer_workflow.py
+++ clinica/pipeline/preprocessing/t1_freesurfer_workflow.py
@@ -1,7 +1,7 @@
 from clinica.engine.cworkflow import *
 
 @Visualize("freeview", "-v ${subject_id}/mri/T1.mgz -f ${subject_id}/surf/lh.white:edgecolor=blue ${subject_id}/surf/lh.pial:edgecolor=green ${subject_id}/surf/rh.white:edgecolor=blue ${subject_id}/surf/rh.pial:edgecolor=green", "subject_id")
-def recon_all_pipeline(data_dir, output_dir, n_output, recon_all_args='-qcache'):
+def recon_all_pipeline(data_dir, output_dir, n_output, field_template, template_args, recon_all_args='-qcache'):
     """
         Creates a pipeline that performs Freesurfer commander, recon-all,
         It takes the input files of MRI T1 images and executes the 31 steps to
@@ -28,7 +28,9 @@ def recon_all_pipeline(data_dir, output_dir, n_output, recon_all_args='-qcache')
 
         :param: data_dir: the directory where to put the input images, eg, example1.nii, example2.nii
         :param: output_dir: the directory where to put the results of the pipeline
-        :param: n_output, scale, the number of output files that you want to contain the results, eg, if you define n_output, then the number of output file should be sub001...sub00(n_output-1)
+        :param: n_output: scale, the number of output files that you want to contain the results, eg, if you define n_output, then the number of output file should be sub001...sub00(n_output-1)
+        :param: field_template: list, you should define it based on your input data structure       
+        :param: template_args: list containing list, you should define it based on your input data structure
         :param: recon_all_args, the default value will be set as '-qcache', which will get the result of the fsaverage.
         return: Recon-all workflow
     """
@@ -63,8 +65,10 @@ def recon_all_pipeline(data_dir, output_dir, n_output, recon_all_args='-qcache')
                                                      outfields=['out_files']), name="datasource",
                                                      iterfield = ['subject_id'])
     datasource.inputs.base_directory = data_dir
-    datasource.inputs.template = '%s/%s.nii'
-    datasource.inputs.template_args = dict(out_files=[['subject_id', 'struct']])
+    
+    datasource.inputs.template = '*'  
+    datasource.inputs.field_template = dict(out_files = field_template)
+    datasource.inputs.template_args = dict(out_files = template_args) 
     datasource.inputs.subject_id = subject_list
     datasource.inputs.sort_filelist = True
 
--- examples/Example_recon-all.py
+++ examples/Example_recon-all.py
@@ -8,15 +8,26 @@ Created on Mon Apr 25 15:04:17 2016
 
 from __future__ import absolute_import
 from clinica.pipeline.preprocessing.t1_freesurfer_workflow import recon_all_pipeline
-import os.path
+from os.path import realpath,split,join
 import tempfile
 
-data_dir = os.path.abspath('data/Recon-all')
+data_dir = join(split(realpath(__file__))[0], 'data/Recon-all')
+field_templateA = '%s/%s.nii'
+template_argsA =  [['subject_id', 'struct']]
+field_templateB = '%s/%s/*.nii'
+template_argsB = [['subject_id', 'dir']]
 output_dir = tempfile.mkdtemp()
 
 print("Data Directory -> %s" % data_dir)
 print("Output Directory -> %s" % output_dir)
 print("Running...")
 
-T1_recon_all= recon_all_pipeline(data_dir, output_dir, 3)
-T1_recon_all.run("MultiProc", plugin_args={'n_procs':4})
+def recon_all_exampleA():
+    return recon_all_pipeline(data_dir, output_dir, 3, field_templateA, template_argsA)
+    
+def recon_all_exampleB():
+    return recon_all_pipeline(data_dir, output_dir, 3, field_templateB, template_argsB)
+T1_recon_allA = recon_all_exampleA()
+T1_recon_allA.run("MultiProc", plugin_args={'n_procs':4})
+T1_recon_allB = recon_all_exampleB()
+T1_recon_allB.run("MultiProc", plugin_args={'n_procs':4})
