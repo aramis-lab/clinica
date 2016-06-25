c9f9856bdc5df3142c9003ed4c7ead6aa5d89b6b
guest <anbai106@hotmail.com>
2016-06-25 11:35:25 +0200

    reconall pipeline
    Former-commit-id: e439b77181ca4edbcd7a8637c2b0a21374ec4930

--- clinica/pipeline/preprocessing/t1_freesurfer_workflow.py
+++ clinica/pipeline/preprocessing/t1_freesurfer_workflow.py
@@ -42,6 +42,7 @@ def recon_all_pipeline(data_dir, output_dir, n_output, field_template, template_
     import nipype.pipeline.engine as pe
     import nipype.interfaces.io as nio
     from nipype.interfaces.freesurfer.preprocess import ReconAll
+    import nipype.interfaces.utility as niu
 
     try:
         if ReconAll.version.fget.func_globals['__version__'].split(".") < ['0', '11', '0']:
@@ -60,30 +61,31 @@ def recon_all_pipeline(data_dir, output_dir, n_output, field_template, template_
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
-
-    wf = pe.Workflow(name='reconall_workflow',base_dir=output_dir)
-
-    datasource = pe.MapNode(interface = nio.DataGrabber(infields=['subject_id'], 
-                                                     outfields=['out_files']), name="datasource",
-                                                     iterfield = ['subject_id'])
-    datasource.inputs.base_directory = data_dir
-    datasource.inputs.template = '*'  
-    datasource.inputs.field_template = dict(out_files = field_template)
-    datasource.inputs.template_args = dict(out_files = template_args) 
-    
-#    datasource.inputs.template = '%s/%s.nii'  
-#    datasource.inputs.field_template = dict(out_files = field_template)
-#    datasource.inputs.template_args = dict(out_files = [['subject_id', 'struct']]) 
-    datasource.inputs.subject_id = subject_list
-    datasource.inputs.sort_filelist = True
-
+    inputnode = pe.MapNode(interface = nio.DataGrabber(infields=['subject_id'], 
+                                                     outfields=['out_files']), name="inputnode",
+                                                     iterfield = ['subject_id'])   
     recon_all = pe.MapNode(interface=ReconAll(),name='recon_all', iterfield=['subject_id', 'T1_files'])
+    outputnode = pe.Node(niu.IdentityInterface(fields=['ReconAll_result']), name='outputnode')
+    
+    wf = pe.Workflow(name='reconall_workflow',base_dir=output_dir)
+   
+    inputnode.inputs.base_directory = data_dir
+    inputnode.inputs.template = '*'  
+    inputnode.inputs.field_template = dict(out_files = field_template)
+    inputnode.inputs.template_args = dict(out_files = template_args) 
+#    inputnode.inputs.template = '%s/%s.nii'  
+#    inputnode.inputs.field_template = dict(out_files = field_template)
+#    inputnode.inputs.template_args = dict(out_files = [['subject_id', 'struct']]) 
+    inputnode.inputs.subject_id = subject_list
+    inputnode.inputs.sort_filelist = True
+
     recon_all.inputs.subject_id = subject_list
     recon_all.inputs.subjects_dir = output_dir
     recon_all.inputs.directive = 'all'
     recon_all.inputs.args = recon_all_args
 
-    wf.connect(datasource,'out_files', recon_all,'T1_files')
+    wf.connect(inputnode,'out_files', recon_all,'T1_files')
+    wf.connect(recon_all, 'subject_id', outputnode, 'ReconAll_result')
 
     return wf
 
