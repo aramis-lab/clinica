0035cba97ad9bb913bf7fc3424b7678d0ac4188c
wen <anbai106@hotmail.com>
2016-07-04 20:54:26 +0200

    ClinicaSurfStat
    Former-commit-id: 22f3bfd88e740442d9ff8d8594bcd939dd2d582b

--- examples/Example_SurfStatGroupAnalysis.py
+++ examples/Example_SurfStatGroupAnalysis.py
@@ -6,11 +6,17 @@ Created on Thu Jun 30 17:03:04 2016
 """
 from __future__ import absolute_import
 from clinica.pipeline.preprocessing.SurfStatPipeline import SurfStatGruopAnalysis
-from os.path import realpath,split,join
+#from os.path import realpath,split,join
+import os
 
-a_required_path = join(split(realpath(__file__))[0], 'ClinicaSurfstat');
-PATH_TO_RECON_ALL_OUTPUTS = join(split(realpath(__file__))[0], 'data/Recon-all_Output');
-CSVFilename  = join(a_required_path, 'Database/template.csv')
+#a_required_path = join(split(realpath(__file__))[0], 'ClinicaSurfstat');
+path = os.getcwd()
+parent_path = os.path.dirname(path)
+a_required_path = os.path.join(parent_path, 'lib/ClinicaSurfstat')
+PATH_TO_RECON_ALL_OUTPUTS = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'data/Recon-all_Output');
+CSVFilename  = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'ClinicaSurfstat/Database/template.csv');
 Format = '%s %s %s %f';
 ContrastLinearModel = '1 + Label + Gender + Age';
-SurfStatGruopAnalysis(ContrastLinearModel,Format, CSVFilename, PATH_TO_RECON_ALL_OUTPUTS, a_required_path)
+result_dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'ClinicaSurfstat');
+SurfStat = SurfStatGruopAnalysis(ContrastLinearModel,Format, CSVFilename, PATH_TO_RECON_ALL_OUTPUTS, a_required_path, result_dir)
+SurfStat.run()
