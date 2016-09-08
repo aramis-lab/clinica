6fc990167b210dd1cb6ac18ff1d450418bca2e32
Alexandre Routier <alexandre.routier@gmail.com>
2016-09-08 16:23:00 +0200

    Example script for T1-FSL pipeline
    Former-commit-id: d4c2115f14ce11998178c98cc06ec018de67f241

---
+++ examples/example_t1_fsl_pipeline.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+"""This module launches the T1-FSL pipeline."""
+
+from __future__ import absolute_import
+from clinica.pipeline.t1.t1_fsl_pipeline import t1_fsl_segmentation_pipeline
+
+
+from os.path import realpath,split,join
+import tempfile
+
+
+data_path = join(split(realpath(__file__))[0], 'external-data/raw_data/subject_example')
+output_directory = tempfile.mkdtemp()
+
+
+fsl_t1 = t1_fsl_segmentation_pipeline(datasink_directory=output_directory, is_bias_corrected=False)
+fsl_t1.inputs.inputnode.in_t1 = join(data_path, 'T1/subject_example_t1.nii.gz')
+
+
+print("Datasink Directory -> %s" % output_directory)
+fsl_t1.run()
+print("Datasink Directory -> %s" % output_directory)
