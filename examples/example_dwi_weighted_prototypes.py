a7abb34e3e4165552aac892620d65693eca80c6d
Pietro Gori <pietro.gori@inria.fr>
2016-09-12 17:45:16 +0200

    Example is Bundle_medium.vtk
    Former-commit-id: 8dcb302a5122964ed7718b41ef90b903404d21f5

--- examples/example_dwi_weighted_prototypes.py
+++ examples/example_dwi_weighted_prototypes.py
@@ -63,17 +63,17 @@ import errno
 cwd_path = split(realpath(__file__))[0]
 
 ## MANDATORY PARAMETERS ##
-lambda_g=4.0
-lambda_a=3.0
-lambda_b=3.0
-type='small' # It can be 'small' 'medium' or 'big'
+lambda_g=7.0
+lambda_a=5.0
+lambda_b=10.0
+type='medium' # It can be 'small' 'medium' or 'big'
 filename_bundle=join(cwd_path, 'external-data/dwi_weighted_prototypes/bundle_%s.vtk') % type
 
 ## OPTIONAL PARAMETERS ##
 bound_limit_input=0.0
 degree_precision_input=0.0
 num_iter_modularity_input=0
-minimum_number_fibers_cluster_input=1
+minimum_number_fibers_cluster_input=0
 minValueTau_input=0.0
 increase_radius_input=0.0
 
