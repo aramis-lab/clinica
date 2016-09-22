e540e91dbfa419cfdddb07321ea67352f37ea4f6
Hao <anbai106@hotmail.com>
2016-09-22 16:46:10 +0200

    Finish for pipeline of surfstat and reconall of command line
    Former-commit-id: c6fdb73a0dd29b0b065e914a47a49347a8a1c38e

--- clinica/cmdline.py
+++ clinica/cmdline.py
@@ -160,7 +160,8 @@ def execute():
     run_parser = sub_parser.add_parser('run')
     #adding the independent pipeline ArgumentParser objects
     # init_cmdparser_objects(run_parser.add_subparsers())
-    init_cmdparser_objects(run_parser.add_subparsers(),[CmdParserT1SPMFullPrep(),CmdParserT1SPMSegment()])
+    init_cmdparser_objects(run_parser.add_subparsers(),[CmdParserT1SPMFullPrep(),CmdParserT1SPMSegment(),
+                                                        CmdParserT1ReconAll(), CmdParserStatisticsSurfStat()])
 
     try:
         argcomplete.autocomplete(parser)
