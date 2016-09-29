32e2b1ed3b936227ef8e96a774b6fe6efa728e25
jsampergonzalez <jsampergonzalez@gmail.com>
2016-09-29 11:51:03 +0200

    t1-spm-segment and t1-spm-full-prep updated
    Former-commit-id: f4e537dbed7e8dee66a7ce6e23ebc09bb94032e5

--- examples/example_t1_spm.py
+++ examples/example_t1_spm.py
@@ -58,4 +58,8 @@
 #
 # #launch_XNATSource_example(server, user, password, experiment_dir, output_dir)
 
-clinica run t1-spm-full-prep /Users/jorge.samper/Workspace/test/full_pipeline_data /Users/jorge.samper/Workspace/test/full_pipeline_temp /Users/jorge.samper/Workspace/test/full_pipeline_output
+clinica run t1-spm-full-prep /Users/jorge.samper/Workspace/test/full_pipeline_data /Users/jorge.samper/Workspace/test/full_pipeline_output -wd /Users/jorge.samper/Workspace/test/full_pipeline_temp -np 10 -ti 1 2 3 4 -dt 1 2 -swu -swm -wdf True True -fwhm 12 12 12 -m False -vs 3 3 3
+
+clinica run t1-spm-segment /Users/jorge.samper/Workspace/test/segment_data /Users/jorge.samper/Workspace/test/segment_output -wd /Users/jorge.samper/Workspace/test/segment_temp -np 10 -ti 1 2 3 4 -dt 1 2 -swu -swm -wdf True True
+
+
