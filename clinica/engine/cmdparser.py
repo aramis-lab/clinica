ede415c04e7da15fa805db75ce5750757378cd4d
Hao <anbai106@hotmail.com>
2016-10-19 15:19:22 +0200

    Correction for the reconall command line
    Former-commit-id: 478d48ed35f57ab713b0810ad28f6914a47551c4

--- clinica/engine/cmdparser.py
+++ clinica/engine/cmdparser.py
@@ -280,19 +280,18 @@ class CmdParserT1ReconAll(CmdParser):
         self._name = 't1-reconall'
 
     def define_options(self):
-        self._args.add_argument("input_directory", help='Directory where the NIFTI images are stored')
-        self._args.add_argument("output_dir", help='Directory to store the result of the pipeline')
-        self._args.add_argument("field_template", help='A list to define the input structure')
-        self._args.add_argument("template_args", help='A list of list to define the input structure, the name of the NIFTI images')
-        self._args.add_argument("-ifs", "--intermediate_files", type=list, default=['orig', 'white'], help='The intermediate files stored in datasinker')
+        self._args.add_argument("input_directory", help='Path to where the NIFTI images are stored')
+        self._args.add_argument("output_dir", help='Path to store the result of the pipeline')
+        self._args.add_argument("tsv_file", help='Path pointed to your tsv file')
+        self._args.add_argument("dataset_name", help='A list of the dataset name')
         self._args.add_argument("-ras", "--reconall_args", type=str, default='-qcache', help='additional flags for reconAll command line, default is -qcache')
 
     def run_pipeline(self, args):
 
         from clinica.pipeline.t1.t1_freesurfer import recon_all_pipeline
 
-        reconall_wf = recon_all_pipeline(self.absolute_path(args.input_directory), self.absolute_path(args.output_dir), args.field_template, args.template_args,
-                                         datasink_para=args.intermediate_files, recon_all_args=args.reconall_args)
+        reconall_wf = recon_all_pipeline(self.absolute_path(args.input_directory), self.absolute_path(args.output_dir), self.absolute_path(args.tsv_file), args.dataset_name,
+                                         recon_all_args=args.reconall_args)
 
         reconall_wf.run("MultiProc", plugin_args={'n_procs':4})
 
