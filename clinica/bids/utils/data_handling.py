9d1da0f035df9e4881341d86570e7b9e219f1fc7
Sabrina Fontanella <sabrina.fontanella@icm-institute.org>
2016-11-25 14:34:12 +0100

    Fix input problem of create-subjects-visits script.
    Former-commit-id: d51d0c457c6e3102158fe4881bd40978c23b69a0

--- clinica/bids/utils/data_handling.py
+++ clinica/bids/utils/data_handling.py
@@ -4,6 +4,11 @@ import pandas as pd
 import os
 
 def find_mod_available(dataset_dir):
+    '''
+    Finds all the modalities available for a given dataset
+    :param dataset_dir:
+    :return:
+    '''
     mods_dict = {}
     subjects_paths_lists = glob(path.join(dataset_dir, '*sub-*'))
     for sub_path in subjects_paths_lists:
@@ -45,9 +50,62 @@ def find_mod_available(dataset_dir):
 
     print mods_dict
 
+
+
+def missing_modalities(in_dir, out_dir):
+    find_mod_available(in_dir)
+    missing_mods_df = pd.DataFrame(columns=['Subject ID'])
+    subjects_paths_lists = glob(path.join(in_dir, '*sub-*'))
+    if len(subjects_paths_lists) == 0:
+        raise "None subject found or dataset not BIDS complaint."
+    df_index = 0
+    for sub_path in subjects_paths_lists:
+        subj_id = sub_path.split(os.sep)[-1]
+        missing_mods_df = missing_mods_df.append({'Subject ID': subj_id}, ignore_index=True)
+        ses_paths = glob(path.join(sub_path, '*ses-*'))
+        for session in ses_paths:
+            # Extract only the directory
+            mods_paths_folders = glob(path.join(session, '*/'))
+            if ['func' in m for m in mods_paths_folders]:
+                missing_mods_df.set_value(df_index,'func', 'x')
+            else:
+                print 'Func folder not found for subject ', subj_id
+                missing_mods_df.set_value(df_index, 'func', '-')
+
+            if ['dwi' in m for m in mods_paths_folders]:
+                missing_mods_df.set_value(df_index, 'dwi', 'x')
+            else:
+                print 'DWI folder not found for subject', subj_id
+                missing_mods_df.set_value(df_index, 'dwi', '-')
+
+            if ['anat' in m for m in mods_paths_folders]:
+                anat_files_paths = glob(path.join(session,'anat','*'))
+
+                for anat_file in anat_files_paths:
+                    anat_name = anat_file.split(os.sep)[-1]
+
+                    # Extract the name of the file without the extension
+                    if '.nii.gz' in anat_name:
+                        anat_name = anat_name.replace('.nii.gz', '')
+                        anat_ext = 'nii.gz'
+                    else:
+                      anat_name = os.path.splitext(anat_name.split(os.sep)[-1])[0]
+                      anat_ext = os.path.splitext(anat_name.split(os.sep)[-1])[1]
+
+
+                    if anat_ext!='json':
+                        file_parts = anat_name.split("_")
+                        anat_type = file_parts[len(file_parts)-1]
+                        if anat_type in missing_mods_df:
+                            missing_mods_df.set_value(df_index, anat_type, 'x')
+                        else:
+                            # Create the column with the default value '-'
+                            print missing_mods_df.index
+                            missing_mods_df[anat_type] = pd.Series('-')
+
 def create_subs_sess_list(dataset_path, out_dir):
     file_name = out_dir.split(os.sep)[-1]
-    if len(file_name) == 0:
+    if len(file_name) == 0 or out_dir=='.':
         file_name = 'subjects_sessions_list.tsv'
     else:
         # Extract the path of the file
@@ -55,6 +113,13 @@ def create_subs_sess_list(dataset_path, out_dir):
 
     if '.' not in file_name:
         file_name = file_name+'.tsv'
+    else:
+        extension = os.path.splitext(file_name)[1]
+        if extension != '.tsv':
+            raise 'Output file must be .tsv.'
+
+    if out_dir ==  '.':
+        out_dir = os.getcwd()
 
     subjs_sess_tsv = open(path.join(out_dir, file_name), 'w')
     subjs_sess_tsv.write('participant_id' + '\t' + 'session_id' + '\n')
