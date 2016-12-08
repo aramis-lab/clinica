3c9c100a3924002bb078f00267ef969d97ad7577
Sabrina Fontanella <sabrina.fontanella@icm-institute.org>
2016-12-08 16:15:24 +0100

    Fix some bugs in MissingModsTracker in converter_utils
    Former-commit-id: c3ad88b03260df4997a55ae008cbd3644efeb8e8

--- clinica/bids/converter_utils.py
+++ clinica/bids/converter_utils.py
@@ -11,8 +11,9 @@ def print_statistics(summary_file, num_subjs, ses_aval, mmt):
         mmt: object MissingModsTracker
     """
     missing_list = mmt.get_missing_list()
+    summary_file.write('**********************************************\n')
     summary_file.write('Number of subjects converted: ' + str(num_subjs) + '\n')
-    summary_file.write('Sessions available: '+ses_aval[0] + ' '+ses_aval[1]+'\n')
+    summary_file.write('Sessions available: '+ses_aval[0] +'\n')
 
     for ses in ses_aval:
         ses_miss = missing_list[ses]['session']
@@ -20,76 +21,79 @@ def print_statistics(summary_file, num_subjs, ses_aval, mmt):
         perc_ses_found = ses_found*100/num_subjs
         summary_file.write('Number of sessions '+ses+" found: "+str(ses_found)+' ('+str(perc_ses_found)+'%)\n')
 
-    summary_file.write('****************************************\n')
-    summary_file.write('Number of missing modalities for each session (more details inside converter.log):\n')
+    summary_file.write('**********************************************\n\n')
+    summary_file.write('Number of missing modalities for each session:\n')
 
     for ses in ses_aval:
-        summary_file.write('\n-' + ses + '-\n')
+        summary_file.write('\n' + ses + '\n')
         for mod in missing_list[ses]:
             if mod != 'session':
-                num_miss_mod = missing_list[ses][mod][0]
+                num_miss_mod = missing_list[ses][mod]
                 percentage_missing = round((num_miss_mod*100/float(num_subjs - missing_list[ses]['session'])),2)
                 summary_file.write(mod+': '+ str(num_miss_mod) + ' ('+str(percentage_missing) + '%) \n')
 
-    summary_file.write('\n\nNumber of incomplete modalities for each session (more details inside converter.log):\n')
-    for ses in ses_aval:
-        summary_file.write('\n-'+ses+'-\n')
-        for mod in missing_list[ses]:
-            if mod != 'session':
-                num_miss_mod = missing_list[ses][mod][1]
-                if num_miss_mod != 0:
-                    percentage_missing = round((num_miss_mod*100/float(num_subjs - missing_list[ses]['session'])), 2)
-                else:
-                    percentage_missing = 0
-                summary_file.write(mod+': ' + str(num_miss_mod) + ' ('+str(percentage_missing)+'%) \n')
+    # summary_file.write('\n\nNumber of incomplete modalities for each session (more details inside converter.log):\n')
+    # for ses in ses_aval:
+    #     summary_file.write('\n-'+ses+'-\n')
+    #     for mod in missing_list[ses]:
+    #         if mod != 'session':
+    #             num_miss_mod = missing_list[ses][mod][1]
+    #             if num_miss_mod != 0:
+    #                 percentage_missing = round((num_miss_mod*100/float(num_subjs - missing_list[ses]['session'])), 2)
+    #             else:
+    #                 percentage_missing = 0
+    #             summary_file.write(mod+': ' + str(num_miss_mod) + ' ('+str(percentage_missing)+'%) \n')
 
 
 class MissingModsTracker:
     """
     Class used for tracking the number of missing modalities in a database
     """
-    def __init__(self, ses):
+    def __init__(self, ses, mod_list=False):
         self.missing = {}
         self.ses = ses
-        for s in ses:
-            self.missing.update({s: {'session': 0,
-                                     'DTI': [0, 0],
-                                     'fMRI': [0, 0],
-                                     'Fieldmap': [0, 0],
-                                     'FLAIR': [0, 0],
-                                      'T1': [0, 0]}
-                                })
-        self.mods_used = []
+        if mod_list!=False:
+            for s in ses:
+                self.missing.update({s:{'session':0}})
+                for mod in mod_list:
+                    self.missing[s].update({mod: 0})
+        else:
+            for s in ses:
+                self.missing.update({s: {'session': 0,
+                                            'Dwi': 0,
+                                            'Func': 0,
+                                            'Fieldmap': 0,
+                                            'FLAIR': 0,
+                                            'T1w': 0}
+                                    })
 
-    def add_missing_mod(self, mod, ses):
+    def add_missing_mod(self, ses, mod):
         """
         Increase the number of missing files for the input modality.
 
         Args:
+            ses: name of the session
             mod: modality missing
         """
-        self.missing[ses][mod][0] += 1
+        self.missing[ses][mod] += 1
 
-        if mod not in self.mods_used:
-            self.mods_used.append(mod)
 
     def increase_missing_ses(self, ses):
-        self.missing[ses] += 1
-
-    def add_incomplete_mod(self, mod, ses):
-        (self.missing[ses][mod])[1] = (self.missing[ses][mod][1]) + 1
-
-    def incr_missing_session(self, ses):
         self.missing[ses]['session'] += 1
 
-    def remove_mods_unused(self):
-        """
-        Remove all the modalities not included into the list mods_used
-        """
-        keys = self.mods_missing.keys()
-        for key in keys:
-            if key not in self.mods_used:
-                del self.mods_missing[key]
+    # def add_incomplete_mod(self, mod, ses):
+    #     (self.missing[ses][mod])[1] = (self.missing[ses][mod][1]) + 1
+
+
+    #
+    # def remove_mods_unused(self):
+    #     """
+    #     Remove all the modalities not included into the list mods_used
+    #     """
+    #     keys = self.mods_missing.keys()
+    #     for key in keys:
+    #         if key not in self.mods_used:
+    #             del self.mods_missing[key]
 
     def get_missing_list(self):
         """
