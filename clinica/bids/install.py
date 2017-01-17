38fa5213a910ff486c04e18e9c8873da50f1acbe
michael.bacci <michael.bacci@inria.fr>
2017-01-17 16:32:13 +0100

    adding install method for adding converters
    Former-commit-id: f40fd84ec989c10c42264a102428cfb054880adf

--- clinica/bids/install.py
+++ clinica/bids/install.py
@@ -3,12 +3,19 @@ from os.path import basename, realpath,split,join
 from shutil import copyfile
 
 def add_bids_converter(src):
-    converter_path = join(split(realpath(__file__))[0], 'converters')
+    copy_data(src, 'converters')
+
+def add_bids_data(src):
+    copy_data(src, 'data')
+
+def copy_data(src, dest):
+    converter_path = join(split(realpath(__file__))[0], dest)
     src_name = basename(src)
     dst = join(converter_path, src_name)
     try:
         copyfile(src, dst)
     except:
         print('re-run with sudo permision')
+        exit(-1)
 
     print('Installation of %s done' % basename(src))
