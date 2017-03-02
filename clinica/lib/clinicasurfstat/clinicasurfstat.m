84e23d761e816f803d6e002352d9964776cffa9d
Hao <anbai106@hotmail.com>
2017-03-02 10:08:43 +0100

    change of name of uncorrected p value3
    Former-commit-id: ca682ebbc815d49a42a6fc46a89f242ee7363f63

--- clinica/lib/clinicasurfstat/clinicasurfstat.m
+++ clinica/lib/clinicasurfstat/clinicasurfstat.m
@@ -236,7 +236,7 @@ if iscell(tsvdata{indexunique})
     %uncorrectedpvalues = double(t<=0.05).*t+double(t>0.05);
     uncorrectedpvalues = 1-tcdf(slm.t,slm.df);
     clearvars struct; struct.P = uncorrectedpvalues; struct.mask = mask; struct.thresh = thresholduncorrectedpvalue;
-    SurfStatView( struct, averagesurface, [ 'ContrastNe-Uncorrected P-values( 'thresholduncorrectedpvalue ')' factor2 '-' factor1]);
+    SurfStatView( struct, averagesurface, [ 'ContrastNe-Uncorrected P-values(' thresholduncorrectedpvalue ')' factor2 '-' factor1]);
     set(gcf,'PaperPositionMode','auto');
     print('contrast_negative_uncorrected_p_value','-djpeg','-r0'); close
     disp('Contrast Negative: Uncorrected Pvalue'); toc;
@@ -309,7 +309,7 @@ else
     %uncorrectedpvalues = double(t<=0.05).*t+double(t>0.05);
     uncorrectedpvalues = 1-tcdf(slm.t,slm.df);
     clearvars struct; struct.P = uncorrectedpvalues; struct.mask = mask; struct.thresh = thresholduncorrectedpvalue;
-    SurfStatView( struct, averagesurface, [ 'Uncorrected P-values( 'thresholduncorrectedpvalue ')' contrast ]);
+    SurfStatView( struct, averagesurface, [ 'Uncorrected P-values(' thresholduncorrectedpvalue ')' contrast ]);
     set(gcf,'PaperPositionMode','auto');
     print('uncorrected_p_value','-djpeg','-r0'); close
     disp('Uncorrected Pvalue'); toc;
