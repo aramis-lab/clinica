f38d06bf3f924dfcd63a4eb86c48d6453c8ccfc7
Jérémy Guillon <jeremy.guillon@inria.fr>
2017-05-24 11:52:51 +0200

    Update fMRI example
    Former-commit-id: 14ded02e637468d91017b366795968e3a9297cb2

--- examples/example_fmri_preprocessing.py
+++ examples/example_fmri_preprocessing.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python#
 # -*- coding: utf-8 -*-
 
-from clinica.pipeline.fmri.preprocessing import FMRIPreprocessing
+from pipelines.fmri_preprocessing.fmri_preprocessing_pipeline import fMRIPreprocessing
 
-pipeline = FMRIPreprocessing('/Users/jeremy.guillon/Repositories/multiconproject/data/HMTC_BIDS',
+pipeline = fMRIPreprocessing('/Users/jeremy.guillon/Repositories/multiconproject/data/HMTC_BIDS',
                              '/Users/jeremy.guillon/Repositories/multiconproject/data/HMTC_CAPS')
 pipeline.parameters = {
     'num_slices': 45,
@@ -14,7 +14,10 @@ pipeline.parameters = {
 }
 
 pipeline.base_dir = '/Users/jeremy.guillon/Tmp2'
-pipeline.build()
-pipeline.write_graph()
+pipeline.build_core_nodes()
+pipeline.build_input_node()
+pipeline.input_node._check_inputs(pipeline.get_input_fields())
+pipeline.input_node._got_inputs
+# pipeline.write_graph()
 
 pipeline.run(plugin='MultiProc', plugin_args={'n_procs' : 2})
