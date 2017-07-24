f85934467008b5741150685a58789d661d06f74a
Sabrina Fontanella <sabrina.fontanella@icm-institute.org>
2017-07-24 10:24:24 +0200

    Update bids_utils.py
    Former-commit-id: 27d5fd12c9519cbef1f9e98929c62cff1908790a

--- clinica/iotools/bids_utils.py
+++ clinica/iotools/bids_utils.py
@@ -14,14 +14,14 @@ __status__ = "Development"
 
 # -- Methods for the clinical data --
 # @ToDo:test this function
-def create_participants_df(input_path, out_path, study_name, clinical_spec_path, clinical_data_dir, bids_ids, delete_non_bids_info=True):
+def create_participants_df(study_name, clinical_spec_path, clinical_data_dir, bids_ids, delete_non_bids_info=True):
     """
     :param input_path: path to the original dataset
     :param out_path: path to the bids folder
     :param study_name: name of the study (Ex. ADNI)
     :param clinical_spec_path: path to the clinical file
-    :param bids_ids:
-    :param delete_non_bids_info:
+    :param bids_ids: list of bids ids
+    :param delete_non_bids_info: if True delete all the rows of the subjects that are not available in the BIDS dataset
     :return: a pandas dataframe that contains the participants data
     """
     import pandas as pd
