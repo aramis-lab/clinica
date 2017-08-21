4a2c997a83ac2beec98d7c925013caa0c72a56dc
Sabrina Fontanella <sabrina.fontanella@icm-institute.org>
2017-08-21 17:33:06 +0200

    Add OASIS converter with CLI to Clinica
    Former-commit-id: 8d58dde5f708798c3517afeabb84408f2153911c

---
+++ clinica/iotools/converters/oasis_to_bids/oasis_to_bids_cli.py
@@ -0,0 +1,31 @@
+import clinica.engine as ce
+
+
+class OasisToBidsCLI(ce.CmdParser):
+    def define_name(self):
+        """Define the sub-command name to run this pipeline.
+        """
+        self._name = 'convert-oasis-to-bids'
+
+    def define_options(self):
+        """Define the sub-command arguments
+        """
+        self._args.add_argument("dataset_directory",
+                               help='Path to the OASIS images directory.')
+        self._args.add_argument("clinical_data_directory",
+                                help='Path to the OASIS clinical data directory.')
+        self._args.add_argument("bids_directory",
+                                help='Path to the BIDS directory.')
+
+    def run_pipeline(self, args):
+        from clinica.iotools.converters.oasis_to_bids.oasis_to_bids import OasisToBids
+        oasis_to_bids = OasisToBids()
+
+        oasis_to_bids.convert_images(args.dataset_directory, args.bids_directory)
+        oasis_to_bids.convert_clinical_data(args.clinical_data_directory, args.bids_directory)
+
+
+
+
+
+
