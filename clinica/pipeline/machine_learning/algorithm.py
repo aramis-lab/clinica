f43fddeed9203fa096165a077b029a59907ed585
jsampergonzalez <jsampergonzalez@gmail.com>
2017-08-24 20:46:28 +0200

    ML classes updated.
    Former-commit-id: e8267b7a6a2ae4750746c272d226b84c16ca33f2

--- clinica/pipeline/machine_learning/algorithm.py
+++ clinica/pipeline/machine_learning/algorithm.py
@@ -1,26 +1,28 @@
 
-from clinica.pipeline.machine_learning import base
+from os import path
+import json
 from multiprocessing.pool import ThreadPool
+
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
-from scipy.stats import mode
-from clinica.pipeline.machine_learning.svm_utils import evaluate_prediction, calculate_auc
+
+from clinica.pipeline.machine_learning import base
+import clinica.pipeline.machine_learning.svm_utils as utils
 
 
-class KernelSVMAlgorithm(base.MLAlgorithm):
+class DualSVMAlgorithm(base.MLAlgorithm):
 
-    def __init__(self, kernel, y, balanced=False, grid_search_folds=10, c_range=np.logspace(-6, 2, 17), n_threads=15):
+    def __init__(self, kernel, y, balanced=True, grid_search_folds=10, c_range=np.logspace(-6, 2, 17), n_threads=15):
         self._kernel = kernel
         self._y = y
         self._balanced = balanced
         self._grid_search_folds = grid_search_folds
         self._c_range = c_range
         self._n_threads = n_threads
-        self._async_result = {}
 
-    def launch_svc(self, kernel_train, x_test, y_train, y_test, c, shared_x=None, train_indices=None,
-                   test_indices=None):
+    def _launch_svc(self, kernel_train, x_test, y_train, y_test, c, shared_x=None, train_indices=None,
+                    test_indices=None):
 
         if self._balanced:
             svc = SVC(C=c, kernel='precomputed', tol=1e-6, class_weight='balanced')
@@ -29,28 +31,31 @@ class KernelSVMAlgorithm(base.MLAlgorithm):
 
         svc.fit(kernel_train, y_train)
         y_hat = svc.predict(x_test)
-        auc = None
+        auc = 0.0
 
         if shared_x is not None and train_indices is not None and test_indices is not None:
-            auc = calculate_auc(svc, shared_x, train_indices, test_indices, y_test)
+            auc = utils.calculate_auc(svc, shared_x, train_indices, test_indices, y_test)
 
-        return y_hat, auc
+        return svc, y_hat, auc
 
-    def inner_grid_search(self, kernel_train, x_test, y_train, y_test, c):
+    def _grid_search(self, kernel_train, x_test, y_train, y_test, c):
 
-        y_hat, _ = self.launch_svc(kernel_train, x_test, y_train, y_test, c)
-        res = evaluate_prediction(y_test, y_hat)
+        _, y_hat, _ = self._launch_svc(kernel_train, x_test, y_train, y_test, c)
+        res = utils.evaluate_prediction(y_test, y_hat)
 
         return res['balanced_accuracy']
 
-    def _select_best_parameter(self):
+    def _select_best_parameter(self, async_result):
 
         c_values = []
         accuracies = []
-        for fold in self._async_result.keys():
-            best_c = 0
-            best_acc = 0
-            for c, acc in self._async_result[fold].iteritems():
+        for fold in async_result.keys():
+            best_c = -1
+            best_acc = -1
+
+            for c, async_acc in async_result[fold].iteritems():
+
+                acc = async_acc.get()
                 if acc > best_acc:
                     best_c = c
                     best_acc = acc
@@ -62,12 +67,12 @@ class KernelSVMAlgorithm(base.MLAlgorithm):
 
         return {'c': best_c, 'balanced_accuracy': best_acc}
 
-    def parameter_estimation(self, train_index):
+    def evaluate(self, train_index, test_index):
 
         inner_pool = ThreadPool(self._n_threads)
-
+        async_result = {}
         for i in range(self._grid_search_folds):
-            self._async_result[i] = {}
+            async_result[i] = {}
 
         outer_kernel = self._kernel[train_index, :][:, train_index]
         y_train = self._y[train_index]
@@ -76,20 +81,76 @@ class KernelSVMAlgorithm(base.MLAlgorithm):
         inner_cv = list(skf.split(np.zeros(len(y_train)), y_train))
 
         for i in range(len(inner_cv)):
-            inner_train_index, inner_test_index = inner_cv[j]
+            inner_train_index, inner_test_index = inner_cv[i]
 
             inner_kernel = outer_kernel[inner_train_index, :][:, inner_train_index]
             x_test_inner = outer_kernel[inner_test_index, :][:, inner_train_index]
             y_train_inner, y_test_inner = y_train[inner_train_index], y_train[inner_test_index]
 
             for c in self._c_range:
-                # print 'Launched %d, %d, %0.5f' %(i, j, c)
-                self._async_result[i][c] = inner_pool.apply_async(self.inner_grid_search,
-                                                                  (inner_kernel, x_test_inner,
-                                                                   y_train_inner, y_test_inner, c))
-
-        # print 'All inner threads launched'
+                async_result[i][c] = inner_pool.apply_async(self._grid_search,
+                                                            (inner_kernel, x_test_inner,
+                                                             y_train_inner, y_test_inner, c))
         inner_pool.close()
         inner_pool.join()
-        # print 'All inner threads finished'
-        return self._select_best_parameter()
+
+        best_parameter = self._select_best_parameter(async_result)
+        x_test = self._kernel[test_index, :][:, train_index]
+        y_train, y_test = self._y[train_index], self._y[test_index]
+
+        _, y_hat, auc = self._launch_svc(outer_kernel, x_test, y_train, y_test, best_parameter['c'])
+
+        result = dict()
+        result['best_parameter'] = best_parameter
+        result['evaluation'] = utils.evaluate_prediction(y_test, y_hat)
+        result['y_hat'] = y_hat
+        result['y'] = y_test
+        result['y_index'] = test_index
+        result['auc'] = auc
+
+        return result
+
+    def apply_best_parameters(self, results_list):
+
+        best_c_list = []
+        bal_acc_list = []
+
+        for result in results_list:
+            best_c_list.append(result['best_parameter']['c'])
+            bal_acc_list.append(result['best_parameter']['balanced_accuracy'])
+
+        # 10^(mean of log10 of best Cs of each fold) is selected
+        best_c = np.power(10, np.mean(np.log10(best_c_list)))
+        # Mean balanced accuracy
+        mean_bal_acc = np.mean(bal_acc_list)
+
+        if self._balanced:
+            svc = SVC(C=best_c, kernel='precomputed', tol=1e-6, class_weight='balanced')
+        else:
+            svc = SVC(C=best_c, kernel='precomputed', tol=1e-6)
+
+        svc.fit(self._kernel, self._y)
+
+        return svc, {'c': best_c, 'balanced_accuracy': mean_bal_acc}
+
+    def save_classifier(self, classifier, output_dir):
+
+        np.savetxt(path.join(output_dir, 'dual_coefficients.txt'), classifier.dual_coef_)
+        np.savetxt(path.join(output_dir, 'support_vectors_indices.txt'), classifier.support_)
+        np.savetxt(path.join(output_dir, 'intersect.txt'), classifier.intercept_)
+
+    def save_weights(self, classifier, x, output_dir):
+
+        dual_coefficients = classifier.dual_coef_
+        sv_indices = classifier.support_
+
+        weighted_sv = dual_coefficients.transpose() * x[sv_indices]
+        weights = np.sum(weighted_sv, 0)
+
+        np.savetxt(path.join(output_dir, 'weights.txt'), weights)
+
+        return weights
+
+    def save_parameters(self, parameters_dict, output_dir):
+        with open(path.join(output_dir, 'best_parameters.json'), 'w') as f:
+            json.dump(parameters_dict, f)
