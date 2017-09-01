5361c8fb126f817903de2462a6d4b477a8e37518
jsampergonzalez <jsampergonzalez@gmail.com>
2017-09-01 18:22:06 +0200

    ML updated
    Former-commit-id: 10a48a653aac929448a52fcb6ae39f898debdb52

--- clinica/pipeline/machine_learning/ml_workflows.py
+++ clinica/pipeline/machine_learning/ml_workflows.py
@@ -105,7 +105,7 @@ class VB_RepKFold_DualSVM(base.MLWorkflow):
 class VB_RepHoldOut_DualSVM(base.MLWorkflow):
 
     def __init__(self, caps_directory, subjects_visits_tsv, diagnoses_tsv, group_id, image_type, output_dir, fwhm=0,
-                 modulated="on", precomputed_kernel=None, mask_zeros=True, n_threads=15, n_iterations=100, test_size=0.3,
+                 modulated="on", pvc=None, precomputed_kernel=None, mask_zeros=True, n_threads=15, n_iterations=100, test_size=0.3,
                  grid_search_folds=10, balanced=True, c_range=np.logspace(-6, 2, 17)):
         self._output_dir = output_dir
         self._n_threads = n_threads
@@ -116,7 +116,8 @@ class VB_RepHoldOut_DualSVM(base.MLWorkflow):
         self._c_range = c_range
 
         self._input = input.CAPSVoxelBasedInput(caps_directory, subjects_visits_tsv, diagnoses_tsv, group_id,
-                                                image_type, fwhm, modulated, mask_zeros, precomputed_kernel)
+                                                image_type, fwhm, modulated, pvc, mask_zeros, precomputed_kernel)
+
         self._validation = None
         self._algorithm = None
 
@@ -187,15 +188,9 @@ class VB_RepHoldOut_LogisticRegression(base.MLWorkflow):
         if not path.exists(classifier_dir):
             os.makedirs(classifier_dir)
 
-        except OSError:
-            pass
-
         self._algorithm.save_classifier(classifier, classifier_dir)
         self._algorithm.save_parameters(best_params, classifier_dir)
         self._validation.save_results(self._output_dir)
-
-
-
 
 
 class RB_RepHoldOut_RandomForest(base.MLWorkflow):
@@ -221,18 +216,16 @@ class RB_RepHoldOut_RandomForest(base.MLWorkflow):
         y = self._input.get_y()
 
         self._algorithm = algorithm.RandomForest(x, y, balanced=self._balanced,
-                                                grid_search_folds=self._grid_search_folds,
-                                                n_estimators_range=self._n_estimators_range,
-                                                n_threads=self._n_threads)
+                                                 grid_search_folds=self._grid_search_folds,
+                                                 n_estimators_range=self._n_estimators_range,
+                                                 n_threads=self._n_threads)
 
         self._validation = validation.RepeatedSplit(self._algorithm, n_iterations=self._n_splits, test_size=self._test_size)
         classifier, best_params, results = self._validation.validate(y, n_threads=self._n_threads)
 
         classifier_dir = os.path.join(self._output_dir, 'classifier')
-        try:
+        if not path.exists(classifier_dir):
             os.makedirs(classifier_dir)
-        except OSError:
-            pass
 
         self._algorithm.save_classifier(classifier, classifier_dir)
         self._algorithm.save_parameters(best_params, classifier_dir)
