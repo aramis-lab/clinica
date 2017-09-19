f49134d9774e012bd025eadd640574dc918da331
Michael Bacci <michael.bacci@inria.fr>
2017-09-19 16:51:21 +0200

    code refactoring: add comments and clean old code
    Former-commit-id: 954ff83db030e7561c5a0d8789ad76d3cc4eb9b3

--- clinica/engine/template.py
+++ clinica/engine/template.py
@@ -1,71 +1,15 @@
 """
-Prototype in order to generate automatic Pipeline
+Script that generate the skeleton for a new Pipeline
 """
 from clinica.engine.cmdparser import CmdParser
-import abc
-import string
-from os.path import realpath,split,join,isdir,splitext
-from os import mkdir, getcwd, listdir
-
-
-def to_camel_case(text):
-    return ''.join(x for x in text.title() if not x.isspace())
-
-
-class AbstractTemplate:
-    __metaclass__ = abc.ABCMeta
-    args = None
-
-    def path_template(self):
-        return join(split(realpath(__file__))[0], '../resources/template')
-
-    @abc.abstractmethod
-    def file_template(self): pass
-
-    @abc.abstractmethod
-    def file_to_generate(self, args): pass
-
-    def add_argument(self, args): pass
-
-    @abc.abstractmethod
-    def dictionary(self, args): pass
-
-
-class PipelineTemplate(AbstractTemplate):
-
-    def file_template(self):
-        return 'pipeline.template'
-
-    def file_to_generate(self, args):
-        return '%sPipeline.py' % args.name
-
-    def dictionary(self, args):
-        return {
-            'clinica_major_version': 0,
-            'clinica_minor_version': 1,
-            'clinica_patch_version': 0,
-            'pipeline_name': args.name
-        }
-
-
-class VisualizeTemplate(AbstractTemplate):
-
-    def file_template(self):
-        return 'visualize.template'
-
-    def file_to_generate(self, args):
-        return '%sVisualize.py' % args.name
-
-    def dictionary(self, args):
-        return {
-            'clinica_major_version': 0,
-            'clinica_minor_version': 1,
-            'clinica_patch_version': 0,
-            'pipeline_name': args.name
-        }
-
+from os.path import join,isdir,splitext
+from os import mkdir, getcwd
 
 class CmdGenerateTemplates(CmdParser):
+    """
+    Using the jinja2 library and the pipeline template's file (in the resource dir)
+     the user can create his own run-to-go pipeline
+    """
 
     def __init__(self):
         CmdParser.__init__(self)
