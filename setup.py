ee97c05c46f8ff96d9c60a3e48ce327656aeb115
Alexandre Routier <alexandre.routier@gmail.com>
2018-05-14 09:33:52 +0200

    Update setup.py in order to be compatible with pip >= 10
    Former-commit-id: 2aa902d33b32652c16f7ee3195a491e9591d1060

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from os.path import dirname, join
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 with open(join(dirname(__file__), 'clinica/VERSION'), 'rb') as f:
     version = f.read().decode('ascii').strip()
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='Clinica',
@@ -15,7 +15,7 @@ setup(
     description='Software platform for clinical neuroscience studies',
     long_description=open('README.md').read(),
     author='ARAMIS Lab',
-    maintainer='Clinica developpers',
+    maintainer='Clinica developers',
     maintainer_email='clinica-user@googlegroups.com',
     license='MIT license',
     packages=find_packages(exclude=('tests', 'tests.*')),
@@ -33,5 +33,4 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ],
-    install_requires=reqs
 )
