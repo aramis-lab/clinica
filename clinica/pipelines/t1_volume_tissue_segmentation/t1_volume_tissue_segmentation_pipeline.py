a59d65e285b75e16547c963d352107f06a86ada8
Mauricio DIAZ <mauricio.diaz@inria.fr>
2018-06-18 14:17:27 +0200

    Fix backslash in t1_volume_tissue_segmentation_pipeline file
    Former-commit-id: e9d2c10db5e1d8337abd8a462be11b219ac77acd

--- clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
+++ clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
@@ -202,8 +202,8 @@ class T1VolumeTissueSegmentation(cpe.Pipeline):
         mlab.MatlabCommand.set_default_paths(spm_home)
 
         matlab_cmd = os.path.join(os.environ['SPMSTANDALONE_HOME'],
-                'run_spm12.sh ') /
-                + os.environ['MCR_HOME'] /
+                'run_spm12.sh ') \
+                + os.environ['MCR_HOME'] \
                 + ' script'
         spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
         
