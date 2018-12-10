f19daf202cee31675830533d1f1537d66e046593
Mauricio DIAZ <mauricio.diaz@inria.fr>
2018-12-10 10:18:48 +0100

    Edit test for coding style using pycodestyle
    Former-commit-id: 801b924127d124aa7ad702372f572b21df73798c

--- test/test_coding_style.py
+++ test/test_coding_style.py
@@ -1,27 +1,27 @@
 """
-   Test tto verify coding style dans clinica
+   Test to verify coding style dans clinica
 """
 
 __author__ = "Mauricio Diaz"
 __copyright__ = "Copyright 2016-2019 The Aramis Lab Team"
-__credits__ = ["Arnaud Marcoux"]
+__credits__ = ["Mauricio Diaz"]
 __license__ = "See LICENSE.txt file"
 __version__ = "0.2.0"
 __maintainer__ = "Mauricio Diaz"
 __email__ = "mauricio.diaz@inria.fr"
 __status__ = "Development"
 
-
-import warnings
-import sys
 import pycodestyle
 
 
 def test_coding_style():
     """Test that we conform to PEP-8."""
-    style = pycodestyle.StyleGuide(quiet=True)
-    result = style.check_files(['file1.py', 'file2.py'])
-    self.assertEqual(result.total_errors, 0,
-                     "Found code style errors (and warnings).")    
+    style = pycodestyle.StyleGuide(
+            quiet=True,
+            ignore=['E203', 'E121', 'E123', 'E126', 'E133',
+                    'E226', 'E241', 'E242', 'E704', 'W503',
+                    'E501', 'W504', 'W505'])
+    result = style.check_files(['clinica/'])
+    assert result.total_errors ==  0, "Found code style errors (and warnings)."
     pass
 
