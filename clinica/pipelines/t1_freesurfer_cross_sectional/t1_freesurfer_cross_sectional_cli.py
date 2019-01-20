e99aba905acb37f641f33af5508537073dd20814
Alexandre Routier <alexandre.routier@gmail.com>
2019-01-20 13:42:37 +0100

    Harmonize CLI for t1-freesurfer pipeline
    Former-commit-id: e8d305148a715881560c70b3be7ae1908701d2c0

--- clinica/pipelines/t1_freesurfer_cross_sectional/t1_freesurfer_cross_sectional_cli.py
+++ clinica/pipelines/t1_freesurfer_cross_sectional/t1_freesurfer_cross_sectional_cli.py
@@ -12,25 +12,31 @@ class T1FreeSurferCrossSectionalCLI(ce.CmdParser):
     def define_description(self):
         """Define a description of this pipeline.
         """
-        self._description = 'Cross-sectional pre-processing of T1w images with FreeSurfer:\nhttp://clinica.run/doc/Pipelines/T1_FreeSurfer/'
+        self._description = 'Cross-sectional pre-processing of T1w images with FreeSurfer:\nhttp://clinica.run/doc/Pipelines/T1_FreeSurfer_CrossSectional/'
 
     def define_options(self):
         """Define the sub-command arguments
         """
-        # default args created by template
-        self._args.add_argument("bids_directory",
-                                help='Path to the BIDS directory.')
-        self._args.add_argument("caps_directory",
-                                help='Path to the CAPS directory.')
-        self._args.add_argument("-tsv", "--subjects_sessions_tsv",
-                                help='TSV file containing a list of subjects with their sessions.')
-        # Custom args added by developers
-        self._args.add_argument("-ras", "--recon_all_args",
-                                help='additional flags for recon-all command line, default will be -qcache')
-        self._args.add_argument("-wd", "--working_directory",
-                                help='Temporary directory to store pipelines intermediate results')
-        self._args.add_argument("-np", "--n_procs", type=int,
-                                help='Number of cores used to run in parallel')
+        from clinica.engine.cmdparser import PIPELINE_CATEGORIES
+        # Clinica compulsory arguments (e.g. BIDS, CAPS, group_id)
+        clinica_comp = self._args.add_argument_group(PIPELINE_CATEGORIES['CLINICA_COMPULSORY'])
+        clinica_comp.add_argument("bids_directory",
+                                  help='Path to the BIDS directory.')
+        clinica_comp.add_argument("caps_directory",
+                                  help='Path to the CAPS directory.')
+        # Optional arguments (e.g. FWHM)
+        optional = self._args.add_argument_group(PIPELINE_CATEGORIES['OPTIONAL'])
+        optional.add_argument("-raa", "--recon_all_args",
+                              help='Additional flags for recon-all command line (default: --recon_all_args "-qcache")')
+        # Clinica standard arguments (e.g. --n_procs)
+        clinica_opt = self._args.add_argument_group(PIPELINE_CATEGORIES['CLINICA_OPTIONAL'])
+        clinica_opt.add_argument("-tsv", "--subjects_sessions_tsv",
+                                 help='TSV file containing a list of subjects with their sessions.')
+        clinica_opt.add_argument("-wd", "--working_directory",
+                                 help='Temporary directory to store pipelines intermediate results')
+        clinica_opt.add_argument("-np", "--n_procs",
+                                 metavar=('N'), type=int,
+                                 help='Number of cores used to run in parallel')
 
     def run_command(self, args):
         """
@@ -40,17 +46,15 @@ class T1FreeSurferCrossSectionalCLI(ce.CmdParser):
         from tempfile import mkdtemp
 
         pipeline = T1FreeSurferCrossSectional(
-            # pass these args by the class attribute itself
             bids_directory=self.absolute_path(args.bids_directory),
             caps_directory=self.absolute_path(args.caps_directory),
-            tsv_file=self.absolute_path(args.subjects_sessions_tsv))
+            tsv_file=self.absolute_path(args.subjects_sessions_tsv)
+        )
 
         pipeline.parameters = {
-            # pass these args by using self.parameters in a dictionary
             'recon_all_args': args.recon_all_args or '-qcache'
         }
 
-        # make sure if working_directory is not defined, using a temp folder to the working directory.
         if args.working_directory is None:
             args.working_directory = mkdtemp()
         pipeline.base_dir = self.absolute_path(args.working_directory)
