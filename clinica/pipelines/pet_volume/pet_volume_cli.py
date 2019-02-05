c2c333243faa2e6bacc8506bac696e5679a28b20
Alexandre Routier <alexandre.routier@gmail.com>
2019-02-05 18:41:58 +0100

    Harmonize & improve CLI help for pet-volume pipeline
    Former-commit-id: 289f16028b2e49b4f4bb80a03d0de5d384b6637d

--- clinica/pipelines/pet_volume/pet_volume_cli.py
+++ clinica/pipelines/pet_volume/pet_volume_cli.py
@@ -30,9 +30,9 @@ class PETVolumeCLI(ce.CmdParser):
                                   help='User-defined identifier for the provided group of subjects.')
         # Optional arguments (e.g. FWHM)
         optional = self._args.add_argument_group(PIPELINE_CATEGORIES['OPTIONAL'])
-        optional.add_argument("-fwhm", "--fwhm_tsv",
-                              help='TSV file containing the fwhm_x, fwhm_y and fwhm_z for each pet image.')
-        optional.add_argument("-pet_type", "--pet_type", type=str, default='fdg', choices=['fdg', 'av45'],
+        optional.add_argument("-psf", "--psf_tsv",
+                              help='TSV file containing for each PET image its point spread function (PSF) measured at full-width and half maximum (fwhm_x, fwhm_y and fwhm_z).')
+        optional.add_argument("-pet_tracer", "--pet_tracer", type=str, default='fdg', choices=['fdg', 'av45'],
                               help='PET tracer. Can be fdg or av45 (default: --pet_tracer fdg)')
         # Clinica standard arguments (e.g. --n_procs)
         clinica_opt = self._args.add_argument_group(PIPELINE_CATEGORIES['CLINICA_OPTIONAL'])
@@ -46,24 +46,23 @@ class PETVolumeCLI(ce.CmdParser):
         # Advanced arguments (i.e. tricky parameters)
         advanced = self._args.add_argument_group(PIPELINE_CATEGORIES['ADVANCED'])
         advanced.add_argument("-mask", "--mask_tissues", nargs='+', type=int, default=[1, 2, 3], choices=range(1, 7),
-                              help="Tissue classes (gray matter, GM; white matter, WM; cerebro-spinal fluid, CSF...) to use for masking the PET image. Ex: 1 2 3 is GM, WM and CSF")
+                              help="Tissue classes (1: gray matter (GM), 2: white matter (WM), 3: cerebrospinal fluid (CSF), 4: bone, 5: soft-tissue, 6: background) to use for masking the PET image (default: GM, WM and CSF i.e. --mask_tissues 1 2 3).")
         advanced.add_argument("-threshold", "--mask_threshold", type=float, default=0.3,
-                              help='Value to be used as threshold to binarize the tissues mask (default: --mask_threshold 0.3).')
+                              help='Value used as threshold to binarize the tissue maps (default: --mask_threshold 0.3).')
         advanced.add_argument("-pvc_mask", "--pvc_mask_tissues",
                               nargs='+', type=int, default=[1, 2, 3], choices=range(1, 7),
-                              help="Tissue classes (gray matter, GM; white matter, WM; cerebro-spinal fluid, CSF...) to use as mask for PVC. Ex: 1 2 3 is GM, WM and CSF")
-        advanced.add_argument("-smooth", "--smooth", nargs='+', type=int, default=[8],
-                              help="A list of integers specifying the different isomorphic fwhm in millimeters to smooth the image")
+                              help="Tissue classes (1: gray matter (GM), 2: white matter (WM), 3: cerebrospinal fluid (CSF), 4: bone, 5: soft-tissue, 6: background) to use as mask for PVC  (default: GM, WM and CSF i.e. --pvc_mask_tissues 1 2 3).")
+        advanced.add_argument("-smooth", "--smooth", nargs='+', type=int, default=[8], metavar="FWHM_N FWHM_M",
+                              help="A list of integers (e.g. --smooth 6 8) specifying the different isomorphic FWHM in millimeters to smooth the image (default: --smooth 8).")
+        list_atlases = ['AAL2', 'LPBA40', 'Neuromorphometrics', 'AICHA', 'Hammers']
         advanced.add_argument("-atlases", "--atlases",
                               nargs='+', type=str, metavar="",
-                              default=['AAL2', 'LPBA40', 'Neuromorphometrics', 'AICHA', 'Hammers'],
-                              choices=['AAL2', 'LPBA40', 'Neuromorphometrics', 'AICHA', 'Hammers'],
-                              help='A list of atlases to use to calculate the mean SUVR at each region (default: all atlases i.e. --atlases AAL2 AICHA Hammers LPBA40 Neuromorphometrics).')
+                              default=list_atlases, choices=list_atlases,
+                              help='A list of atlases used to calculate regional mean SUVRs (default: all atlases i.e. --atlases AAL2 AICHA Hammers LPBA40 Neuromorphometrics).')
 
     def run_command(self, args):
         """
         """
-
         from clinica.pipelines.pet_volume.pet_volume_pipeline import PETVolume
 
         pipeline = PETVolume(
@@ -71,11 +70,11 @@ class PETVolumeCLI(ce.CmdParser):
             caps_directory=self.absolute_path(args.caps_directory),
             tsv_file=self.absolute_path(args.subjects_sessions_tsv),
             group_id=args.group_id,
-            fwhm_tsv=self.absolute_path(args.fwhm_tsv)
+            fwhm_tsv=self.absolute_path(args.psf_tsv)
         )
 
         pipeline.parameters.update({
-            'pet_type': args.pet_type,
+            'pet_type': args.pet_tracer,
             'mask_tissues': args.mask_tissues,
             'mask_threshold': args.mask_threshold,
             'pvc_mask_tissues': args.pvc_mask_tissues,
