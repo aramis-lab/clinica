be3b64e285c1b7b270193020ce1753ecbe143657
Nono <arnaud.marcoux@inria.fr>
2019-03-14 16:56:59 +0100

    Improve CLI for aibl converter
    Former-commit-id: b01b9af530a4f35e50e20c09acd1407269d24f7a

--- clinica/iotools/converters/aibl_to_bids/aibl_to_bids_cli.py
+++ clinica/iotools/converters/aibl_to_bids/aibl_to_bids_cli.py
@@ -30,6 +30,49 @@ class AiblToBidsCLI(ce.CmdParser):
 
     def run_command(self, args):
         from clinica.iotools.converters.aibl_to_bids.aibl_to_bids import convert_clinical_data, convert_images
+        from clinica.utils.stream import cprint
+        from colorama import Fore
+        import sys
+
+        def check_bin(bin_name):
+            """
+
+            :param bin_name: name of the executable that needs to be accessed
+            :return: status code : 0 -> executable found. 1 -> not found
+            """
+            import subprocess
+            from colorama import Fore
+
+            completed_process = subprocess.run(bin_name, shell=True)
+            returncode = completed_process.returncode
+            if returncode == 127:
+                cprint(Fore.RED + bin_name + ' not found in your system. '
+                       + 'Have you installed the corresponding software ?'
+                       + Fore.RESET)
+                res = 1
+            else:
+                res = 0
+            return res
+
+        missing_bin = 0
+        bin_to_test = ['dcm2nii', 'dcm2niix', 'mri_convert']
+        for binary in bin_to_test:
+            missing_bin = missing_bin + check_bin(binary)
+        if missing_bin > 0:
+            cprint(Fore.RED + str(missing_bin) + ' were found missing. '
+                   + 'Most important are : dcm2nii and dcm2niix.' + Fore.RESET)
+            while True:
+                cprint('Do you still want to run the converter ? (yes/no): ')
+                answer = input('')
+                if answer.lower() in ['yes', 'no']:
+                    break
+                else:
+                    cprint('Possible answers are yes or no.\n')
+            if answer.lower() == 'yes':
+                cprint('Running the pipeline anyway.')
+            if answer.lower() == 'no':
+                cprint('Exiting clinica...')
+                sys.exit()
 
         convert_images(args.dataset_directory, args.clinical_data_directory, args.bids_directory)
         convert_clinical_data(args.bids_directory, args.clinical_data_directory)
