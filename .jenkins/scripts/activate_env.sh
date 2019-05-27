7bcee955cfe9713c9321faac49830ff2f33557fe
Mauricio DIAZ <mauricio.diaz@inria.fr>
2019-05-27 19:40:27 +0200

    Edit script to verify is the conda environment exists

--- .jenkins/scripts/activate_env.sh
+++ .jenkins/scripts/activate_env.sh
@@ -12,12 +12,14 @@ ENVS=$(conda env list | awk '{print $1}' )
 
 if  [[ $ENVS = *"$CLINICA_ENV_BRANCH"* ]]
 then
-  echo "Conda environment exits, continue..."
+  echo "Conda environment exists, continue script..."
 else
   echo "Create Conda environment $CLINICA_ENV_BRANCH because it does not exist."
   conda env create --force --file environment.yml -n $CLINICA_ENV_BRANCH
 fi;
 
 # Activate conda environment
-echo "Activate conda environment $CLINICA_ENV_BRANCH..."
-source activate $CLINICA_ENV_BRANCH
+#echo "Activate conda environment $CLINICA_ENV_BRANCH..."
+#CONDA_BASE=$(conda info --base)
+#source $CONDA_BASE/etc/profile.d/conda.sh
+#conda activate $CLINICA_ENV_BRANCH
