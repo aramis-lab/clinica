647615518d52cb0107ebefd95ba892fc60409cf5
Alexandre Routier <alexandre.routier@gmail.com>
2019-08-07 11:26:44 +0200

    Add check_input_caps_files function

--- clinica/utils/io.py
+++ clinica/utils/io.py
@@ -223,3 +223,104 @@ def check_input_caps_file(list_caps_files, caps_type, pipeline_name, caps_direct
              Fore.BLUE, list_caps_files, Fore.RESET,
              Fore.BLUE, caps_directory, pipeline_to_path[pipeline_name], Fore.RESET)
         )
+
+
+def caps_type_to_path(caps_type, caps_directory, participant_id, session_id):
+    pipeline_to_path = {
+        "t1-freesurfer": "%s/subjects/%s/%s/t1/freesurfer_cross_sectional/%s_%s" %
+                         (caps_directory, participant_id, session_id, participant_id, session_id),
+        "dwi-preprocessing": "%s/subjects/%s/%s/dwi/preprocessing" %
+                             (caps_directory, participant_id, session_id),
+    }
+    dict_caps_type_to_path = {
+        # t1-freesurfer
+        "T1_FS_WM": "%s/mri/wm.seg.mgz" %
+                    pipeline_to_path["t1-freesurfer"],
+        "T1_FS_DESIKAN": "%s/mri/aparc+aseg.mgz" %
+                         pipeline_to_path["t1-freesurfer"],
+        "T1_FS_DESTRIEUX": "%s/mri/aparc.a2009s+aseg.mgz" %
+                           pipeline_to_path["t1-freesurfer"],
+        "T1_FS_BM": "%s/mri/brainmask.mgz" %
+                    pipeline_to_path["t1-freesurfer"],
+        # dwi-preprocessing
+        "DWI_PREPROC_BVAL": "%s/%s_%s*_preproc.bval" %
+                            (pipeline_to_path["dwi-preprocessing"], participant_id, session_id),
+        "DWI_PREPROC_BVEC": "%s/%s_%s*_preproc.bvec" %
+                            (pipeline_to_path["dwi-preprocessing"], participant_id, session_id),
+        "DWI_PREPROC_NII": "%s/%s_%s*_preproc.(nii|nii.gz)" %
+                           (pipeline_to_path["dwi-preprocessing"], participant_id, session_id),
+        "DWI_PREPROC_BM": "%s/%s_%s*_brainmask.(nii|nii.gz)" %
+                          (pipeline_to_path["dwi-preprocessing"], participant_id, session_id),
+    }
+    return dict_caps_type_to_path[caps_type]
+
+
+def caps_type_to_description(caps_type):
+    dict_caps_type_to_description = {
+        # t1-freesurfer
+        "T1_FS_WM": "white matter segmentation",
+        "T1_FS_DESIKAN": "Desikan parcellation",
+        "T1_FS_DESTRIEUX": "Destrieux parcellation",
+        "T1_FS_BM": "T1w brainmask",
+        "T1_FS_BE": "brain extracted T1w",
+        # dwi-preprocessing
+        "DWI_PREPROC_BVAL": "preprocessed bval",
+        "DWI_PREPROC_BVEC": "preprocessed bvec",
+        "DWI_PREPROC_NII": "preprocessed DWI",
+        "DWI_PREPROC_BM": "b0 brainmask",
+    }
+    return dict_caps_type_to_description[caps_type]
+
+
+def check_input_caps_files(list_caps_files, caps_type, pipeline_name, caps_directory, participant_ids, session_ids):
+    import re
+    import collections
+    from colorama import Fore
+
+    subject_ids = [participant_ids[i] + '_' + session_ids[i] for i in range(len(participant_ids))]
+
+    id_caps_files = [re.search(r'(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)', caps_file).group()
+                     for caps_file in list_caps_files]
+
+    id_duplicated_files = [item for item, count in collections.Counter(id_caps_files).items() if count > 1]
+
+    id_missing_files = list(set(subject_ids) - set(id_caps_files))
+
+    error_message = ""
+    if id_missing_files:
+        error_message += (
+            "\n%s[Error] Clinica could not find %s file in CAPS directory for the following subject(s):%s\n" %
+            (Fore.RED, caps_type_to_description(caps_type), Fore.RESET)
+        )
+        for id in id_missing_files:
+            participant_id = id.split('_')[0]
+            session_id = id.split('_')[1]
+            caps_path = caps_type_to_path(caps_type, caps_directory, participant_id, session_id)
+            error_message += (
+                "* %s%s%s. Expected path: %s\n" %
+                (Fore.BLUE, id.replace('_', '|'), Fore.RESET, caps_path)
+            )
+        error_message += (
+            "%s* Error explanations:\n"
+            " - Did you run the %s pipeline on this image?%s\n" %
+            (Fore.YELLOW, pipeline_name, Fore.RESET)
+        )
+
+    if id_duplicated_files:
+        error_message += (
+            "\n%s[Error] Clinica found multiple %s files in BIDS directory for the following subject(s):%s\n" %
+            (Fore.RED, caps_type_to_description(caps_type), Fore.RESET)
+        )
+        for id in id_duplicated_files:
+            error_message += (
+                "* %s%s%s. Found files: %s\n" % (
+                    Fore.BLUE, id.replace('_', '|'), Fore.RESET,
+                    (Fore.BLUE + '|' + Fore.RESET).join(s for s in list_caps_files if id in s)
+                    ))
+        error_message += (
+            "%s* Error explanations:\n"
+            " - Clinica expected to find a single file in CAPS directory for each subject.\n"
+            " - Did you duplicate files in CAPS directory?%s\n" %
+            (Fore.YELLOW, Fore.RESET)
+        )
+        return error_message
