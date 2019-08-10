10dd7b23c92ab5dd98b4ef837105ebbb26f361fa
Alexandre Routier <alexandre.routier@gmail.com>
2019-08-10 21:12:01 +0200

    Add check_input_bids_files function

--- clinica/utils/io.py
+++ clinica/utils/io.py
@@ -112,6 +112,53 @@ def check_caps_folder(caps_directory):
         )
 
 
+def bids_type_to_path(bids_type, bids_directory, participant_id, session_id):
+    dict_bids_type_to_path = {
+        # anat/
+        "T1W_NII": "%s/%s/%s/anat/%s_%s*_T1w.(nii|nii.gz)" %
+                   (bids_directory, participant_id, session_id, participant_id, session_id),
+        # dwi/
+        "DWI_BVAL": "%s/%s/%s/dwi/%s_%s*_dwi.bval" %
+                    (bids_directory, participant_id, session_id, participant_id, session_id),
+        "DWI_BVEC": "%s/%s/%s/dwi/%s_%s*_dwi.bvec" %
+                    (bids_directory, participant_id, session_id, participant_id, session_id),
+        "DWI_NII": "%s/%s/%s/dwi/%s_%s*_dwi.(nii|nii.gz)" %
+                   (bids_directory, participant_id, session_id, participant_id, session_id),
+        "DWI_JSON": "%s/%s/%s/dwi/%s_%s*_dwi.json" %
+                    (bids_directory, participant_id, session_id, participant_id, session_id),
+        # fmap/
+        "FMAP_MAGNITUDE1_NII": "%s/%s/%s/dwi/%s_%s*_magnitude1.(nii|nii.gz)" %
+                               (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_MAGNITUDE1_JSON": "%s/%s/%s/dwi/%s_%s*_magnitude1.json" %
+                                (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_MAGNITUDE2_NII": "%s/%s/%s/dwi/%s_%s*_magnitude2.(nii|nii.gz)" %
+                               (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_MAGNITUDE2_JSON": "%s/%s/%s/dwi/%s_%s*_magnitude2.json" %
+                                (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_PHASEDIFF_NII": "%s/%s/%s/dwi/%s_%s*_phasediff.(nii|nii.gz)" %
+                              (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_PHASEDIFF_JSON": "%s/%s/%s/dwi/%s_%s*_phasediff.json" %
+                               (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_PHASE1_NII": "%s/%s/%s/dwi/%s_%s*_phase1.(nii|nii.gz)" %
+                           (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_PHASE1_JSON": "%s/%s/%s/dwi/%s_%s*_phase1.json" %
+                            (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_PHASE2_NII": "%s/%s/%s/dwi/%s_%s*_phase2.(nii|nii.gz)" %
+                           (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_PHASE2_JSON": "%s/%s/%s/dwi/%s_%s*_phase2.json" %
+                            (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_MAGNITUDE_NII": "%s/%s/%s/dwi/%s_%s*_magnitude.(nii|nii.gz)" %
+                              (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_MAGNITUDE_JSON": "%s/%s/%s/dwi/%s_%s*_magnitude.json" %
+                               (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_NII": "%s/%s/%s/dwi/%s_%s*_fieldmap.(nii|nii.gz)" %
+                    (bids_directory, participant_id, session_id, participant_id, session_id),
+        "FMAP_JSON": "%s/%s/%s/dwi/%s_%s*_fieldmap.json" %
+                     (bids_directory, participant_id, session_id, participant_id, session_id),
+    }
+    return dict_bids_type_to_path[bids_type]
+
+
 def caps_type_to_path(caps_type, caps_directory, participant_id, session_id):
     pipeline_to_path = {
         "t1-freesurfer": "%s/subjects/%s/%s/t1/freesurfer_cross_sectional/%s_%s" %
@@ -144,6 +191,34 @@ def caps_type_to_path(caps_type, caps_directory, participant_id, session_id):
     return dict_caps_type_to_path[caps_type]
 
 
+def bids_type_to_description(bids_type):
+    dict_bids_type_to_description = {
+        # anat/
+        "T1W_NII": "T1 weighted image",
+        # dwi/
+        "DWI_BVAL": "bval file",
+        "DWI_BVEC": "bvec file",
+        "DWI_NII": "diffusion weighted image",
+        "DWI_JSON": "DWI JSON file",
+        # fmap/
+        "FMAP_MAGNITUDE1_NII": "magnitude (1st) image",
+        "FMAP_MAGNITUDE1_JSON": "magnitude (1st) JSON file",
+        "FMAP_MAGNITUDE2_NII": "magnitude (2st) image",
+        "FMAP_MAGNITUDE2_JSON": "magnitude (2st) JSON file",
+        "FMAP_PHASEDIFF_NII": "phase difference image",
+        "FMAP_PHASEDIFF_JSON": "phase difference JSON file",
+        "FMAP_PHASE1_NII": "phase (1st) image",
+        "FMAP_PHASE1_JSON": "phase (1st) JSON file",
+        "FMAP_PHASE2_NII": "phase (2nd) image",
+        "FMAP_PHASE2_JSON": "phase (2nd) JSON file",
+        "FMAP_MAGNITUDE_NII": "magnitude image",
+        "FMAP_MAGNITUDE_JSON": "magnitude JSON file",
+        "FMAP_NII": "fieldmap image",
+        "FMAP_JSON": "fieldmap JSON file",
+    }
+    return dict_bids_type_to_description[bids_type]
+
+
 def caps_type_to_description(caps_type):
     dict_caps_type_to_description = {
         # t1-freesurfer
@@ -159,6 +234,58 @@ def caps_type_to_description(caps_type):
         "DWI_PREPROC_BM": "b0 brainmask",
     }
     return dict_caps_type_to_description[caps_type]
+
+
+def check_input_bids_files(list_bids_files, bids_type, bids_directory, participant_ids, session_ids):
+    import collections
+    from colorama import Fore
+
+    subject_ids = [participant_ids[i] + '_' + session_ids[i] for i in range(len(participant_ids))]
+
+    id_bids_files = extract_image_ids(list_bids_files)
+
+    id_duplicated_files = [item for item, count in collections.Counter(id_bids_files).items() if count > 1]
+
+    id_missing_files = list(set(subject_ids) - set(id_bids_files))
+
+    error_message = ""
+    if id_missing_files:
+        error_message += (
+            "\n%s[Error] Clinica could not find %s file in BIDS directory for the following subject(s):%s\n" %
+            (Fore.RED, bids_type_to_description(bids_type), Fore.RESET)
+        )
+        for id_missing_file in id_missing_files:
+            participant_id = id_missing_file.split('_')[0]
+            session_id = id_missing_file.split('_')[1]
+            bids_path = bids_type_to_path(bids_type, bids_directory, participant_id, session_id)
+            error_message += (
+                "* %s%s%s. Expected path: %s\n" %
+                (Fore.BLUE, id_missing_file.replace('_', '|'), Fore.RESET, bids_path)
+            )
+        error_message += (
+            "%s* Error explanations:\n"
+            " - Did the subject(s) have the acquisition?%s\n" %
+            (Fore.YELLOW, Fore.RESET)
+        )
+
+    if id_duplicated_files:
+        error_message += (
+            "\n%s[Error] Clinica found multiple %s files in BIDS directory for the following subject(s):%s" %
+            (Fore.RED, bids_type_to_description(bids_type), Fore.RESET)
+        )
+        for id_duplicated_file in id_duplicated_files:
+            error_message += (
+                "\n* %s%s%s. Found files:\n%s" % (
+                    Fore.BLUE, id_duplicated_file.replace('_', '|'), Fore.RESET,
+                    '\n'.join('- ' + s for s in list_bids_files if id_duplicated_file in s)
+                    ))
+        error_message += (
+            "\n%s* Error explanations:\n"
+            " - Clinica expected to find a single file in BIDS directory for each subject.\n"
+            " - If you have different runs and/or acquisitions on the same folder, Clinica can not handle this situation.%s\n" %
+            (Fore.YELLOW, Fore.RESET)
+        )
+    return error_message
 
 
 def check_input_caps_files(list_caps_files, caps_type, pipeline_name, caps_directory, participant_ids, session_ids):
