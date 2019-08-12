644ee420e26004c9c8ec3aa2eecbb1b0fce6a17c
Alexandre Routier <alexandre.routier@gmail.com>
2019-08-12 15:38:16 +0200

    [WIP - t1-volume-tissue-segmentation] Add get_tpm() util

--- clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
+++ clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
@@ -4,7 +4,7 @@ import clinica.pipelines.engine as cpe
 
 __author__ = "Jorge Samper-Gonzalez"
 __copyright__ = "Copyright 2016-2019 The Aramis Lab Team"
-__credits__ = ["Jorge Samper-Gonzalez"]
+__credits__ = ["Jorge Samper-Gonzalez", "Alexandre Routier"]
 __license__ = "See LICENSE.txt file"
 __version__ = "0.1.0"
 __maintainer__ = "Jorge Samper-Gonzalez"
@@ -140,63 +140,17 @@ class T1VolumeTissueSegmentation(cpe.Pipeline):
     def build_core_nodes(self):
         """Build and connect the core nodes of the pipelines.
         """
-        import os
-        import platform
-        import nipype.interfaces.spm as spm
-        import nipype.interfaces.matlab as mlab
         import nipype.pipeline.engine as npe
         import nipype.interfaces.utility as nutil
+        import nipype.interfaces.io as nio
+        import nipype.interfaces.spm as spm
         import clinica.pipelines.t1_volume_tissue_segmentation.t1_volume_tissue_segmentation_utils as utils
         from clinica.utils.io import unzip_nii
-        import nipype.interfaces.utility as nutil
-        import nipype.pipeline.engine as npe
-        import clinica.pipelines.t1_volume_tissue_segmentation.t1_volume_tissue_segmentation_utils as utils
-        import nipype.interfaces.io as nio
-        from clinica.utils.io import fix_join
-        from clinica.utils.io import zip_nii
+        from clinica.utils.io import (fix_join, zip_nii)
+        from clinica.utils.spm import get_tpm
 
-
-
-        spm_home = os.getenv("SPM_HOME")
-        mlab_home = os.getenv("MATLABCMD")
-        mlab.MatlabCommand.set_default_matlab_cmd(mlab_home)
-        mlab.MatlabCommand.set_default_paths(spm_home)
-
-        if 'SPMSTANDALONE_HOME' in os.environ:
-            if 'MCR_HOME' in os.environ:
-                matlab_cmd = os.path.join(os.environ['SPMSTANDALONE_HOME'],
-                                          'run_spm12.sh') \
-                             + ' ' + os.environ['MCR_HOME'] \
-                             + ' script'
-                spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
-                version = spm.SPMCommand().version
-            else:
-                raise EnvironmentError('MCR_HOME variable not in environment. Although, '
-                                       + 'SPMSTANDALONE_HOME has been found')
-        else:
-            version = spm.Info.getinfo()
-
-        if version:
-            if isinstance(version, dict):
-                spm_path = version['path']
-                if version['name'] == 'SPM8':
-                    print('You are using SPM version 8. The recommended version to use with Clinica is SPM 12. '
-                          + 'Please upgrade your SPM toolbox.')
-                    tissue_map = os.path.join(spm_path, 'toolbox/Seg/TPM.nii')
-                elif version['name'] == 'SPM12':
-                    tissue_map = os.path.join(spm_path, 'tpm/TPM.nii')
-                else:
-                    raise RuntimeError('SPM version 8 or 12 could not be found. Please upgrade your SPM toolbox.')
-            if isinstance(version, str):
-                if float(version) >= 12.7169:
-                    if platform.system() == 'Darwin':
-                        tissue_map = os.path.join(str(spm_home), 'spm12.app/Contents/MacOS/spm12_mcr/spm12/spm12/tpm/TPM.nii')
-                    else:
-                        tissue_map = os.path.join(str(spm_home), 'spm12_mcr/spm/spm12/tpm/TPM.nii')
-                else:
-                    raise RuntimeError('SPM standalone version not supported. Please upgrade SPM standalone.')
-        else:
-            raise RuntimeError('SPM could not be found. Please verify your SPM_HOME environment variable.')
+        # Get TPM from SPM
+        tissue_map = get_tpm()
 
         # Get <subject_id> (e.g. sub-CLNC01_ses-M00) from input_node
         # and print begin message
