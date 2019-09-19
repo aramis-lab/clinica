6ae4575e5a5e2d8499a042139b5b7b82dcec59b7
Nono <arnaud.marcoux@inria.fr>
2019-09-19 17:09:11 +0200

    TPM.nii is located differently between SPM standalone MAC and Linux

--- clinica/pipelines/pet_surface/pet_surface_utils.py
+++ clinica/pipelines/pet_surface/pet_surface_utils.py
@@ -914,7 +914,11 @@ def get_wf(subject_id,
                            name='vol2vol_mask')
 
     if 'SPMSTANDALONE_HOME' in os.environ:
+        # Path is different between SPM standalone MAC and Linux. First is MAC location
         tpmnii = os.path.join(str(os.getenv("SPMSTANDALONE_HOME")), 'spm12_mcr/spm12/tpm/TPM.nii')
+        if not os.path.exists(tpmnii):
+            # Try Linux location
+            tpmnii = os.path.join(str(os.getenv("SPMSTANDALONE_HOME")), 'spm12_mcr/spm12/spm12/tpm/TPM.nii')
     else:
         tpmnii = os.path.join(os.path.expandvars('$SPM_HOME'), 'tpm', 'TPM.nii')
     if not os.path.exists(tpmnii):
