addbff9e242e4c367ce3ad073bd29c5acb50d689
Nono <arnaud.marcoux@inria.fr>
2019-09-24 18:51:25 +0200

    Create clinica_group_reader function

--- clinica/utils/inputs.py
+++ clinica/utils/inputs.py
@@ -1,3 +1,15 @@
+def insensitive_glob(pattern_glob):
+    """
+    This function is the glob.glob() function that is insensitive to the case
+    :param pattern_glob: sensitive-to-the-case pattern
+    :return: insensitive-to-the-case pattern
+    """
+    from glob import glob
+
+    def either(c):
+        return '[%s%s]' % (c.lower(), c.upper()) if c.isalpha() else c
+
+    return glob(''.join(map(either, pattern_glob)))
 
 
 def clinica_file_reader(subjects,
@@ -52,20 +64,9 @@ def clinica_file_reader(subjects,
 
     """
 
-    from glob import glob
     from os.path import join, isdir
     from os import listdir
     from clinica.utils.io import check_bids_folder, check_caps_folder
-
-    def insensitive_glob(pattern_glob):
-        """
-        This function is the glob.glob() function that is insensitive to the case
-        :param pattern_glob: sensitive-to-the-case pattern
-        :return: insensitive-to-the-case pattern
-        """
-        def either(c):
-            return '[%s%s]' % (c.lower(), c.upper()) if c.isalpha() else c
-        return glob(''.join(map(either, pattern_glob)))
 
     def determine_caps_or_bids(input_dir):
         """
@@ -86,9 +87,13 @@ def clinica_file_reader(subjects,
             if len([sub for sub in listdir(input_dir) if (sub.startswith('sub-') and isdir(join(input_dir, sub)))]) > 0:
                 return True
             else:
-                raise RuntimeError('Could not determine if ' + input_dir + ' is a CAPS or BIDS directory')
+                if isdir(join(input_dir, 'groups')):
+                    return False
+                else:
+                    raise RuntimeError('Could not determine if ' + input_dir + ' is a CAPS or BIDS directory')
 
     is_bids = determine_caps_or_bids(input_directory)
+
     if is_bids:
         check_bids_folder(input_directory)
     else:
@@ -100,6 +105,8 @@ def clinica_file_reader(subjects,
                               + ' directory_name/filename.extension or filename.extensionin the pattern argument'
     assert recursive_search_max >= 1, 'recursive_search_max argument must be >= 1'
     assert len(subjects) == len(sessions), 'Subjects and sessions must have the same length'
+    if len(subjects) == 0:
+        raise RuntimeError('No subjects and sessions provided.')
 
     # rez is the list containing the results
     rez = []
@@ -144,15 +151,39 @@ def clinica_file_reader(subjects,
     return rez, error_message
 
 
+def clinica_group_reader(caps_directory, pattern, recursive_search_max=10):
+    from clinica.utils.io import check_caps_folder
+    from os.path import join
+
+    # Some check on the formatting on the data
+    assert pattern[0] != '/', 'pattern argument cannot start with char : / (does not work in os.path.join function). ' \
+                              + 'If you want to indicate the exact name of the file, use the format' \
+                              + ' directory_name/filename.extension or filename.extensionin the pattern argument'
+    assert recursive_search_max >= 1, 'recursive_search_max argument must be >= 1'
+
+    check_caps_folder(caps_directory)
+
+    error_string = None
+    # This search at each level if the file is found. We stop when a result is found or when the
+    # maximum level of depth is reached
+    for level in range(recursive_search_max):
+        current_pattern = join(caps_directory, '*/' * level, pattern)
+        current_glob_found = insensitive_glob(current_pattern)
+        # print('Trying ' + str(current_pattern))
+        if len(current_glob_found) > 0:
+            break
+    if len(current_glob_found) == 0:
+        error_string = 'No file found that matches the pattern ' + pattern + ' in ' + caps_directory
+    return current_glob_found, error_string
+
+
 if __name__ == '__main__':
-    subjs = ['sub-Adni011S4105']
-    sesss = ['ses-M00']
-    bids = '/Users/arnaud.marcoux/CI/new_data/PETSurface/in/bids'
-    file = clinica_file_reader(subjs, sesss, bids, '*fdg_pet.nii*')
-    print(file)
-    caps = '/Users/arnaud.marcoux/CI/new_data/PETSurface/in/caps'
-    print(clinica_file_reader(subjs, sesss, caps, 'orig_nu.mgz'))
-    print(clinica_file_reader(subjs, sesss, bids, '*_pet.json'))
-    print(clinica_file_reader(subjs, sesss, caps, 'sub-*_ses-*/surf/rh.white'))
-    print(clinica_file_reader(subjs, sesss, caps, 'sub-*_ses-*/*/lh.aparc.a2009s.annot'))
-    print(clinica_file_reader(subjs, sesss, caps, 'sub-*_ses-*/*/lh.aparc.annot'))
+    g_id = 'UnitTest'
+    a, st = clinica_group_reader('/Users/arnaud.marcoux/CI/new_data/T1VolumeExistingTemplate/in/caps',
+                                 'group-' + g_id + '*_iteration-*_template.nii*',
+                                 recursive_search_max=10)
+    a, st = clinica_group_reader('/Users/arnaud.marcoux/CI/new_data/T1VolumeExistingTemplate/in/caps',
+                                 'group-' + g_id + '_template.nii*',
+                                 recursive_search_max=10)
+    for e in a:
+        print(e)
