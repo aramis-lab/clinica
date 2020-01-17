6b011ad2b688a43564bb1e7e5c1fa50a40a2a7b7
Alexandre Routier <alexandre.routier@gmail.com>
2020-01-17 18:08:23 +0100

    Add longitudinal_utils.py
    longitudinal_utils.py contains functions to deal with longitudinal datasets. Currently, Clinica Pipeline class and clinica/utils folder can not handle the case where we need to manipulate longitudinal IDs. When a new longitudinal pipeline will be developed into Clinica, refactoring will be needed.

--- clinica/utils/longitudinal.py
+++ clinica/utils/longitudinal.py
@@ -33,6 +33,20 @@ def get_long_id(list_session_id):
     return long_id
 
 
+def get_participants_long_id(list_participant_id, list_session_id):
+    """Extract list of longitudinal IDs from a set of participant and session IDs."""
+    from .participant import get_unique_subjects
+
+    unique_subject_list, per_subject_session_list = get_unique_subjects(list_participant_id, list_session_id)
+
+    list_long_id = []
+    for i in range(0, len(unique_subject_list)):
+        for s_id in per_subject_session_list[i]:
+            list_long_id.append(get_long_id(per_subject_session_list[i]))
+
+    return list_long_id
+
+
 def save_long_id(list_session_id, output_dir, file_name=None):
     """Save long ID to `file_name`."""
     import os
@@ -70,96 +84,3 @@ def extract_session_ids(tsv_sessions):
         )
 
     return list(ss_df.session_id)
-
-
-def grab_image_ids_from_caps_directory(caps_dir):
-    """
-    Parse CAPS directory to extract participants, sessions and longitudinal IDs.
-
-    Use case:
-
-    - CAPS
-        - subjects
-            - sub-CLNC01
-                - long-M00M18
-                - ses-M00
-                - ses-M18
-                - ses-M36
-            - sub-CLNC02
-                - ses-M00
-                - ses-M18
-
-    part_ids = ["sub-CLNC01",  "sub-CLNC01",  "sub-CLNC01" ]
-    sess_ids = ["ses-M00",     "ses-M18",     "ses-M36"    ]
-    long_ids = ["long-M00M18", "long-M00M18", "long-M00M18"]
-
-    Args:
-        caps_dir (str): Path to the CAPS directory.
-    """
-    from glob import glob
-    import os
-
-    participants_paths = glob(os.path.join(caps_dir, 'subjects', '*sub-*'))
-
-    participants_paths.sort()
-    if len(participants_paths) == 0:
-        raise IOError('Dataset empty or not CAPS compliant.')
-
-    part_ids = []
-    sess_ids = []
-    long_ids = []
-    for part_path in participants_paths:
-        part_id = part_path.split(os.sep)[-1]
-        session_paths = glob(os.path.join(part_path, '*ses-*'))
-        longitudinal_paths = glob(os.path.join(part_path, '*long-*'))
-        if len(session_paths) and len(longitudinal_paths):
-            for ses_path in session_paths:
-                sess_id = ses_path.split(os.sep)[-1]
-                for long_path in longitudinal_paths:
-                    long_id = long_path.split(os.sep)[-1]
-
-                    part_ids.append(part_id)
-                    sess_ids.append(sess_id)
-                    long_ids.append(long_id)
-
-    return part_ids, sess_ids, long_ids
-
-
-def read_participant_tsv(tsv_file):
-    """Extract participant IDs and session IDs from TSV file.
-
-    Raise:
-        ClinicaException if tsv_file is not a file
-        ClinicaException if participant_id or session_id column is missing from TSV file
-    """
-    import os
-    import pandas as pd
-    from colorama import Fore
-    from clinica.utils.exceptions import ClinicaException
-
-    if not os.path.isfile(tsv_file):
-        raise ClinicaException(
-            "\n%s[Error] The TSV file you gave is not a file.%s\n"
-            "\n%sError explanations:%s\n"
-            " - Clinica expected the following path to be a file: %s%s%s\n"
-            " - If you gave relative path, did you run Clinica on the good folder?" %
-            (Fore.RED, Fore.RESET,
-             Fore.YELLOW, Fore.RESET,
-             Fore.BLUE, tsv_file, Fore.RESET)
-        )
-    ss_df = pd.io.parsers.read_csv(tsv_file, sep='\t')
-    if 'participant_id' not in list(ss_df.columns.values):
-        raise ClinicaException(
-            "\n%s[Error] The TSV file does not contain participant_id column (path: %s)%s" %
-            (Fore.RED, tsv_file, Fore.RESET)
-        )
-    if 'session_id' not in list(ss_df.columns.values):
-        raise ClinicaException(
-            "\n%s[Error] The TSV file does not contain session_id column (path: %s)%s" %
-            (Fore.RED, tsv_file, Fore.RESET)
-        )
-    participants = list(ss_df.participant_id)
-    sessions = list(ss_df.session_id)
-
-    # Remove potential whitespace in participant_id or session_id
-    return [sub.strip(' ') for sub in participants], [ses.strip(' ') for ses in sessions]
