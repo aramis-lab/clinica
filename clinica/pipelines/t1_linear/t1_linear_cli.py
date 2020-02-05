f699466178f3c92b6303ef1985e286e01b69d35b
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-02-05 09:11:54 +0100

    Add template files for T1_linear pipeline

---
+++ clinica/pipelines/t1_linear/t1_linear_cli.py
@@ -0,0 +1,93 @@
+# coding: utf8
+
+"""T1 Linear - Clinica Command Line Interface.
+This file has been generated automatically by the `clinica generate template`
+command line tool. See here for more details:
+http://clinica.run/doc/InteractingWithClinica/
+"""
+
+
+import clinica.engine as ce
+
+
+class T1LinearCLI(ce.CmdParser):
+
+    def define_name(self):
+        """Define the sub-command name to run this pipeline.
+        """
+
+        self._name = 't1-linear'
+
+    def define_description(self):
+        """Define a description of this pipeline.
+        """
+        self._description = ('Brief description:\n'
+                             'http://clinica.run/doc/Pipelines/T1Linear/')
+
+    def define_options(self):
+        """Define the sub-command arguments
+        """
+        from clinica.engine.cmdparser import PIPELINE_CATEGORIES
+
+        # Clinica compulsory arguments (e.g. BIDS, CAPS, group_id...)
+        clinica_comp = self._args.add_argument_group(PIPELINE_CATEGORIES['CLINICA_COMPULSORY'])
+        clinica_comp.add_argument("bids_directory",
+                                  help='Path to the BIDS directory.')
+        clinica_comp.add_argument("caps_directory",
+                                  help='Path to the CAPS directory.')
+
+        # group_id can be used by certain pipelines when some operations are performed at the group level
+        # (for example, generation of a template in pipeline t1-volume)
+        # clinica_comp.add_argument("group_id",
+        #                          help='User-defined identifier for the provided group of subjects.')
+
+        # Clinica standard arguments
+        clinica_opt = self._args.add_argument_group(PIPELINE_CATEGORIES['CLINICA_OPTIONAL'])
+        clinica_opt.add_argument("-tsv", "--subjects_sessions_tsv",
+                                 help='TSV file containing a list of subjects with their sessions.')
+        clinica_opt.add_argument("-wd", "--working_directory",
+                                 help='Temporary directory to store pipelines intermediate results.')
+        clinica_opt.add_argument("-np", "--n_procs",
+                                 metavar=('N'), type=int,
+                                 help='Number of cores used to run in parallel.')
+
+        # Add your own pipeline command line arguments here to be used in the
+        # method below. Example below:
+        optional = self._args.add_argument_group(PIPELINE_CATEGORIES['OPTIONAL'])
+        optional.add_argument("-hw", "--hello_word_arg",
+                              help='Word to say hello.')
+
+        # Add advanced arguments
+        advanced = self._args.add_argument_group(PIPELINE_CATEGORIES['ADVANCED'])
+        advanced.add_argument("-aa", "--advanced_arg",
+                              help='Your advanced argument.')
+
+    def run_command(self, args):
+        """
+        """
+
+        from tempfile import mkdtemp
+        from .t1_linear_pipeline import T1Linear
+
+        # Most of the time, you will want to instantiate your pipeline with a
+        # BIDS and CAPS directory as inputs:
+        # pipeline = T1Linear(
+        #     bids_directory=self.absolute_path(args.bids_directory),
+        #     caps_directory=self.absolute_path(args.caps_directory),
+        #     tsv_file=self.absolute_path(args.subjects_sessions_tsv))
+        pipeline = T1Linear()
+        pipeline.parameters = {
+            # Add your own pipeline parameters here to use them inside your
+            # pipeline. See the file `t1_linear_pipeline.py` to
+            # see an example of use.
+            'hello_word'        : args.hello_word_arg or 'Hello'
+            'advanced_argument' : args.advanced_arg
+        }
+        if args.working_directory is None:
+            args.working_directory = mkdtemp()
+        pipeline.base_dir = self.absolute_path(args.working_directory)
+        if args.n_procs:
+            pipeline.run(plugin='MultiProc',
+                         plugin_args={'n_procs': args.n_procs})
+        else:
+            pipeline.run()
---
+++ clinica/pipelines/t1_linear/t1_linear_utils.py
@@ -0,0 +1,108 @@
+# coding: utf8
+
+"""T1 Linear - Clinica Utilities.
+This file has been generated automatically by the `clinica generate template`
+command line tool. See here for more details:
+http://clinica.run/doc/InteractingWithClinica/
+"""
+
+# Get containers to produce the CAPS structure
+# Warning!!! This file should be in the future in the utils package of Clinica
+def container_from_filename(bids_or_caps_filename):
+    """Extract container from BIDS or CAPS file.
+    Args:
+       bids_or_caps_filename (str): full path to BIDS or CAPS filename.
+    Returns:
+       Container path of the form "subjects/<participant_id>/<session_id>"
+    Examples:
+       >>> from clinica.utils.nipype import container_from_filename
+       >>> container_from_filename('/path/to/bids/sub-CLNC01/ses-M00/anat/sub-CLNC01_ses-M00_T1w.nii.gz')
+               'subjects/sub-CLNC01/ses-M00'
+       >>> container_from_filename('caps/subjects/sub-CLNC01/ses-M00/dwi/preprocessing/sub-CLNC01_ses-M00_preproc.nii')
+               'subjects/sub-CLNC01/ses-M00'
+    """
+
+    import os
+    import re
+    m = re.search(r'(sub-[a-zA-Z0-9]+)/(ses-[a-zA-Z0-9]+)', bids_or_caps_filename)
+    if m is None:
+        raise ValueError('Input filename is not in a BIDS or CAPS compliant format.'
+                'It does not contain the participant and session ID.')
+    subject = m.group(1)
+    session = m.group(2)
+    return os.path.join('subjects', subject, session)
+
+
+def get_data_datasink(image_id):
+    substitutions_ls = [  # registration
+            (image_id + '_T1w_corrected.nii.gz',
+                image_id + '_corrected_T1w.nii.gz'),
+            (image_id + 'Warped_cropped_intensity_norm.nii.gz',
+                image_id + '_space-MNI152NLin2009cSym_res-1x1x1_intensity_norm_T1w.nii.gz'),
+            (image_id + 'Warped_cropped.nii.gz',
+                image_id + '_space-MNI152NLin2009cSym_res-1x1x1_T1w.nii.gz'),
+            (image_id + 'Warped_cropped.pt',
+                image_id + '_space-MNI152NLin2009cSym_res-1x1x1_T1w.pt'),
+            (image_id + 'Warped.nii.gz',
+                image_id + '_space-MNI152NLin2009cSym_res-1x1x1_linear_registration_T1w.nii.gz')
+            ]
+    return image_id, substitutions_ls
+
+
+# Function used by the nipype interface 
+# It crops an image based on the reference
+def crop_nifti(input_img, ref_img):
+    """Crop input image based on the reference. It uses nilearn
+    `resample_to_img` function.
+    Args:
+       input_img (str): image to be processed
+       ref_img (str): template used to crop the image
+    Returns:
+       output_img (nifty image): crop image on disk.
+       crop_template: (nifty image): output template on disk.
+    """
+
+    import nibabel as nib
+    import os
+    import numpy as np
+    from nilearn.image import resample_img, crop_img, resample_to_img
+    from nibabel.spatialimages import SpatialImage
+
+    basedir = os.getcwd()
+    crop_ref = crop_img(ref_img, rtol=0.5)
+    crop_ref.to_filename(os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped_template.nii.gz'))
+    crop_template = os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped_template.nii.gz')
+
+    ## resample the individual MRI onto the cropped template image
+    crop_img = resample_to_img(input_img, crop_template)
+    crop_img.to_filename(os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped.nii.gz'))
+
+    output_img = os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped.nii.gz')
+
+    return output_img, crop_template
+
+
+def ants_histogram_intensity_normalization(crop_template, input_img, image_dimension):
+    """Histogram-based intensity normalization. It uses the `ImageMath` binary
+    with the parameter `HistogramMatch` provieded by the ANTS package.
+    This is a function to do histogram-based intensity normalization.
+    Normalize the grayscale values for a source image by matching the shape of
+    the source image histogram to a reference histogram.
+    Args:
+       crop_template (str): reference histogram obtained from this image.
+       input_img (str): source image (histogram to be normalized).
+       image_dimension (int): 2 or 3, for the image channels.
+    Returns:
+       output_img (Nifty image): image with histogram normalized.
+    """
+
+    import os
+
+    basedir = os.getcwd()
+    output_img = os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_intensity_norm.nii.gz')
+
+
+    cmd = 'ImageMath ' + str(image_dimension) + ' ' + output_img + ' HistogramMatch ' + input_img + ' ' + crop_template
+    os.system(cmd)
+
+    return output_img
