781b1edf778011806f274614c874b51cd43fc811
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-02-09 15:08:00 +0100

    Edit function to crop image in t1_linear, it uses fixed template

--- clinica/pipelines/t1_linear/t1_linear_utils.py
+++ clinica/pipelines/t1_linear/t1_linear_utils.py
@@ -51,7 +51,7 @@ def get_data_datasink(image_id):
 
 # Function used by the nipype interface 
 # It crops an image based on the reference
-def crop_nifti(input_img, ref_img):
+def crop_nifti(input_img, ref_crop):
     """Crop input image based on the reference. It uses nilearn
     `resample_to_img` function.
     Args:
@@ -69,12 +69,12 @@ def crop_nifti(input_img, ref_img):
     from nibabel.spatialimages import SpatialImage
 
     basedir = os.getcwd()
-    crop_ref = crop_img(ref_img, rtol=0.5)
-    crop_ref.to_filename(os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped_template.nii.gz'))
-    crop_template = os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped_template.nii.gz')
+    #crop_ref = crop_img(ref_img, rtol=0.5)
+    #crop_ref.to_filename(os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped_template.nii.gz'))
+    #crop_template = os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped_template.nii.gz')
 
     ## resample the individual MRI onto the cropped template image
-    crop_img = resample_to_img(input_img, crop_template)
+    crop_img = resample_to_img(input_img, ref_crop)
     crop_img.to_filename(os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped.nii.gz'))
 
     output_img = os.path.join(basedir, os.path.basename(input_img).split('.nii')[0] + '_cropped.nii.gz')
