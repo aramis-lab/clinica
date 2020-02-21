03b7e7d58a48863289d4cdaf05a1c8ab874e2bf5
Mauricio DIAZ <mauricio.diaz@inria.fr>
2020-02-21 14:32:20 +0100

    Change manual way to  add standard parameters by the method of the class

--- clinica/pipelines/t1_linear/t1_linear_cli.py
+++ clinica/pipelines/t1_linear/t1_linear_cli.py
@@ -41,15 +41,8 @@ class T1LinearCLI(ce.CmdParser):
         # clinica_comp.add_argument("group_id",
         #                          help='User-defined identifier for the provided group of subjects.')
 
-        # Clinica standard arguments
-        clinica_opt = self._args.add_argument_group(PIPELINE_CATEGORIES['CLINICA_OPTIONAL'])
-        clinica_opt.add_argument("-tsv", "--subjects_sessions_tsv",
-                                 help='TSV file containing a list of subjects with their sessions.')
-        clinica_opt.add_argument("-wd", "--working_directory",
-                                 help='Temporary directory to store pipelines intermediate results.')
-        clinica_opt.add_argument("-np", "--n_procs",
-                                 metavar=('N'), type=int,
-                                 help='Number of cores used to run in parallel.')
+        # Clinica standard arguments (e.g. --n_procs)
+        clinica_opt = self.add_clinica_standard_arguments()
 
         # Add your own pipeline command line arguments here to be used in the
         # method below. Example below:
