d5747c0e408f5acf446225ee3812a0e9f4ebd1c9
Elina Thibeau-Sutre <elina.ts@free.fr>
2021-01-12 18:29:04 +0100

    Update roi extraction

--- clinica/pipelines/deeplearning_prepare_data/roi_option.py
+++ clinica/pipelines/deeplearning_prepare_data/roi_option.py
@@ -1,6 +1,6 @@
 
 
-def extract_roi(input_path, basedir, masks_location, input_pattern, roi_list, uncrop_output):
+def extract_roi(input_path, basedir, masks_location, input_pattern, roi_list, uncrop_output, template):
     """Extracts regions of interest defined by masks
 
     This function extracts regions of interest from the preprocessed nifti image.
@@ -12,6 +12,7 @@ def extract_roi(input_path, basedir, masks_location, input_pattern, roi_list, un
         basedir: path to the extracted files.
         roi_list: list of the names of the regions that will be extracted.
         uncrop_output: if True, the final region is not cropped.
+        template: name of the template used for the preprocessing pipeline.
 
     Returns:
         file: multiple tensors saved on the disk, suffixes corresponds to
@@ -33,11 +34,11 @@ def extract_roi(input_path, basedir, masks_location, input_pattern, roi_list, un
     input_tensor_filename = os.path.basename(input_path)
     txt_idx = input_tensor_filename.rfind("_")
     it_filename_prefix = input_tensor_filename[0:txt_idx]
-    it_filename_suffix = input_tensor_filename[txt_idx:]
+    it_filename_suffix = input_tensor_filename[txt_idx:].split('.')[0]
 
     output_roi = []
     for index_roi, roi in enumerate(roi_list):
-        roi_path = path.join(masks_location, "roi-%s_%s_mask.nii.gz" % (roi, input_pattern))
+        roi_path = path.join(masks_location, "tpl-%s_%s_roi-%s_mask.nii.gz" % (template, input_pattern, roi))
         roi_mask = nib.load(roi_path).get_fdata()
         if len(roi_mask.shape) == 3:
             roi_mask = roi_mask[np.newaxis, :]
@@ -52,7 +53,7 @@ def extract_roi(input_path, basedir, masks_location, input_pattern, roi_list, un
         output_roi.append(
             os.path.join(
                 basedir,
-                "%sroi-%s%s%s" % (it_filename_prefix, roi, crop_desc, it_filename_suffix)
+                "%sroi-%s%s%s.pt" % (it_filename_prefix, roi, crop_desc, it_filename_suffix)
             )
         )
         os.makedirs(output_path, exist_ok=True)
@@ -81,6 +82,7 @@ if __name__ == "__main__":
     import argparse
     from os import path
     import os
+    import json
 
     from ...utils.inputs import clinica_file_reader
 
@@ -96,18 +98,23 @@ if __name__ == "__main__":
                         help="Disable cropping option so the output tensors have the same size than the whole image.")
     args = parser.parse_args()
 
-    cropping = "_desc-Crop"
+    template_dir = path.dirname(path.realpath(__file__))
+
+    with open(path.join(template_dir, "templates.json"), "r") as f:
+        templates_dict = json.load(f)
+
+    cropping = "desc-Crop_"
     if args.use_uncropped_image:
         cropping = ""
 
-    preprocessing = "t1_linear"
-    input_pattern = "space-MNI152NLin2009cSym%s_res-1x1x1" % cropping
+    preprocessing = "t1-linear"
+    input_pattern = "%sres-1x1x1" % cropping
 
     # Load the corresponding masks
-    masks_location = path.join(args.caps_directory, 'masks', 'roi_based', preprocessing)
+    masks_location = path.join(args.caps_directory, 'masks', 'roi_based', "tpl-%s" % templates_dict[preprocessing])
 
     if args.roi_list is None:
-        args.roi_list = [mask.split("_")[0].split("-")[1] for mask in os.listdir(masks_location)]
+        args.roi_list = [mask.split("_")[-2].split("-")[1] for mask in os.listdir(masks_location)]
         if len(args.roi_list) == 0:
             raise ValueError('No ROI was specified, and no mask was found at %s' % masks_location)
     else:
@@ -118,8 +125,9 @@ if __name__ == "__main__":
         for session in os.listdir(subject_path):
             input_path = clinica_file_reader([subject], [session], args.caps_directory,
                                              {"needed_pipeline": preprocessing,
-                                              "pattern": "*%s*" % input_pattern,
+                                              "pattern": "*%s*.nii.gz" % input_pattern,
                                               "description": "cropped output of t1-linear"})[0]
             output_path = path.join(subject_path, session, "deeplearning_prepare_data", "roi_based", preprocessing)
             extract_roi(input_path, output_path, masks_location, input_pattern,
-                        roi_list=args.roi_list, uncrop_output=args.uncrop_output)
+                        roi_list=args.roi_list, uncrop_output=args.uncrop_output,
+                        template=templates_dict[preprocessing])
