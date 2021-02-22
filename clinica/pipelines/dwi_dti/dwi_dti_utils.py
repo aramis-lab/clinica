5e21d23ed7329e24fee5635f1c73d2cf7ae5da21
Alexandre Routier <alexandre.routier@guerbet.com>
2021-02-22 11:09:05 +0100

    Migrate code style to Black

--- clinica/pipelines/dwi_dti/dwi_dti_utils.py
+++ clinica/pipelines/dwi_dti/dwi_dti_utils.py
@@ -2,8 +2,7 @@
 
 
 def erode_mask(in_mask, npass=6, nthreads=2):
-    """
-    Erode the mask.
+    """Erode the mask.
 
     This function is used for the erosion of the brain mask thus preventing
     voxels near the edge of the brain from potentially being erroneously
@@ -20,21 +19,25 @@ def erode_mask(in_mask, npass=6, nthreads=2):
     Returns:
         An eroded mask.
     """
-    import os.path as op
     import os
+    import os.path as op
 
-    assert(op.isfile(in_mask))
+    assert op.isfile(in_mask)
 
-    out_eroded_mask = op.abspath('eroded_mask.nii.gz')
-    cmd = 'maskfilter -npass %s -nthreads %s %s erode %s' \
-          % (npass, nthreads, in_mask, out_eroded_mask)
+    out_eroded_mask = op.abspath("eroded_mask.nii.gz")
+    cmd = "maskfilter -npass %s -nthreads %s %s erode %s" % (
+        npass,
+        nthreads,
+        in_mask,
+        out_eroded_mask,
+    )
     os.system(cmd)
+
     return out_eroded_mask
 
 
 def statistics_on_atlases(in_registered_map, name_map, prefix_file=None):
-    """
-    Computes a list of statistics files for each atlas.
+    """Computes a list of statistics files for each atlas.
 
     Args:
         in_registered_map (str): Map already registered on atlases.
@@ -47,13 +50,17 @@ def statistics_on_atlases(in_registered_map, name_map, prefix_file=None):
     """
     from os import getcwd
     from os.path import abspath, join
-    from nipype.utils.filemanip import split_filename
-    from clinica.utils.atlas import (AtlasAbstract, JHUDTI811mm,
-                                     JHUTracts01mm, JHUTracts251mm)
-    from clinica.utils.statistics import statistics_on_atlas
 
-    in_atlas_list = [JHUDTI811mm(),
-                     JHUTracts01mm(), JHUTracts251mm()]
+    from clinica.utils.atlas import (
+        AtlasAbstract,
+        JHUDTI811mm,
+        JHUTracts01mm,
+        JHUTracts251mm,
+    )
+    from clinica.utils.statistics import statistics_on_atlas
+    from nipype.utils.filemanip import split_filename
+
+    in_atlas_list = [JHUDTI811mm(), JHUTracts01mm(), JHUTracts251mm()]
 
     atlas_statistics_list = []
     for atlas in in_atlas_list:
@@ -62,13 +69,15 @@ def statistics_on_atlases(in_registered_map, name_map, prefix_file=None):
 
         if prefix_file is None:
             _, base, _ = split_filename(in_registered_map)
-            filename = '%s_space-%s_res-%s_map-%s_statistics.tsv' % \
-                       (base, atlas.get_name_atlas(),
-                        atlas.get_spatial_resolution(), name_map)
+            filename = (
+                f"{base}_space-{atlas.get_name_atlas()}"
+                f"_res-{atlas.get_spatial_resolution()}_map-{name_map}_statistics.tsv"
+            )
         else:
-            filename = '%s_space-%s_res-%s_map-%s_statistics.tsv' % \
-                       (prefix_file, atlas.get_name_atlas(),
-                        atlas.get_spatial_resolution(), name_map)
+            filename = (
+                f"{prefix_file}_space-{atlas.get_name_atlas()}"
+                f"_res-{atlas.get_spatial_resolution()}_map-{name_map}_statistics.tsv"
+            )
 
         out_atlas_statistics = abspath(join(getcwd(), filename))
 
@@ -79,127 +88,142 @@ def statistics_on_atlases(in_registered_map, name_map, prefix_file=None):
 
 
 def extract_bids_identifier_from_caps_filename(caps_dwi_filename):
-    """Extract BIDS identifier from CAPS filename"""
+    """Extract BIDS identifier from CAPS filename."""
     import re
 
-    m = re.search(r'(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+).*_dwi',
-                  caps_dwi_filename)
+    m = re.search(r"(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+).*_dwi", caps_dwi_filename)
 
     if m is None:
-        raise ValueError('Input filename is not in a CAPS compliant format.')
+        raise ValueError("Input filename is not in a CAPS compliant format.")
     bids_identifier = m.group(0)
 
     return bids_identifier
 
 
 def get_caps_filenames(caps_dwi_filename):
-    """
-    Prepare some filenames with CAPS naming convention
-    """
+    """Prepare some filenames with CAPS naming convention."""
     import re
 
     m = re.search(
-        r'(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+).*_dwi_space-[a-zA-Z0-9]+',
-        caps_dwi_filename)
+        r"(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+).*_dwi_space-[a-zA-Z0-9]+",
+        caps_dwi_filename,
+    )
     if m is None:
-        raise ValueError('Input filename is not in a CAPS compliant format.')
+        raise ValueError("Input filename is not in a CAPS compliant format.")
 
     caps_prefix = m.group(0)
-    bids_source = m.group(1) + '_' + m.group(2) + '_dwi'
+    bids_source = m.group(1) + "_" + m.group(2) + "_dwi"
 
-    out_dti = caps_prefix + '_model-DTI_diffmodel.nii.gz'
-    out_fa = caps_prefix + '_FA.nii.gz'
-    out_md = caps_prefix + '_MD.nii.gz'
-    out_ad = caps_prefix + '_AD.nii.gz'
-    out_rd = caps_prefix + '_RD.nii.gz'
+    out_dti = caps_prefix + "_model-DTI_diffmodel.nii.gz"
+    out_fa = caps_prefix + "_FA.nii.gz"
+    out_md = caps_prefix + "_MD.nii.gz"
+    out_ad = caps_prefix + "_AD.nii.gz"
+    out_rd = caps_prefix + "_RD.nii.gz"
 
     return bids_source, out_dti, out_fa, out_md, out_ad, out_rd
 
 
-def rename_into_caps(in_caps_dwi,
-                     in_norm_fa, in_norm_md, in_norm_ad, in_norm_rd,
-                     in_b_spline_transform, in_affine_matrix):
-    """
-    Rename different outputs of the pipelines into CAPS format.
+def rename_into_caps(
+    in_caps_dwi,
+    in_norm_fa,
+    in_norm_md,
+    in_norm_ad,
+    in_norm_rd,
+    in_b_spline_transform,
+    in_affine_matrix,
+):
+    """Rename different outputs of the pipelines into CAPS format.
 
     Returns:
         The different outputs with CAPS naming convention
     """
+    from clinica.pipelines.dwi_dti.dwi_dti_utils import (
+        extract_bids_identifier_from_caps_filename,
+    )
     from nipype.interfaces.utility import Rename
-    from clinica.pipelines.dwi_dti.dwi_dti_utils import extract_bids_identifier_from_caps_filename
 
     bids_identifier = extract_bids_identifier_from_caps_filename(in_caps_dwi)
 
     # CAPS normalized FA
     rename_fa = Rename()
     rename_fa.inputs.in_file = in_norm_fa
-    rename_fa.inputs.format_string = bids_identifier + "_space-MNI152Lin_res-1x1x1_FA.nii.gz"
+    rename_fa.inputs.format_string = (
+        bids_identifier + "_space-MNI152Lin_res-1x1x1_FA.nii.gz"
+    )
     out_caps_fa = rename_fa.run()
     # CAPS normalized MD
     rename_md = Rename()
     rename_md.inputs.in_file = in_norm_md
-    rename_md.inputs.format_string = bids_identifier + "_space-MNI152Lin_res-1x1x1_MD.nii.gz"
+    rename_md.inputs.format_string = (
+        bids_identifier + "_space-MNI152Lin_res-1x1x1_MD.nii.gz"
+    )
     out_caps_md = rename_md.run()
     # CAPS normalized AD
     rename_ad = Rename()
     rename_ad.inputs.in_file = in_norm_ad
-    rename_ad.inputs.format_string = bids_identifier + "_space-MNI152Lin_res-1x1x1_AD.nii.gz"
+    rename_ad.inputs.format_string = (
+        bids_identifier + "_space-MNI152Lin_res-1x1x1_AD.nii.gz"
+    )
     out_caps_ad = rename_ad.run()
     # CAPS normalized RD
     rename_rd = Rename()
     rename_rd.inputs.in_file = in_norm_rd
-    rename_rd.inputs.format_string = bids_identifier + "_space-MNI152Lin_res-1x1x1_RD.nii.gz"
+    rename_rd.inputs.format_string = (
+        bids_identifier + "_space-MNI152Lin_res-1x1x1_RD.nii.gz"
+    )
     out_caps_rd = rename_rd.run()
     # CAPS B-spline transform
     rename_b_spline = Rename()
     rename_b_spline.inputs.in_file = in_b_spline_transform
-    rename_b_spline.inputs.format_string = bids_identifier + "_space-MNI152Lin_res-1x1x1_deformation.nii.gz"
+    rename_b_spline.inputs.format_string = (
+        bids_identifier + "_space-MNI152Lin_res-1x1x1_deformation.nii.gz"
+    )
     out_caps_b_spline_transform = rename_b_spline.run()
     # CAPS Affine matrix
     rename_affine = Rename()
     rename_affine.inputs.in_file = in_affine_matrix
-    rename_affine.inputs.format_string = bids_identifier + "_space-MNI152Lin_res-1x1x1_affine.mat"
+    rename_affine.inputs.format_string = (
+        bids_identifier + "_space-MNI152Lin_res-1x1x1_affine.mat"
+    )
     out_caps_affine_matrix = rename_affine.run()
 
-    return out_caps_fa.outputs.out_file, out_caps_md.outputs.out_file,\
-        out_caps_ad.outputs.out_file, out_caps_rd.outputs.out_file, \
-        out_caps_b_spline_transform.outputs.out_file, \
-        out_caps_affine_matrix.outputs.out_file
+    return (
+        out_caps_fa.outputs.out_file,
+        out_caps_md.outputs.out_file,
+        out_caps_ad.outputs.out_file,
+        out_caps_rd.outputs.out_file,
+        out_caps_b_spline_transform.outputs.out_file,
+        out_caps_affine_matrix.outputs.out_file,
+    )
 
 
 def print_begin_pipeline(in_bids_or_caps_file):
-    """
-    """
-    from clinica.utils.stream import cprint
-    import re
     import datetime
+    import re
+
+    from clinica.utils.stream import cprint
     from colorama import Fore
 
-    m = re.search(r'(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)',
-                  in_bids_or_caps_file)
+    m = re.search(r"(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)", in_bids_or_caps_file)
     if m is None:
-        raise ValueError(
-            'Input filename is not in a BIDS or CAPS compliant format.')
-    now = datetime.datetime.now().strftime('%H:%M:%S')
+        raise ValueError("Input filename is not in a BIDS or CAPS compliant format.")
+    now = datetime.datetime.now().strftime("%H:%M:%S")
 
-    cprint('%s[%s]%s Running pipeline for %s...' % (
-        Fore.BLUE, now, Fore.RESET, m.group(0)))
+    cprint(
+        "%s[%s]%s Running pipeline for %s..." % (Fore.BLUE, now, Fore.RESET, m.group(0))
+    )
 
 
 def print_end_pipeline(in_bids_or_caps_file, final_file_1, final_file_2):
-    """
-    """
-    from clinica.utils.stream import cprint
-    import re
     import datetime
+    import re
+
+    from clinica.utils.stream import cprint
     from colorama import Fore
 
-    m = re.search(r'(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)',
-                  in_bids_or_caps_file)
+    m = re.search(r"(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)", in_bids_or_caps_file)
     if m is None:
-        raise ValueError(
-            'Input filename is not in a BIDS or CAPS compliant format.')
-    now = datetime.datetime.now().strftime('%H:%M:%S')
+        raise ValueError("Input filename is not in a BIDS or CAPS compliant format.")
+    now = datetime.datetime.now().strftime("%H:%M:%S")
 
-    cprint('%s[%s]%s ...%s has completed.' % (
-        Fore.GREEN, now, Fore.RESET, m.group(0)))
+    cprint("%s[%s]%s ...%s has completed." % (Fore.GREEN, now, Fore.RESET, m.group(0)))
--- clinica/pipelines/dwi_dti/dwi_dti_workflows.py
+++ clinica/pipelines/dwi_dti/dwi_dti_workflows.py
@@ -2,11 +2,9 @@
 
 
 def register_dti_maps_on_atlas(
-        working_directory=None,
-        name="register_dti_maps_on_atlas"):
-    """
-    Register FA-map on a subject towards a FA atlas and apply the estimated
-    deformation to MD, AD & RD.
+    working_directory=None, name="register_dti_maps_on_atlas"
+):
+    """Register FA-map on a subject towards a FA atlas and apply the estimated deformation to MD, AD & RD.
 
     This pipelines performs the analysis of tracts using a white matter atlas
     and computes mean value of the scalar on each tracts of this atlas. The
@@ -40,13 +38,16 @@ def register_dti_maps_on_atlas(
     """
     import os
     import tempfile
+
     import nipype.interfaces.fsl as fsl
     import nipype.interfaces.utility as niu
     import nipype.pipeline.engine as pe
-    from nipype.interfaces.ants import RegistrationSynQuick
     from clinica.utils.atlas import AtlasAbstract, JHUDTI811mm
-    from clinica.utils.mri_registration import apply_ants_registration_syn_quick_transformation
     from clinica.utils.check_dependency import check_environment_variable
+    from clinica.utils.mri_registration import (
+        apply_ants_registration_syn_quick_transformation,
+    )
+    from nipype.interfaces.ants import RegistrationSynQuick
 
     atlas = JHUDTI811mm()
 
@@ -56,91 +57,209 @@ def register_dti_maps_on_atlas(
     if working_directory is None:
         working_directory = tempfile.mkdtemp()
 
-    inputnode = pe.Node(niu.IdentityInterface(
-        fields=['in_fa', 'in_md', 'in_ad', 'in_rd', 'in_atlas_scalar_image']),
-        name='inputnode')
-    fsl_dir = check_environment_variable('FSLDIR', 'FSL')
-    fa_map = os.path.join(fsl_dir, 'data', 'atlases', 'JHU', 'JHU-ICBM-FA-1mm.nii.gz')
+    inputnode = pe.Node(
+        niu.IdentityInterface(
+            fields=["in_fa", "in_md", "in_ad", "in_rd", "in_atlas_scalar_image"]
+        ),
+        name="inputnode",
+    )
+    fsl_dir = check_environment_variable("FSLDIR", "FSL")
+    fa_map = os.path.join(fsl_dir, "data", "atlases", "JHU", "JHU-ICBM-FA-1mm.nii.gz")
     inputnode.inputs.in_atlas_scalar_image = fa_map
 
-    register_fa = pe.Node(
-        interface=RegistrationSynQuick(),
-        name='register_fa')
+    register_fa = pe.Node(interface=RegistrationSynQuick(), name="register_fa")
 
-    apply_ants_registration_for_md = pe.Node(interface=niu.Function(
-        input_names=['in_image', 'in_reference_image',
-                     'in_affine_transformation', 'in_bspline_transformation',
-                     'name_output_image'],
-        output_names=['out_deformed_image'],
-        function=apply_ants_registration_syn_quick_transformation),
-        name='apply_ants_registration_for_md')
-    apply_ants_registration_for_md.inputs.name_output_image = \
-        'space-' + atlas.get_name_atlas() + '_res-' + atlas.get_spatial_resolution() + '_MD.nii.gz'
-    apply_ants_registration_for_ad = pe.Node(interface=niu.Function(
-        input_names=['in_image', 'in_reference_image',
-                     'in_affine_transformation', 'in_bspline_transformation',
-                     'name_output_image'],
-        output_names=['out_deformed_image'],
-        function=apply_ants_registration_syn_quick_transformation),
-        name='apply_ants_registration_for_ad')
-    apply_ants_registration_for_ad.inputs.name_output_image = \
-        'space-' + atlas.get_name_atlas() + '_res-' + atlas.get_spatial_resolution() + '_AD.nii.gz'
-    apply_ants_registration_for_rd = pe.Node(interface=niu.Function(
-        input_names=['in_image', 'in_reference_image',
-                     'in_affine_transformation', 'in_bspline_transformation',
-                     'name_output_image'],
-        output_names=['out_deformed_image'],
-        function=apply_ants_registration_syn_quick_transformation),
-        name='apply_ants_registration_for_rd')
-    apply_ants_registration_for_rd.inputs.name_output_image = \
-        'space-' + atlas.get_name_atlas() + '_res-' + atlas.get_spatial_resolution() + '_RD.nii.gz'
+    apply_ants_registration_for_md = pe.Node(
+        interface=niu.Function(
+            input_names=[
+                "in_image",
+                "in_reference_image",
+                "in_affine_transformation",
+                "in_bspline_transformation",
+                "name_output_image",
+            ],
+            output_names=["out_deformed_image"],
+            function=apply_ants_registration_syn_quick_transformation,
+        ),
+        name="apply_ants_registration_for_md",
+    )
+    apply_ants_registration_for_md.inputs.name_output_image = (
+        "space-"
+        + atlas.get_name_atlas()
+        + "_res-"
+        + atlas.get_spatial_resolution()
+        + "_MD.nii.gz"
+    )
+    apply_ants_registration_for_ad = pe.Node(
+        interface=niu.Function(
+            input_names=[
+                "in_image",
+                "in_reference_image",
+                "in_affine_transformation",
+                "in_bspline_transformation",
+                "name_output_image",
+            ],
+            output_names=["out_deformed_image"],
+            function=apply_ants_registration_syn_quick_transformation,
+        ),
+        name="apply_ants_registration_for_ad",
+    )
+    apply_ants_registration_for_ad.inputs.name_output_image = (
+        "space-"
+        + atlas.get_name_atlas()
+        + "_res-"
+        + atlas.get_spatial_resolution()
+        + "_AD.nii.gz"
+    )
+    apply_ants_registration_for_rd = pe.Node(
+        interface=niu.Function(
+            input_names=[
+                "in_image",
+                "in_reference_image",
+                "in_affine_transformation",
+                "in_bspline_transformation",
+                "name_output_image",
+            ],
+            output_names=["out_deformed_image"],
+            function=apply_ants_registration_syn_quick_transformation,
+        ),
+        name="apply_ants_registration_for_rd",
+    )
+    apply_ants_registration_for_rd.inputs.name_output_image = (
+        "space-"
+        + atlas.get_name_atlas()
+        + "_res-"
+        + atlas.get_spatial_resolution()
+        + "_RD.nii.gz"
+    )
 
-    thres_map = pe.Node(fsl.Threshold(thresh=0.0),
-                        iterfield=['in_file'],
-                        name='RemoveNegative')
-    thres_fa = thres_map.clone('RemoveNegative_FA')
-    thres_md = thres_map.clone('RemoveNegative_MD')
-    thres_ad = thres_map.clone('RemoveNegative_AD')
-    thres_rd = thres_map.clone('RemoveNegative_RD')
+    thres_map = pe.Node(
+        fsl.Threshold(thresh=0.0), iterfield=["in_file"], name="RemoveNegative"
+    )
+    thres_fa = thres_map.clone("RemoveNegative_FA")
+    thres_md = thres_map.clone("RemoveNegative_MD")
+    thres_ad = thres_map.clone("RemoveNegative_AD")
+    thres_rd = thres_map.clone("RemoveNegative_RD")
 
-    outputnode = pe.Node(niu.IdentityInterface(
-        fields=['out_norm_fa', 'out_norm_md', 'out_norm_ad', 'out_norm_rd',
-                'out_affine_matrix', 'out_b_spline_transform']),
-        name='outputnode')
+    outputnode = pe.Node(
+        niu.IdentityInterface(
+            fields=[
+                "out_norm_fa",
+                "out_norm_md",
+                "out_norm_ad",
+                "out_norm_rd",
+                "out_affine_matrix",
+                "out_b_spline_transform",
+            ]
+        ),
+        name="outputnode",
+    )
 
     wf = pe.Workflow(name=name, base_dir=working_directory)
-    wf.connect([
-        # Registration of FA-map onto the atlas:
-        (inputnode, register_fa, [('in_fa',                 'moving_image'),  # noqa
-                                  ('in_atlas_scalar_image', 'fixed_image')]),  # noqa
-        # Apply deformation field on MD, AD & RD:
-        (inputnode,   apply_ants_registration_for_md, [('in_md',                 'in_image')]),  # noqa
-        (inputnode,   apply_ants_registration_for_md, [('in_atlas_scalar_image', 'in_reference_image')]),  # noqa
-        (register_fa, apply_ants_registration_for_md, [('out_matrix',            'in_affine_transformation')]),  # noqa
-        (register_fa, apply_ants_registration_for_md, [('forward_warp_field',    'in_bspline_transformation')]),  # noqa
-
-        (inputnode,   apply_ants_registration_for_ad, [('in_ad',                 'in_image')]),  # noqa
-        (inputnode,   apply_ants_registration_for_ad, [('in_atlas_scalar_image', 'in_reference_image')]),  # noqa
-        (register_fa, apply_ants_registration_for_ad, [('out_matrix',            'in_affine_transformation')]),  # noqa
-        (register_fa, apply_ants_registration_for_ad, [('forward_warp_field',    'in_bspline_transformation')]),  # noqa
-
-        (inputnode,   apply_ants_registration_for_rd, [('in_rd',                 'in_image')]),  # noqa
-        (inputnode,   apply_ants_registration_for_rd, [('in_atlas_scalar_image', 'in_reference_image')]),  # noqa
-        (register_fa, apply_ants_registration_for_rd, [('out_matrix',            'in_affine_transformation')]),  # noqa
-        (register_fa, apply_ants_registration_for_rd, [('forward_warp_field',    'in_bspline_transformation')]),  # noqa
-        # Remove negative values from the DTI maps:
-        (register_fa,                    thres_fa, [('warped_image',       'in_file')]),  # noqa
-        (apply_ants_registration_for_md, thres_md, [('out_deformed_image', 'in_file')]),  # noqa
-        (apply_ants_registration_for_rd, thres_rd, [('out_deformed_image', 'in_file')]),  # noqa
-        (apply_ants_registration_for_ad, thres_ad, [('out_deformed_image', 'in_file')]),  # noqa
-        # Outputnode:
-        (thres_fa,    outputnode, [('out_file',           'out_norm_fa')]),  # noqa
-        (register_fa, outputnode, [('out_matrix',         'out_affine_matrix'),  # noqa
-                                   ('forward_warp_field', 'out_b_spline_transform'),  # noqa
-                                   ('inverse_warp_field', 'out_inverse_warp')]),  # noqa
-        (thres_md,    outputnode,  [('out_file',          'out_norm_md')]),  # noqa
-        (thres_ad,    outputnode,  [('out_file',          'out_norm_ad')]),  # noqa
-        (thres_rd,    outputnode,  [('out_file',          'out_norm_rd')])   # noqa
-    ])
+    wf.connect(
+        [
+            # Registration of FA-map onto the atlas:
+            (
+                inputnode,
+                register_fa,
+                [
+                    ("in_fa", "moving_image"),
+                    ("in_atlas_scalar_image", "fixed_image"),
+                ],
+            ),
+            # Apply deformation field on MD, AD & RD:
+            (
+                inputnode,
+                apply_ants_registration_for_md,
+                [("in_md", "in_image")],
+            ),
+            (
+                inputnode,
+                apply_ants_registration_for_md,
+                [("in_atlas_scalar_image", "in_reference_image")],
+            ),
+            (
+                register_fa,
+                apply_ants_registration_for_md,
+                [("out_matrix", "in_affine_transformation")],
+            ),
+            (
+                register_fa,
+                apply_ants_registration_for_md,
+                [("forward_warp_field", "in_bspline_transformation")],
+            ),
+            (
+                inputnode,
+                apply_ants_registration_for_ad,
+                [("in_ad", "in_image")],
+            ),
+            (
+                inputnode,
+                apply_ants_registration_for_ad,
+                [("in_atlas_scalar_image", "in_reference_image")],
+            ),
+            (
+                register_fa,
+                apply_ants_registration_for_ad,
+                [("out_matrix", "in_affine_transformation")],
+            ),
+            (
+                register_fa,
+                apply_ants_registration_for_ad,
+                [("forward_warp_field", "in_bspline_transformation")],
+            ),
+            (
+                inputnode,
+                apply_ants_registration_for_rd,
+                [("in_rd", "in_image")],
+            ),
+            (
+                inputnode,
+                apply_ants_registration_for_rd,
+                [("in_atlas_scalar_image", "in_reference_image")],
+            ),
+            (
+                register_fa,
+                apply_ants_registration_for_rd,
+                [("out_matrix", "in_affine_transformation")],
+            ),
+            (
+                register_fa,
+                apply_ants_registration_for_rd,
+                [("forward_warp_field", "in_bspline_transformation")],
+            ),
+            # Remove negative values from the DTI maps:
+            (register_fa, thres_fa, [("warped_image", "in_file")]),
+            (
+                apply_ants_registration_for_md,
+                thres_md,
+                [("out_deformed_image", "in_file")],
+            ),
+            (
+                apply_ants_registration_for_rd,
+                thres_rd,
+                [("out_deformed_image", "in_file")],
+            ),
+            (
+                apply_ants_registration_for_ad,
+                thres_ad,
+                [("out_deformed_image", "in_file")],
+            ),
+            # Outputnode:
+            (thres_fa, outputnode, [("out_file", "out_norm_fa")]),
+            (
+                register_fa,
+                outputnode,
+                [
+                    ("out_matrix", "out_affine_matrix"),
+                    ("forward_warp_field", "out_b_spline_transform"),
+                    ("inverse_warp_field", "out_inverse_warp"),
+                ],
+            ),
+            (thres_md, outputnode, [("out_file", "out_norm_md")]),
+            (thres_ad, outputnode, [("out_file", "out_norm_ad")]),
+            (thres_rd, outputnode, [("out_file", "out_norm_rd")]),
+        ]
+    )
 
     return wf
