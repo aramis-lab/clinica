1b48f6e6f9712e004e65299244b0fed647f7a0dc
ravih18 <rhassana96@gmail.com>
2021-03-04 11:28:58 +0100

    Removed visualizer from repository

--- clinica/pipelines/pet_linear/pet_linear_visualizer.py
+++
@@ -1,7 +0,0 @@
-# coding: utf8
-
-"""pet_linear - Clinica Visualizer.
-This file has been generated automatically by the `clinica generate template`
-command line tool. See here for more details:
-http://clinica.run/doc/InteractingWithClinica/
-"""
