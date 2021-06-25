7dc04245b1459956d1b63e10d1b0d45d6dff3d82
Ghislain Antony Vaillant <ghislain.vaillant@icm-institute.org>
2021-06-25 11:40:13 +0200

    Fix docstring formatting

--- clinica/iotools/bids_utils.py
+++ clinica/iotools/bids_utils.py
@@ -17,8 +17,7 @@ def create_participants_df(
     Args:
         study_name: name of the study (Ex. ADNI)
         clinical_spec_path: path to the clinical file
-        clinical_data_dir: path to the directory where the clinical data are
-        stored
+        clinical_data_dir: path to the directory where the clinical data are stored
         bids_ids: list of bids ids
         delete_non_bids_info: if True delete all the rows of the subjects that
         are not available in the BIDS dataset
