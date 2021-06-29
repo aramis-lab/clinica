3db2544bc5eaa030285e47082355f97d933c645d
omar.elrifai <omar.void@gmail.com>
2021-06-29 11:59:40 +0200

    Add merge_tsv() required target var in CLI

--- clinica/iotools/utils/cli.py
+++ clinica/iotools/utils/cli.py
@@ -175,7 +175,8 @@ def create_subjects_visits(bids_directory: str, output_tsv: str) -> None:
 )
 @click.option(
     "-p",
-    "--pipelines",
+    "--pipeline",
+    "pipelines",
     multiple=True,
     type=click.Choice(["t1-freesurfer", "t1-volume", "pet-volume"]),
     help="Pipeline to merge to the ouput TSV file. All pipelines are merged by default.",
