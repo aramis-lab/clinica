53cba14015e69d20b8ae3cf5e0341d9294235308
JOULOT Matthieu <matthieu.joulot@umr-colli-mp036.icm-institute.org>
2021-07-05 09:57:12 +0200

    Change the position of the functions compute_status for ptau and amyloid within write_adni_sessions_tsv so that they wouldn't be referenced before assignment in enclosing scope

--- clinica/iotools/converters/adni_to_bids/adni_utils.py
+++ clinica/iotools/converters/adni_to_bids/adni_utils.py
@@ -472,6 +472,34 @@ def write_adni_sessions_tsv(df_subj_sessions, bids_subjs_paths):
     import os
     from os import path
 
+    def compute_amyloid_status(row, tau_status=False):
+        stat = ""
+        if (
+            pd.isnull(row["adni_av45"])
+            and pd.isnull(row["adni_pib"])
+            and isinstance(row["adni_abeta"], float)
+        ):
+            stat = "Au"
+        elif row["adni_av45"] > 1.1 or row["adni_pib"] > 1.5 or row["adni_abeta"] < 192:
+            stat = "A+"
+        elif (
+            (pd.isnull(row["adni_av45"]) or row["adni_av45"] < 1.1)
+            and (pd.isnull(row["adni_pib"]) or row["adni_pib"] < 1.5)
+            and (isinstance(row["adni_abeta"], float) or row["adni_abeta"] > 192)
+        ):
+            stat = "A-"
+        return stat
+
+    def compute_ptau_status(row):
+        stat = ""
+        if pd.isnull(row["adni_ptau"]):
+            stat = "Tu"
+        elif row["adni_ptau"] > 23:
+            stat = "T+"
+        else:
+            stat = "T-"
+        return stat
+
     df_subj_sessions["adas_memory"] = (
         df_subj_sessions["adas_Q1"]
         + df_subj_sessions["adas_Q4"]
@@ -523,34 +551,6 @@ def write_adni_sessions_tsv(df_subj_sessions, bids_subjs_paths):
                 index=False,
                 encoding="utf-8",
             )
-
-    def compute_amyloid_status(row, tau_status=False):
-        stat = ""
-        if (
-            pd.isnull(row["adni_av45"])
-            and pd.isnull(row["adni_pib"])
-            and isinstance(row["adni_abeta"], float)
-        ):
-            stat = "Au"
-        elif row["adni_av45"] > 1.1 or row["adni_pib"] > 1.5 or row["adni_abeta"] < 192:
-            stat = "A+"
-        elif (
-            (pd.isnull(row["adni_av45"]) or row["adni_av45"] < 1.1)
-            and (pd.isnull(row["adni_pib"]) or row["adni_pib"] < 1.5)
-            and (isinstance(row["adni_abeta"], float) or row["adni_abeta"] > 192)
-        ):
-            stat = "A-"
-        return stat
-
-    def compute_ptau_status(row):
-        stat = ""
-        if pd.isnull(row["adni_ptau"]):
-            stat = "Tu"
-        elif row["adni_ptau"] > 23:
-            stat = "T+"
-        else:
-            stat = "T-"
-        return stat
 
 
 def remove_fields_duplicated(bids_fields):
