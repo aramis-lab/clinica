0c47da683c66da594824a1fad4fcc42321d9af12
omar-rifai <omar.void@gmail.com>
2021-07-22 18:40:25 +0200

    Hide the OASIS3 converter CLI for the release (#340)

--- clinica/iotools/converters/cli.py
+++ clinica/iotools/converters/cli.py
@@ -17,7 +17,7 @@ cli.add_command(adni_to_bids_cli.cli)
 cli.add_command(aibl_to_bids_cli.cli)
 cli.add_command(nifd_to_bids_cli.cli)
 cli.add_command(oasis_to_bids_cli.cli)
-cli.add_command(oasis3_to_bids_cli.cli)
+# cli.add_command(oasis3_to_bids_cli.cli)
 
 
 if __name__ == "__main__":
