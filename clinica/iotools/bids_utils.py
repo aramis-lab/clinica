4688f9e4ca3756624553c56e497d1cf6059f01f6
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2021-08-05 18:05:20 +0200

    Print the dcm2niix command if the return code is different from 0

--- clinica/iotools/bids_utils.py
+++ clinica/iotools/bids_utils.py
@@ -774,6 +774,7 @@ def run_dcm2niix(command):
         cprint(
             msg=(
                 "DICOM to BIDS conversion with dcm2niix failed:\n"
+                f"command: {command}\n"
                 f"{output_dcm2niix.stdout.decode('utf-8')}"
             ),
             lvl="warning",
