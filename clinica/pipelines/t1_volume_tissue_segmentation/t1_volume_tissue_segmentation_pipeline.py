fea11c249d31838f30ec4293b818dd33fc743c18
Alexandre Routier <alexandreroutier@users.noreply.github.com>
2021-08-06 10:57:21 +0200

    Use Python dict setdefault() (#376)
    * Use Python dict setdefault()
    
    * Remove duplicated tissue_probability_maps key

--- clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
+++ clinica/pipelines/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
@@ -24,20 +24,11 @@ class T1VolumeTissueSegmentation(cpe.Pipeline):
         """Check pipeline parameters."""
         from clinica.utils.spm import get_tpm
 
-        if "tissue_classes" not in self.parameters.keys():
-            self.parameters["tissue_classes"] = [1, 2, 3]
-        if "dartel_tissues" not in self.parameters.keys():
-            self.parameters["dartel_tissues"] = [1, 2, 3]
-        if "tissue_probability_maps" not in self.parameters.keys():
-            self.parameters["tissue_probability_maps"] = None
-        if "save_warped_unmodulated" not in self.parameters.keys():
-            self.parameters["save_warped_unmodulated"] = True
-        if "save_warped_modulated" not in self.parameters.keys():
-            self.parameters["save_warped_modulated"] = False
-
-        # Get Tissue Probability Map from SPM
-        if not self.parameters["tissue_probability_maps"]:
-            self.parameters["tissue_probability_maps"] = get_tpm()
+        self.parameters.setdefault("tissue_classes", [1, 2, 3])
+        self.parameters.setdefault("dartel_tissues", [1, 2, 3])
+        self.parameters.setdefault("save_warped_unmodulated", True)
+        self.parameters.setdefault("save_warped_modulated", False)
+        self.parameters.setdefault("tissue_probability_maps", get_tpm())
 
     def get_input_fields(self):
         """Specify the list of possible inputs of this pipeline.
