a52ba2f517913b66721d11cfc1e591d2c9c69f0c
Mauricio DIAZ <mauricio.diaz@inria.fr>
2021-09-03 14:47:27 +0200

    Edit instantation test to use PathLib

--- test/conftest.py
+++ test/conftest.py
@@ -6,10 +6,10 @@
 """
 
 __author__ = "Mauricio Diaz"
-__copyright__ = "Copyright 2016-2019 The Aramis Lab Team"
+__copyright__ = "Copyright 2016-2021 The Aramis Lab Team"
 __credits__ = ["Mauricio Diaz"]
 __license__ = "See LICENSE.txt file"
-__version__ = "0.2.0"
+__version__ = "0.5.1"
 __maintainer__ = "Mauricio Diaz"
 __email__ = "mauricio.diaz@inria.fr"
 __status__ = "Development"
@@ -20,10 +20,18 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
+        "--input_data_directory",
+        action="store",
+        help="Directory for (only-read) inputs for tests",
+    )
+    parser.addoption(
         "--working_directory", action="store", help="Working directory for tests"
     )
 
 
 @pytest.fixture
 def cmdopt(request):
-    return request.config.getoption("--working_directory")
+    config_param = {}
+    config_param["input"] = request.config.getoption("--input_data_directory")
+    config_param["wd"] = request.config.getoption("--working_directory")
+    return config_param
