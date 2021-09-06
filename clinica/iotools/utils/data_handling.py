b5192f530b4eda8201383e013c695d2838023aec
omar.elrifai <omar.void@gmail.com>
2021-09-06 18:17:00 +0200

    Change behavior of checks to handle empty panda dataframes

--- clinica/iotools/utils/data_handling.py
+++ clinica/iotools/utils/data_handling.py
@@ -191,10 +191,10 @@ def create_merge_file(
         if not pipelines:
             for pipeline_name, pipeline_fn in pipeline_options.items():
                 merged_df, summary_df = pipeline_fn(caps_dir, merged_df, **kwargs)
-                if summary_df and not summary_df.empty:
+                if summary_df is not None and not summary_df.empty:
                     merged_summary_df = pd.concat([merged_summary_df, summary_df])
 
-                if not summary_df or len(summary_df) == 0:
+                if summary_df is None or len(summary_df) == 0:
                     cprint(
                         f"{pipeline_name} outputs were not found in the CAPS folder."
                     )
