2c405d0f89b704825fc0be9a7cd8358b359e0e46
Ghislain Antony Vaillant <ghisvail@users.noreply.github.com>
2021-09-14 15:40:39 +0200

    Fix unexpected argument in non-regression tests (#414)

--- test/nonregression/pipelines/test_run_pipelines_anat.py
+++ test/nonregression/pipelines/test_run_pipelines_anat.py
@@ -443,7 +443,7 @@ def test_run_T1Linear(cmdopt):
     out_folder = join(root, "out")
     ref_folder = join(root, "ref")
 
-    compare_folders(out_folder, ref_folder, shared_folder_name="caps")
+    compare_folders(out_folder, ref_folder, tmp_path="caps")
 
     clean_folder(join(root, "out", "caps"), recreate=False)
     clean_folder(join(working_dir, "T1Linear"), recreate=False)
