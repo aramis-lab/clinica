2203d4ed6b1a6db40a869d8828448740f56246bd
Alexandre Routier <alexandre.routier@guerbet.com>
2021-10-11 20:51:39 +0200

    Remove t1_linear_visualizer.py

--- clinica/pipelines/t1_linear/t1_linear_visualizer.py
+++
@@ -1,7 +0,0 @@
-# coding: utf8
-
-"""T1 Linear - Clinica Visualizer.
-This file has been generated automatically by the `clinica generate template`
-command line tool. See here for more details:
-https://aramislab.paris.inria.fr/clinica/docs/public/latest/InteractingWithClinica/
-"""
