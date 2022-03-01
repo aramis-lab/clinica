9cdcf8381ed0d1b6ae2fb218049d9d268611ec27
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2022-03-01 17:48:14 +0100

    Add ukb  converter to the cli

--- clinica/iotools/converters/cli.py
+++ clinica/iotools/converters/cli.py
@@ -6,6 +6,7 @@ from .habs_to_bids import habs_to_bids_cli
 from .nifd_to_bids import nifd_to_bids_cli
 from .oasis3_to_bids import oasis3_to_bids_cli
 from .oasis_to_bids import oasis_to_bids_cli
+from .ukb_to_bids import ukb_to_bids_cli
 
 
 @click.group("convert")
@@ -20,6 +21,7 @@ cli.add_command(habs_to_bids_cli.cli)
 cli.add_command(nifd_to_bids_cli.cli)
 cli.add_command(oasis_to_bids_cli.cli)
 cli.add_command(oasis3_to_bids_cli.cli)
+cli.add_command(ukb_to_bids_cli.cli)
 
 if __name__ == "__main__":
     cli()
