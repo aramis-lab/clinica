9cb4596ced211d4fe4906ead4945e5338f732021
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2022-03-01 17:38:36 +0100

    Initial creation of the UKB Converter. Based on OASIS3 converter.

---
+++ clinica/iotools/converters/ukb_to_bids/ukb_to_bids.py
@@ -0,0 +1,46 @@
+"""Convert the UKB dataset into BIDS."""
+
+from os import PathLike
+from typing import List
+
+
+def convert_images(
+    path_to_dataset: PathLike,
+    bids_dir: PathLike,
+    path_to_clinical: PathLike,
+) -> List[PathLike]:
+    """Convert the entire dataset in BIDS.
+
+    Scans available files in the path_to_dataset,
+    identifies the patients that have images described by the JSON file,
+    converts the image with the highest quality for each category.
+    """
+    from .ukb_utils import (
+        dataset_to_bids,
+        intersect_data,
+        read_clinical_data,
+        read_imaging_data,
+        write_bids,
+    )
+
+    # read the clinical data files
+    dict_df = read_clinical_data(path_to_clinical)
+
+    # makes a df of the imaging data
+    imaging_data = read_imaging_data(path_to_dataset)
+
+    # intersect the data
+    imaging_data, df_small = intersect_data(imaging_data, dict_df)
+
+    # build the tsv
+    participants, sessions, scans = dataset_to_bids(imaging_data, df_small)
+
+    written = write_bids(
+        to=bids_dir,
+        participants=participants,
+        sessions=sessions,
+        scans=scans,
+        dataset_directory=path_to_dataset,
+    )
+
+    return written
---
+++ clinica/iotools/converters/ukb_to_bids/ukb_to_bids_cli.py
@@ -0,0 +1,34 @@
+from os import PathLike
+
+import click
+
+from clinica.iotools.converters import cli_param
+
+
+@click.command(name="ukb-to-bids")
+@cli_param.dataset_directory
+@cli_param.clinical_data_directory
+@cli_param.bids_directory
+def cli(
+    dataset_directory: PathLike,
+    clinical_data_directory: PathLike,
+    bids_directory: PathLike,
+) -> None:
+    """UKB to BIDS converter.
+
+    Convert the imaging and clinical data of UKB, located in DATASET_DIRECTORY and
+    CLINICAL_DATA_DIRECTORY respectively, to a BIDS dataset in the target BIDS_DIRECTORY.
+    """
+    from clinica.iotools.converters.ukb_to_bids.ukb_to_bids import convert_images
+    from clinica.utils.check_dependency import check_dcm2niix
+    from clinica.utils.stream import cprint
+
+    check_dcm2niix()
+
+    convert_images(dataset_directory, bids_directory, clinical_data_directory)
+
+    cprint("Conversion to BIDS succeeded.")
+
+
+if __name__ == "__main__":
+    cli()
