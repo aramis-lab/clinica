50a089e3fdac65f1df90112da89d49044cc6b00f
Ghislain Antony Vaillant <ghislain.vaillant@icm-institute.org>
2022-03-07 11:39:29 +0100

    Fix PET pattern in statistics surface pipeline

--- clinica/pipelines/statistics_surface/statistics_surface_utils.py
+++ clinica/pipelines/statistics_surface/statistics_surface_utils.py
@@ -21,7 +21,7 @@ def get_pet_surface_custom_file(acq_label, suvr_reference_region):
         "@session",
         "pet",
         "surface",
-        f"@subject_@session_task-rest_acq-{acq_label}_pet"
+        f"@subject_@session_trc-{acq_label}_pet"
         f"_space-fsaverage_suvr-{suvr_reference_region}_pvc-iy_hemi-@hemi_fwhm-@fwhm_projection.mgh",
     )
     return custom_file
