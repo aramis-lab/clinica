c44e3c518602b90db9b8246fc29b9200ed3e5c36
omar.elrifai <omar.void@gmail.com>
2022-05-25 15:35:10 +0200

    Replace Datasink with subprocess

--- clinica/pipelines/interfaces.py
+++ clinica/pipelines/interfaces.py
@@ -1,6 +1,6 @@
 from pydra.tasks.nipype1.utils import Nipype1Task
 import nipype.interfaces.io as nio
-from nipype import DataSink
+import pydra
 
 
 def bids_reader_task(input_dir, query_bids):
@@ -9,28 +9,25 @@ def bids_reader_task(input_dir, query_bids):
     """
 
     bids_data_grabber = nio.BIDSDataGrabber(
-        base_dir=input_dir,
-        output_query=query_bids,
+            output_query=query_bids,
     )
-
+    
     task_bids_data_grabber = Nipype1Task(
-        name="pydra_bids_data_reader", interface=bids_data_grabber
+        name="pydra_bids_data_reader", base_dir=input_dir, interface=bids_data_grabber
     )
-
+    
     return task_bids_data_grabber
 
 
-def bids_writer_task(output_dir, parametrization=False):
-    """
-    Return pydra Task for writting bids-compliant dataset
-    """
 
-    data_writer = DataSink(base_directory=output_dir, parametrization=parametrization)
-    data_writer.inputs.structural = (
-        "/Users/omar.elrifai/workspace/experimentations/pydra/IN/structural.nii.gz"
-    )
-    task_bids_data_writer = Nipype1Task(
-        name="pydra_bids_data_writer", interface=data_writer
-    )
 
-    return task_bids_data_writer
+@pydra.mark.task
+
+def bids_writer_task(input_file, output_dir):
+    """
+    Task to write files to output_dir
+    """
+    import subprocess
+    subprocess.run(["cp", input_file, output_dir])
+
+    
