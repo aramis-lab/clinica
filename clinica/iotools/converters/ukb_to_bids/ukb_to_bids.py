130c283797fe103ae58bc2eda3cae58e88a21b34
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2022-06-08 11:17:27 +0200

    Fix wrong return type

--- clinica/iotools/converters/ukb_to_bids/ukb_to_bids.py
+++ clinica/iotools/converters/ukb_to_bids/ukb_to_bids.py
@@ -1,14 +1,13 @@
 """Convert the UKB dataset into BIDS."""
 
 from os import PathLike
-from typing import List
 
 
 def convert_images(
     path_to_dataset: PathLike,
     bids_dir: PathLike,
     path_to_clinical: PathLike,
-) -> List[PathLike]:
+):
     """Convert the entire dataset to BIDS.
 
     Scans available files in the path_to_dataset,
@@ -39,12 +38,10 @@ def convert_images(
     # build the tsv
     result = dataset_to_bids(df_clinical)
 
-    written = write_bids(
+    write_bids(
         to=bids_dir,
         participants=result["participants"],
         sessions=result["sessions"],
         scans=result["scans"],
         dataset_directory=path_to_dataset,
     )
-
-    return written
