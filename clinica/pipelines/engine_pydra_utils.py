1d39c6baaff5d5a1c2c3423f71a9e1e1aff0cbc6
omar.elrifai <omar.void@gmail.com>
2022-07-04 15:38:10 +0200

    add engine_pydra_utils

---
+++ clinica/pipelines/engine_pydra_utils.py
@@ -0,0 +1,36 @@
+from pydra import Workflow
+
+
+def list_keys(query_bids: dict) -> list:
+    """
+    :query_bids: query dictionary to BIDSDataGrabber
+    :return: IDS of queried elements
+    """
+
+    return list(query_bids.keys())
+
+
+def list_out_fields(wf: Workflow) -> list:
+    """
+    :return: list of workflow "wf" fields in output_spec
+    """
+    return [x[0] for x in wf.output_spec.fields if not x[0].startswith("_")]
+
+
+def list_in_fields(wf: Workflow) -> list:
+    """
+    :return: list of worfklow wf fields in input_spec
+    """
+    return [x[0] for x in wf.input_spec.fields if not x[0].startswith("_")]
+
+
+def bids_query(keys: list) -> dict:
+    """
+    keys: a list of BIDS items to query
+    returns: a dictionary formated for input to BIDSDataGrabber()
+    """
+    bids_keys_available = {
+        "T1w": {"datatype": "anat", "suffix": "T1w", "extension": [".nii.gz"]}
+    }
+
+    return {key: bids_keys_available[key] for key in keys}
