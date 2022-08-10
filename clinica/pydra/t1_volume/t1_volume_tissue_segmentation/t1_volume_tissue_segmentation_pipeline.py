5ba38b45dc288d88ab5bb21ef1a19bf9480c57fd
omar.elrifai <omar.void@gmail.com>
2022-08-10 14:16:19 +0200

    Avoid checking for spm

--- clinica/pydra/t1_volume/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
+++ clinica/pydra/t1_volume/t1_volume_tissue_segmentation/t1_volume_tissue_segmentation_pipeline.py
@@ -18,40 +18,41 @@ from clinica.pydra.t1_volume.t1_volume_utils import (
 in_dir = Path("/Users/omar.elrifai/workspace/experimentations/pydra/IN/")
 nifti_list = [in_dir / "t1w.nii.gz"]
 test_nii = in_dir / "t1w.nii"
-parameters = {}
-parameters.setdefault("tissue_classes", [1, 2, 3])
-parameters.setdefault("dartel_tissues", [1, 2, 3])
-parameters.setdefault("save_warped_unmodulated", True)
-parameters.setdefault("save_warped_modulated", False)
-
-if not parameters["tissue_probability_maps"]:
-    parameters.setdefault("tissue_probability_maps", spm_utils.get_tpm())
-
-tissue_tuples = get_tissue_tuples(
-    parameters["tissue_probability_maps"],
-    parameters["tissue_classes"],
-    parameters["dartel_tissues"],
-    parameters["save_warped_unmodulated"],
-    parameters["save_warped_modulated"],
-)
 
 
 @clinica_io
-def t1volume_tissue_segmentation(name: str = "t1volume") -> Workflow:
-
+def t1volume_tissue_segmentation(
+    name: str = "t1volume", tissue_probability_map=None
+) -> Workflow:
     """Workflow for tissue segmentation, bias correction and spatial normalization"""
 
     from clinica.pydra.t1_volume.t1_volume_tasks import (
         check_volume_location_in_world_coordinate_system,
     )
 
-    # Initialize the workflow
-
     workflow = Workflow(
         name,
         input_spec=["T1w"],
     )
 
+    parameters = {}
+    parameters.setdefault("tissue_classes", [1, 2, 3])
+    parameters.setdefault("dartel_tissues", [1, 2, 3])
+    parameters.setdefault("save_warped_unmodulated", True)
+    parameters.setdefault("save_warped_modulated", False)
+
+    if tissue_probability_map:
+        parameters.setdefault("tissue_probability_maps", tissue_probability_map)
+    else:
+        parameters.setdefault("tissue_probability_maps", spm_utils.get_tpm())
+
+    tissue_tuples = get_tissue_tuples(
+        parameters["tissue_probability_maps"],
+        parameters["tissue_classes"],
+        parameters["dartel_tissues"],
+        parameters["save_warped_unmodulated"],
+        parameters["save_warped_modulated"],
+    )
     # Verify the input is centered
 
     workflow.add(
