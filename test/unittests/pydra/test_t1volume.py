8159cef445abcb84fe689231db6e3f32260fe50b
omar.elrifai <omar.void@gmail.com>
2022-08-17 15:44:46 +0200

    Update unit test

--- test/unittests/pydra/test_t1volume.py
+++ test/unittests/pydra/test_t1volume.py
@@ -1,14 +1,7 @@
-from dataclasses import dataclass
-from os import PathLike
-from typing import Union
-
-import pytest
-
-
 def test_ApplySegmentationDeformation():
     """Uses SPM to apply a deformation field obtained from Segmentation routine to a given file"""
 
-    import clinica.pydra.t1_volume.t1_volume_tasks as t1vol_tasks
+    import clinica.pydra.t1_volume.tasks as t1vol_tasks
 
     inv = t1vol_tasks.ApplySegmentationDeformation()
     assert inv._jobname == "defs"
