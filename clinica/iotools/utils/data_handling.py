bf912427ed08368ea43046a3839c89a7d82610a8
omar-rifai <omar.void@gmail.com>
2022-09-01 17:34:24 +0200

    Update clinica/iotools/utils/data_handling.py
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>

--- clinica/iotools/utils/data_handling.py
+++ clinica/iotools/utils/data_handling.py
@@ -1218,7 +1218,7 @@ def get_center_volume(header: Nifti1Header) -> ndarray:
     """Get the voxel coordinates of the center of the data, using header information.
     Parameters
     ----------
-        header: Nifti1Header
+    header: Nifti1Header
             Contains image metadata
     Returns
     -------
