67465b5b34c1863715717c64ef26cddb55052279
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2022-09-09 14:24:32 +0200

    Remove useless class

--- clinica/iotools/bids_readme.py
+++ clinica/iotools/bids_readme.py
@@ -15,25 +15,16 @@ class BIDSReadme:
     See
     """
 
-    class DatasetType(str, Enum):
-        raw = "raw"
-        derivative = "derivative"
-
     name: str
     bids_version: str = BIDS_VERSION
-    dataset_type: DatasetType = DatasetType.raw
 
     def write(self, to: IO[str], readme_dict):
         import clinica
 
         # datadict_link = {
-        #     "ADNI": "http://adni.loni.usc.edu",
-        #     "AIBL": "http://adni.loni.usc.edu/study-design/collaborative-studies/aibl/",
         #     "HABS": "https://habs.mgh.harvard.edu",
         # }
         # datadict_description = {
-        #     "ADNI": "ADNI is a global research effort that actively supports the investigation and development of treatments that slow or stop the progression of Alzheimer's disease (AD).This multisite, longitudinal study assesses clinical, imaging, genetic and biospecimen biomarkers through the process of normal aging to mild cognitive impairment (MCI) and AD dementia.With established, standardized methods for imaging and biomarker collection and analysis, ADNI facilitates a way for scientists to conduct cohesive research and share compatible data with other researchers around the world.",
-        #     "AIBL": "The Australian Imaging, Biomarker & Lifestyle Flagship Study of Ageing (AIBL) seeks to discover which biomarkers, cognitive characteristics, and health and lifestyle factors determine the development of AD.Although AIBL and ADNI have many of the same goals, there are differences between the two projects.",
         #     "HABS": "The overall goal of the Harvard Aging Brain Study (HABS) is to elucidate the earliest changes in molecular, functional and structural imaging markers that signal the transition from normal cognition to progressive cognitive decline along the trajectory of preclinical Alzheimer’s Disease.",
         # }
 
