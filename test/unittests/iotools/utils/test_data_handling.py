a9a0f442d6034c9668837241b802827d0b76807d
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2022-09-12 15:45:12 +0200

    Remove a useless call, and add details to the doctstring  of the test

--- test/unittests/iotools/utils/test_data_handling.py
+++ test/unittests/iotools/utils/test_data_handling.py
@@ -6,7 +6,11 @@ import pytest
 
 
 def test_vox_to_world_space():
-    """Test function `vox_to_world_space_method_1`."""
+    """Test function `vox_to_world_space_method_1`.
+
+    .. note::
+        We want to verify, that the function does run with it's normal parameters
+        and that it returns what we expect it to return."""
 
     import numpy as np
     from nibabel.nifti1 import Nifti1Image
@@ -17,7 +21,7 @@ def test_vox_to_world_space():
     img = Nifti1Image(vol, np.eye(4))
     nii_header = img.get_header()
     coords = np.array([0.0, 0.0, 0.0])
-    vox_to_world_space_method_1(coordinates_vol=coords, header=nii_header)
+
     assert np.array_equal(
         vox_to_world_space_method_1(coordinates_vol=coords, header=nii_header), coords
     )
