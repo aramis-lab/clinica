e3d8af5f24bafde2892ef4551b72bff4acbdaf0d
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2022-09-13 11:33:22 +0200

    Implement a better test

--- test/unittests/iotools/utils/test_data_handling.py
+++ test/unittests/iotools/utils/test_data_handling.py
@@ -1,34 +1,8 @@
-# def test_vox_to_world_space():
-#     """Test function `vox_to_world_space_method_1`.
-
-#     .. note::
-#         We want to verify, that the function does run with it's normal parameters
-#         and that it returns what we expect it to return."""
-
-#     import numpy as np
-#     from nibabel.nifti1 import Nifti1Image
-
-#     from clinica.iotools.utils.data_handling import vox_to_world_space_method_1
-
-#     vol = np.ndarray(shape=(5, 5, 2, 100))
-#     img = Nifti1Image(vol, np.eye(4))
-#     coords = np.array([0.0, 0.0, 0.0])
-
-#     assert np.array_equal(
-#         vox_to_world_space_method_1(coordinates_vol=coords, header=img.get_header()),
-#         coords,
-#     )
-
-
-def test_vox_to_world_space():
-    """Test function `vox_to_world_space_method_1`.
-
-    .. note::
-        We want to verify, that the function does run with it's normal parameters
-        and that it returns what we expect it to return."""
+def test_vox_to_world_space_method_1():
+    """Test function `vox_to_world_space_method_1`."""
 
     import numpy as np
-    from nibabel.nifti1 import Nifti1Header, Nifti1Image
+    from nibabel.nifti1 import Nifti1Header
 
     from clinica.iotools.utils.data_handling import vox_to_world_space_method_1
 
