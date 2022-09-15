97e22fe437aac6319a579e9c06dc0b0ba92d2af2
Gensollen <nicolas.gensollen@gmail.com>
2022-09-15 19:27:54 +0200

    [REF] Refactor `read_psf_information` (#748)
    * Refactor read_psf_information
    
    * Add unittests
    
    * Apply suggestions from code review
    
    Co-authored-by: Ghislain Vaillant <ghisvail@users.noreply.github.com>
    
    * lower case valid_columns
    
    * Fix type hints
    
    Co-authored-by: Ghislain Vaillant <ghisvail@users.noreply.github.com>

--- clinica/utils/pet.py
+++ clinica/utils/pet.py
@@ -1,5 +1,9 @@
 """This module contains utilities for PET data handling."""
+import os
+import typing as ty
 from enum import Enum
+
+import pandas as pd
 
 
 class Tracer(str, Enum):
@@ -18,17 +22,46 @@ class Tracer(str, Enum):
     FMM = "18FFMM"
 
 
-def read_psf_information(pvc_psf_tsv, subject_ids, session_ids, pet_tracer):
+def read_psf_information(
+    pvc_psf_tsv: os.PathLike,
+    subject_ids: ty.List[str],
+    session_ids: ty.List[str],
+    pet_tracer: str,
+) -> ty.List[ty.List[int]]:
     """Read PSF information from TSV file.
 
-    Args:
-        pvc_psf_tsv: TSV file containing participant_id, session_id, acq_label,
-            psf_x, psf_y & psf_z columns
-        subject_ids: list of participant IDs (e.g. ['sub-CLNC01', 'sub-CLNC01'])
-        session_ids: list of session IDs (e.g. ['ses-M00', 'ses-M18'])
-        pet_tracer: Tracer we want to select in acq_label column. Other tracers
-            will not be read in this function
+    Parameters
+    ----------
+    pvc_psf_tsv : PathLike
+        Path to the TSV file containing the following columns:
+        'participant_id', 'session_id', 'acq_label', 'psf_x',
+        'psf_y', and 'psf_z'
 
+    subject_ids : List[str]
+        List of participant IDs.
+        ex: ['sub-CLNC01', 'sub-CLNC01']
+
+        .. warning::
+            Must have the same length as `session_ids`.
+
+    session_ids : List[str]
+        List of session IDs.
+        ex: ['ses-M00', 'ses-M18']
+
+        .. warning::
+            Must have the same length as `subject_ids`.
+
+    pet_tracer : str
+        Tracer we want to select in the 'acq_label' column.
+        Other tracers will not be read in this function
+
+    Returns
+    -------
+    psf : List[List[int]]
+        The PSF information following [subject_ids, session_ids] order.
+
+    Examples
+    --------
     Example of pvc_psf_tsv:
 
     participant_id    session_id     acq_label     psf_x    psf_y    psf_z
@@ -37,65 +70,44 @@ def read_psf_information(pvc_psf_tsv, subject_ids, session_ids, pet_tracer):
     sub-CLNC01        ses-M00        AV45          7        6        5
     sub-CLNC02        ses-M00        FDG           8        9        10
     sub-CLNC03        ses-M00        FDG           8        9        10
-
-    Returns:
-        PSF information following [subject_ids, session_ids] order
     """
-    import os
-
-    from pandas import read_csv
-
-    if not os.path.isfile(pvc_psf_tsv):
-        raise FileNotFoundError(f"Could not find the psf_tsv file {pvc_psf_tsv}")
-    try:
-        psf_df = read_csv(pvc_psf_tsv, sep="\t")
-    except (IOError, UnicodeDecodeError):
-        raise RuntimeError("An error while reading {pvc_psf_tsv} happened")
-
-    if any(
-        elem
-        not in ["participant_id", "session_id", "acq_label", "psf_x", "psf_y", "psf_z"]
-        for elem in list(psf_df.columns)
-    ):
+    valid_columns = {
+        "participant_id",
+        "session_id",
+        "acq_label",
+        "psf_x",
+        "psf_y",
+        "psf_z",
+    }
+    psf_df = pd.read_csv(pvc_psf_tsv, sep="\t")
+    diff = valid_columns.symmetric_difference(set(psf_df.columns))
+    if len(diff) > 0:
         raise IOError(
             f"The file {pvc_psf_tsv} must contain the following columns (separated by tabulations):\n"
             f"participant_id, session_id, acq_label, psf_x, psf_y, psf_z\n"
             f"{str(list(psf_df.columns))}\n"
             f"Pay attention to the spaces (there should be none)."
         )
-
-    subjects_psf = list(psf_df.participant_id)
-    sessions_psf = list(psf_df.session_id)
-    pet_tracer_psf = list(psf_df.acq_label)
-    idx_reordered = []
-    for i, sub in enumerate(subject_ids):
-        current_ses = session_ids[i]
-        idx_sub = [
-            j
-            for j in range(len(subjects_psf))
-            if (sub == subjects_psf[j])
-            and (current_ses == sessions_psf[j])
-            and (pet_tracer == pet_tracer_psf[j])
-        ]
-        if len(idx_sub) == 0:
+    psf = []
+    for sub, ses in zip(subject_ids, session_ids):
+        result = psf_df.query(
+            f"participant_id == '{sub}' and session_id == '{ses}' and acq_label == '{pet_tracer}'"
+        )
+        if len(result) == 0:
             raise RuntimeError(
-                f"Subject {sub} with session {current_ses} and tracer {pet_tracer} "
+                f"Subject {sub} with session {ses} and tracer {pet_tracer} "
                 f"that you want to proceed was not found in the TSV file containing "
                 f"PSF specifications ({pvc_psf_tsv})."
             )
-        if len(idx_sub) > 1:
+        if len(result) > 1:
             raise RuntimeError(
-                f"Subject {sub} with session {current_ses} and tracer {pet_tracer} "
+                f"Subject {sub} with session {ses} and tracer {pet_tracer} "
                 f"that you want to proceed was found multiple times "
                 f"in the TSV file containing PSF specifications ({pvc_psf_tsv})."
             )
-        idx_reordered.append(idx_sub[0])
+        psf.append(result[["psf_x", "psf_y", "psf_z"]].values.tolist()[0])
 
-    psf_x = list(psf_df.psf_x)
-    psf_y = list(psf_df.psf_y)
-    psf_z = list(psf_df.psf_z)
-    iterables_psf = [[psf_x[i], psf_y[i], psf_z[i]] for i in idx_reordered]
-    return iterables_psf
+    return psf
 
 
 LIST_SUVR_REFERENCE_REGIONS = ["pons", "cerebellumPons", "pons2", "cerebellumPons2"]
