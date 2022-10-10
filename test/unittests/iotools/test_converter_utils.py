c6ac0bdfdf65cbc39847d6d5bb60fef1bb72debc
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2022-10-10 14:53:17 +0200

    Add unit test for viscode_to_session

--- test/unittests/iotools/test_converter_utils.py
+++ test/unittests/iotools/test_converter_utils.py
@@ -12,3 +12,17 @@ def test_sort_session_list():
     assert sort_session_list(
         ["ses-M0", "ses-M6", "ses-M12", "ses-M24", "ses-M48", "ses-M3"]
     ) == ["ses-M0", "ses-M3", "ses-M6", "ses-M12", "ses-M24", "ses-M48"]
+
+
+def test_viscode_to_session():
+    """Test function `viscode_to_session`."""
+
+    from clinica.iotools.converter_utils import viscode_to_session
+
+    assert viscode_to_session("bl") == "ses-M000"
+    assert viscode_to_session("m0") == "ses-M000"
+    assert viscode_to_session("m3") == "ses-M003"
+    assert viscode_to_session("m03") == "ses-M003"
+    assert viscode_to_session("m003") == "ses-M003"
+    assert viscode_to_session("m0003") == "ses-M003"
+    assert viscode_to_session("m00") == "ses-M000"
