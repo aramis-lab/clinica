21c34a3190df74fa38504e72f9c342934b3cb9b9
NicolasGensollen <nicolas.gensollen@gmail.com>
2022-11-07 16:50:17 +0100

    Start adding tests and docstrings

--- clinica/utils/filemanip.py
+++ clinica/utils/filemanip.py
@@ -1,3 +1,7 @@
+import re
+from typing import List
+
+
 def zip_nii(in_file: str, same_dir: bool = False):
     import gzip
     import shutil
@@ -80,25 +84,73 @@ def save_participants_sessions(participant_ids, session_ids, out_folder, out_fil
         raise e
 
 
+def _raise_non_bids_or_caps_compliant_filename(filename: str) -> None:
+    raise ValueError(
+        f"Input filename {filename} is not in a BIDS or CAPS compliant format."
+        " It does not contain the subject and session information."
+    )
+
+
 def get_subject_id(bids_or_caps_file: str) -> str:
-    """Extract "sub-<participant_id>_ses-<session_label>" from BIDS or CAPS file."""
-    import re
+    """Extract the subject ID from a BIDS or CAPS file path.
 
+    The subject ID is defined as
+
+        sub-<participant_id>_ses-<session_label>
+
+    In other words, it is the concatenation of the subject and session labels,
+    separated by "_".
+
+    Parameters
+    ----------
+    bids_or_caps_file: str
+        Path to a file from a BIDS or CAPS folder.
+
+    Returns
+    -------
+    subject_id: str
+        The subject ID corresponding to the given file.
+
+    Examples
+    --------
+    >>> get_subject_id("sub-01/ses-M000/pet/sub-01_ses-M000_trc-18FAV45_pet.nii.gz")
+    'sub-01_ses-M000'
+    >>> get_subject_id("sub-01_ses-M000_trc-18FAV45_pet.nii.gz")
+    Traceback (most recent call last):
+    ValueError: Input filename sub-01_ses-M000_trc-18FAV45_pet.nii.gz is not in a BIDS or CAPS compliant format. It does not contain the subject and session information.
+
+    See also
+    --------
+    extract_image_ids
+    """
     m = re.search(r"(sub-[a-zA-Z0-9]+)/(ses-[a-zA-Z0-9]+)", bids_or_caps_file)
-
     if not m:
-        raise ValueError(
-            f"Input filename {bids_or_caps_file} is not in a BIDS or CAPS compliant format."
-            " It does not contain the subject and session information."
-        )
-
+        _raise_non_bids_or_caps_compliant_filename(bids_or_caps_file)
     subject_id = m.group(1) + "_" + m.group(2)
 
     return subject_id
 
 
-def get_filename_no_ext(filename):
-    """Get filename without extension [".nii.gz", ".tar.gz", ".niml.dset"]."""
+def get_filename_no_ext(filename: str) -> str:
+    """Get the filename without the extension.
+
+    Parameters
+    ----------
+    filename: str
+        The full filename from which to extract the extension out.
+
+    Returns
+    -------
+    filename_no_ext: str
+        The filename with extension removed.
+
+    Examples
+    --------
+    >>> get_filename_no_ext("foo.nii.gz")
+    'foo'
+    >>> get_filename_no_ext("sub-01/ses-M000/sub-01_ses-M000.tar.gz")
+    'sub-01_ses-M000'
+    """
     from nipype.utils.filemanip import split_filename
 
     _, filename_no_ext, _ = split_filename(filename)
@@ -106,14 +158,46 @@ def get_filename_no_ext(filename):
     return filename_no_ext
 
 
-def extract_image_ids(bids_or_caps_files):
-    """Extract image IDs (e.g. ['sub-CLNC01_ses-M00', 'sub-CLNC01_ses-M18']  from `bids_or_caps_files`."""
-    import re
+def extract_image_ids(bids_or_caps_files: List[str]) -> List[str]:
+    """Extract the image IDs from a list of BIDS or CAPS files.
 
-    id_bids_or_caps_files = [
-        re.search(r"(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)", file).group()
-        for file in bids_or_caps_files
-    ]
+    .. warning::
+        The image ID is the same as the subject ID from function
+        `get_subject_id()` but is extracted from the filename rather
+        than from the path. (See examples section).
+
+    Parameters
+    ----------
+    bids_or_caps_files: List[str]
+        List of file names from which to extract the image IDs.
+
+    Returns
+    -------
+    id_bids_or_caps_files: List[str]
+        List of extracted image IDs.
+
+    Examples
+    --------
+    >>> extract_image_ids(["sub-01/ses-M000/pet/sub-01_ses-M000_trc-18FAV45_pet.nii.gz"])
+    ['sub-01_ses-M000']
+
+    Beware, the image IDs is extracted from the filename and not from the folder names
+    as it is the case for `get_subject_id()`:
+
+    >>> extract_image_ids(["foo/bar/baz/sub-foo/ses-bar/foooo/sub-01_ses-M000_foo.json"])
+    ['sub-01_ses-M000']
+
+    See also
+    --------
+    get_subject_id
+    """
+    id_bids_or_caps_files = []
+    for f in bids_or_caps_files:
+        m = re.search(r"(sub-[a-zA-Z0-9]+)_(ses-[a-zA-Z0-9]+)", f)
+        if not m:
+            _raise_non_bids_or_caps_compliant_filename(f)
+        id_bids_or_caps_files.append(m.group())
+
     return id_bids_or_caps_files
 
 
@@ -199,7 +283,7 @@ def extract_metadata_from_json(json_file, list_keys):
         raise EnvironmentError(
             f"[Error] Clinica could not open the following JSON file: {json_file}"
         )
-    except KeyError as e:
+    except KeyError:
         now = datetime.datetime.now().strftime("%H:%M:%S")
         error_message = f"[{now}] Error: Clinica could not find the e key in the following JSON file: {json_file}"
         raise ClinicaException(error_message)
---
+++ test/unittests/utils/test_filemanip.py
@@ -0,0 +1,85 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "foo",
+        "sub-01.json",
+        "ses-M000.nii.gz",
+        "sub-01_ses-M000.json",
+        "sub-01_ses-M000_trc-18FAV45_pet.nii.gz",
+    ],
+)
+def test_get_subject_id_error(filename):
+    from clinica.utils.filemanip import get_subject_id
+
+    with pytest.raises(
+        ValueError,
+        match=(
+            f"Input filename {filename} is not in a BIDS or CAPS compliant format. "
+            "It does not contain the subject and session information."
+        ),
+    ):
+        get_subject_id(filename)
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        (
+            "sub-01/ses-M000/pet/sub-01_ses-M000_trc-18FAV45_pet.nii.gz",
+            "sub-01_ses-M000",
+        ),
+        (
+            "foo/bar/baz/sub-foo/ses-bar/foooo/sub-01_ses-M000_foo.json",
+            "sub-foo_ses-bar",
+        ),
+    ],
+)
+def test_get_subject_id(filename, expected):
+    from clinica.utils.filemanip import get_subject_id
+
+    assert get_subject_id(filename) == expected
+
+
+@pytest.mark.parametrize(
+    "filename,expected",
+    [
+        ("foo.nii.gz", "foo"),
+        ("sub-01/ses-M000/sub-01_ses-M000.tar.gz", "sub-01_ses-M000"),
+        ("foo/bar/baz/foo-bar_baz.niml.dset", "foo-bar_baz"),
+    ],
+)
+def test_get_filename_no_ext(filename, expected):
+    from clinica.utils.filemanip import get_filename_no_ext
+
+    assert get_filename_no_ext(filename) == expected
+
+
+def test_extract_image_ids_error():
+    from clinica.utils.filemanip import extract_image_ids
+
+    with pytest.raises(
+        ValueError,
+        match=(
+            "Input filename foo.bar is not in a BIDS or CAPS compliant format. "
+            "It does not contain the subject and session information."
+        ),
+    ):
+        extract_image_ids(["foo.bar"])
+
+
+def test_extract_image_ids():
+    from clinica.utils.filemanip import extract_image_ids
+
+    assert (
+        extract_image_ids(
+            [
+                "sub-01/ses-M000/pet/sub-01_ses-M000_trc-18FAV45_pet.nii.gz",
+                "foo/bar/baz/sub-foo/ses-bar/foooo/sub-01_ses-M000_foo.json",
+                "sub-01_ses-M000.tar.gz",
+            ]
+        )
+        == ["sub-01_ses-M000"] * 3
+    )
