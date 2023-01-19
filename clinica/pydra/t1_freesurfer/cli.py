b777a1f70073f2ce52c59f844efab3815aa374fd
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-01-19 18:47:47 +0100

    WIP: Add Pydra implementation for T1FreeSurfer

--- clinica/pydra/t1_freesurfer/cli.py
+++ clinica/pydra/t1_freesurfer/cli.py
@@ -1,13 +1,36 @@
 import click
 
+from clinica.pipelines import cli_param
 from clinica.pipelines.engine import clinica_pipeline
+from clinica.pydra import engine_utils
+
+name = "pydra-t1-freesurfer"
 
 
 @clinica_pipeline
-@click.command(name="pydra-t1-freesurfer", hidden=True)
-def cli() -> None:
+@click.command(name=name, hidden=True)
+@cli_param.argument.bids_directory
+@cli_param.argument.caps_directory
+@cli_param.option_group.pipeline_specific_options
+@cli_param.option.atlas_path
+def cli(
+    bids_directory: str,
+    caps_directory: str,
+    atlas_path: str,
+) -> None:
     """Cross-sectional pre-processing of T1w volumes with FreeSurfer."""
-    pass
+    from . import pipeline
+
+    parameters = {"atlas_path": atlas_path}
+
+    workflow = pipeline.build_core_workflow(
+        name=name,
+        input_dir=bids_directory,
+        output_dir=caps_directory,
+        parameters=parameters,
+    )
+
+    engine_utils.run(workflow)
 
 
 if __name__ == "__main__":
---
+++ clinica/pydra/t1_freesurfer/pipeline.py
@@ -0,0 +1,57 @@
+import pydra
+
+from clinica.pydra.engine import clinica_io
+
+
+@clinica_io
+def build_core_workflow(name: str, parameters: dict) -> pydra.Workflow:
+    from pydra.tasks.bids import BIDSFileInfo
+    from pydra.tasks.freesurfer import ReconAll
+
+    from .tasks import compute_freesurfer_subject_id
+
+    input_spec = pydra.specs.SpecInfo(
+        name="Input",
+        fields=[("T1w", str, {"mandatory": True})],
+        bases=(pydra.specs.BaseSpec,),
+    )
+
+    workflow = pydra.Workflow(name=name, input_spec=input_spec)
+
+    workflow.add(
+        BIDSFileInfo(
+            output_entities={
+                "participant_id": "sub",
+                "session_id": "ses",
+            }
+        ).to_task(
+            name="bids_file_info",
+            file_path=workflow.lzin.T1w,
+        )
+    )
+
+    workflow.add(
+        compute_freesurfer_subject_id(
+            name="compute_freesurfer_subject_id",
+            participant_id=workflow.bids_file_info.lzout.participant_id,
+            session_id=workflow.bids_file_info.lzout.session_id,
+        )
+    )
+
+    workflow.add(
+        ReconAll(
+            name="recon_all",
+            t1_volume_file=workflow.lzin.T1w,
+            subject_id=workflow.compute_freesurfer_subject_id.lzout.subject_id,
+            subjects_dir=".",
+        )
+    )
+
+    workflow.set_output(
+        [
+            ("subjects_dir", workflow.recon_all.lzout.subjects_dir),
+            ("subject_id", workflow.recon_all.lzout.subject_id),
+        ]
+    )
+
+    return workflow
