cdf8f931cdab0c4b42dab447e79055296da412c7
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-01-30 14:52:35 +0100

    refactor tests according to suggestion from code review

--- test/unittests/utils/test_check_dependency.py
+++ test/unittests/utils/test_check_dependency.py
@@ -30,48 +30,76 @@ def test_check_environment_variable():
     os.environ.pop("FOO")
 
 
-def test_check_software():
+@pytest.mark.parametrize(
+    "binaries,env,expected_msg",
+    [
+        (
+            ["foo"],
+            None,
+            "Clinica could not find foo software: the foo command is not present in your PATH environment.",
+        ),
+        (
+            ["ls"],
+            ("FOO", "foo"),
+            "Clinica could not find foo software: the FOO variable is not set.",
+        ),
+    ],
+)
+def test_check_software_errors(binaries, env, expected_msg):
     from clinica.utils.check_dependency import _check_software
     from clinica.utils.exceptions import ClinicaMissingDependencyError
 
-    assert _check_software("foo") is None
     with pytest.raises(
         ClinicaMissingDependencyError,
-        match=(
-            "Clinica could not find foo software: the foo "
-            "command is not present in your PATH environment."
+        match=expected_msg,
+    ):
+        _check_software("foo", binaries=binaries, env=env)
+
+
+@pytest.mark.parametrize(
+    "binaries,env,complementary,expected_msg",
+    [
+        (
+            ["ls"],
+            ("FOO", "foo"),
+            None,
+            "The FOO environment variable you gave is not a folder",
         ),
-    ):
-        _check_software("foo", binaries=["foo"])
-
-    assert _check_software("foo", binaries=["ls"]) is None
-    with pytest.raises(
-        ClinicaMissingDependencyError,
-        match="Clinica could not find foo software: the FOO variable is not set.",
-    ):
-        _check_software("foo", binaries=["ls"], env=("FOO", "foo"))
-
-    os.environ["FOO"] = "foo"
-
-    with pytest.raises(
-        ClinicaMissingDependencyError,
-        match="The FOO environment variable you gave is not a folder",
-    ):
-        _check_software("foo", binaries=["ls"], env=("FOO", "foo"))
-
-    os.environ["FOO"] = "."
-    assert _check_software("foo", binaries=["ls"], env=("FOO", "foo")) is None
-    with pytest.raises(
-        ClinicaMissingDependencyError,
-        match=(
-            "Clinica could not find foo software: the bar command is not "
-            "present in your PATH environment. Foo bar baz"
+        (
+            ["ls", "bar"],
+            ("FOO", "."),
+            "Foo bar baz",
+            (
+                "Clinica could not find foo software: the bar command is "
+                "not present in your PATH environment. Foo bar baz"
+            ),
         ),
+    ],
+)
+def test_check_software_env_variable_errors(binaries, env, complementary, expected_msg):
+    from clinica.utils.check_dependency import _check_software
+    from clinica.utils.exceptions import ClinicaMissingDependencyError
+
+    name, value = env
+    os.environ[name] = value
+    with pytest.raises(
+        ClinicaMissingDependencyError,
+        match=expected_msg,
     ):
         _check_software(
             "foo",
-            binaries=["ls", "bar"],
+            binaries=binaries,
             env=("FOO", "foo"),
-            complementary_info="Foo bar baz",
+            complementary_info=complementary,
         )
+    os.environ.pop(name)
+
+
+def test_check_software():
+    from clinica.utils.check_dependency import _check_software
+
+    assert _check_software("foo") is None
+    assert _check_software("foo", binaries=["ls"]) is None
+    os.environ["FOO"] = "."
+    assert _check_software("foo", binaries=["ls"], env=("FOO", "foo")) is None
     os.environ.pop("FOO")
