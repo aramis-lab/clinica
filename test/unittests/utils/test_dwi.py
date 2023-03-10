2e81dfd5345a69cd5be29c3e3551d77b15840df1
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-03-10 13:22:34 +0100

    add unit tests

--- test/unittests/utils/test_dwi.py
+++ test/unittests/utils/test_dwi.py
@@ -1,6 +1,7 @@
 import nibabel as nib
 import numpy as np
 import pytest
+from numpy.testing import assert_array_equal
 
 
 def test_rename_files_errors(tmp_path):
@@ -96,3 +97,40 @@ def test_generate_acq_file(tmp_path, image_id, phase, expected):
     with open(acq_file, "r") as fp:
         lines = fp.readlines()
     assert lines == expected
+
+
+def test_generate_index_file_bvalue_file_error(tmp_path):
+    from clinica.utils.dwi import generate_index_file
+
+    with pytest.raises(
+        FileNotFoundError,
+        match="Unable to find b-values file",
+    ):
+        generate_index_file(str(tmp_path / "foo.txt"))
+
+
+def test_generate_index_file_no_b0_error(tmp_path):
+    from clinica.utils.dwi import generate_index_file
+
+    np.savetxt(tmp_path / "foo.bval", [1000] * 8)
+    with pytest.raises(
+        ValueError,
+        match="Could not find b-value <= 5.0 in bval file",
+    ):
+        generate_index_file(str(tmp_path / "foo.bval"))
+
+
+@pytest.mark.parametrize("image_id", [None, "foo", "foo_bar"])
+def test_generate_index_file(tmp_path, image_id):
+    from clinica.utils.dwi import generate_index_file
+
+    np.savetxt(tmp_path / "foo.bval", [1000, 1000, 0, 0, 0, 1000, 1000, 0])
+    index_file = generate_index_file(str(tmp_path / "foo.bval"), image_id=image_id)
+    if image_id:
+        assert index_file == str(tmp_path / f"{image_id}_index.txt")
+    else:
+        assert index_file == str(tmp_path / "index.txt")
+    index = np.loadtxt(index_file)
+    # This is wrong and needs to be fixed
+    # the array should be length 8...
+    assert_array_equal(index, np.array([1.0, 2.0, 3.0, 3.0, 3.0, 4.0]))
