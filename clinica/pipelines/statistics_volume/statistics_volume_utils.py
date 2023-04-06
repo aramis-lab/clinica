189313a1267c9825f6fe1ada8fd3ab8eb7d40564
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2023-04-06 10:30:07 +0200

    make format

--- clinica/pipelines/statistics_volume/statistics_volume_utils.py
+++ clinica/pipelines/statistics_volume/statistics_volume_utils.py
@@ -660,11 +660,12 @@ def copy_and_rename_spm_output_files(
     from shutil import copyfile
 
     from clinica.pipelines.statistics_volume.statistics_volume_utils import (  # noqa
-        rename_spm_figures,
-        rename_spm_t_maps,
         rename_beta_files,
         rename_spm_contrast_files,
+        rename_spm_figures,
+        rename_spm_t_maps,
     )
+
     spm_dir = dirname(spm_mat)
     if not isfile(spm_mat):
         if not isdir(spm_dir):
