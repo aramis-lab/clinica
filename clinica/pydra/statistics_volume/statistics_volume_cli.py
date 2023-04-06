8d45f86526264b6177e39d2272fcc1d44f507953
Matthieu Joulot <85217698+MatthieuJoulot@users.noreply.github.com>
2023-04-06 16:25:03 +0200

    Pydra implementation of  statistics-volume (#885)
    * First implementation of the pydra version of statistics-volume
    
    * First implementation of the pydra version of statistics-volume
    
    * use correct names for input
    
    * use correct names for input
    
    * remove base_dir
    
    * Add input_dir=None
    
    * Remove pipe for type hint since it does not seem to work with pydra
    
    * Correct input and output typos
    
    * add query
    
    * Add matlab files
    
    * remove split
    
    * uncomment nodes
    
    * Remove useless comments
    
    * remove useless comments following review
    
    * Refactor run_m_script a little
    
    * Elaborate docstring
    
    * Remove ducplicate functions from task
    
    * change functions name to function_task
    
    * correct typo
    
    * change zip node
    
    * Remove rest of unzip function
    
    * Remove .m files from pydra
    
    * Change path to files
    
    * Raise errors as indicated in by the review
    
    * factorize readoutput in smaller functions
    
    * factorize model_creation
    
    * add typehints
    
    * follow review suggestions
    
    * fix docstring argument order
    
    * get regression_coef to a one-liner
    
    * Add t1_volume as a possible input
    
    * Apply suggestions from code review
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * Factorize query alternatives
    
    * add _check_pipeline_parameters
    
    * add handling of custom-pipelines inputs
    
    * Apply suggestions from code review
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * apply suggestion from review
    
    * apply suggestion regarding classes_idx
    
    * change attribute
    
    * Correct classes_idx problem
    
    * ensure m_file is str
    
    * remove classes_idx
    
    * remove Path(m_file)
    
    * Chane variable name
    
    * Correct custom pipeline handling
    
    * make format
    
    * Apply suggestions from code review
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * correct input_name
    
    * Apply suggestions from code review
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * add path to m_file
    
    * Apply suggestion from review
    
    * fix nipype pipeline
    
    * add non regression test for statistics volume with t1-volume
    
    * factorize parents
    
    * Make a separate function for building query
    
    * Apply suggestions from code review
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * add missing imports
    
    * Update clinica/pipelines/statistics_volume/statistics_volume_utils.py
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * import get_parents correctly
    
    * Update clinica/pydra/statistics_volume/pipeline.py
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>
    
    * make format
    
    ---------
    
    Co-authored-by: Gensollen <nicolas.gensollen@gmail.com>

---
+++ clinica/pydra/statistics_volume/statistics_volume_cli.py
@@ -0,0 +1,139 @@
+from typing import Optional
+
+import click
+
+import clinica
+import clinica.pydra.engine_utils as pydra_utils
+import clinica.pydra.statistics_volume.pipeline as pydra_statistics_volume
+from clinica.pipelines import cli_param
+from clinica.pipelines.engine import clinica_pipeline
+
+pipeline_name = "pydra-statistics-volume"
+
+
+@clinica_pipeline
+@click.command(name=pipeline_name, hidden=True)
+@cli_param.argument.caps_directory
+@cli_param.argument.group_label
+@cli_param.argument.orig_input_data_volume
+@cli_param.argument.subject_visits_with_covariates_tsv
+@cli_param.argument.contrast
+@cli_param.option_group.pipeline_specific_options
+@cli_param.option_group.option(
+    "-dartel",
+    "--group_label_dartel",
+    default="*",
+    show_default=True,
+    help="Name of the DARTEL template that Clinica needs to use to grab the input files.",
+)
+@cli_param.option_group.option(
+    "-fwhm",
+    "--full_width_at_half_maximum",
+    default=8,
+    show_default=True,
+    help="Full Width at Half Maximum (FWHM) of the smoothing used in your input files.",
+)
+@cli_param.option_group.custom_pipeline_options(
+    "Pipeline options if you use inputs from pet-volume pipeline"
+)
+@cli_param.option.acq_label
+@cli_param.option.suvr_reference_region
+@cli_param.option.use_pvc_data
+@cli_param.option_group.custom_pipeline_options(
+    "Pipeline options if you selected custom-pipeline"
+)
+@cli_param.option_group.option(
+    "-cf",
+    "--custom_file",
+    help=(
+        "Custom file string. Specify filename using * when the subject or session name "
+        "appears e.g. '*_trc-18FFDG_pet_space-Ixi549Space_pet.nii.gz' will grab "
+        "the corresponding file in all the subjects/sessions. "
+        "This flag must be specified with the --measure_label flag). "
+        "See Wiki for an example."
+    ),
+)
+@cli_param.option_group.option(
+    "-ml",
+    "--measure_label",
+    help=(
+        "Name of the feature type, it will be saved on the CAPS "
+        "_measure-MEASURE_LABEL key-value association. "
+        "This flag must be specified with the --custom_file flag). "
+        "See Wiki for an example."
+    ),
+)
+@cli_param.option_group.common_pipelines_options
+@cli_param.option.working_directory
+@cli_param.option.n_procs
+@cli_param.option_group.advanced_pipeline_options
+@cli_param.option_group.option(
+    "-ct",
+    "--cluster_threshold",
+    default=0.001,
+    show_default=True,
+    help="Threshold to define a cluster in the process of cluster-wise correction.",
+)
+def cli(
+    caps_directory: str,
+    group_label: str,
+    orig_input_data_volume: str,
+    subject_visits_with_covariates_tsv: str,
+    contrast: str,
+    group_label_dartel: str = "*",
+    full_width_at_half_maximum: int = 8,
+    acq_label: Optional[str] = None,
+    suvr_reference_region: Optional[str] = None,
+    use_pvc_data: bool = False,
+    custom_file: Optional[str] = None,
+    measure_label: Optional[str] = None,
+    cluster_threshold: float = 1e-3,
+    working_directory: Optional[str] = None,
+    n_procs: Optional[int] = None,
+) -> None:
+    """Volume-based mass-univariate analysis with SPM.
+
+       GROUP_LABEL is an user-defined identifier to target a specific group of subjects.
+
+       The type of volume-based feature can be defined by using the third argument: t1-volume to use gray matter maps, pet-volume to use PET data or custom-pipeline for you own data in CAPS directory.
+
+       SUBJECT_VISITS_WITH_COVARIATES_TSV is a TSV file containing a list of subjects with their sessions and all the covariates and factors of the model.
+
+       CONTRAST is a string defining the contrast matrix or the variable of interest for the GLM.
+
+    See https://aramislab.paris.inria.fr/clinica/docs/public/latest/Pipelines/Stats_Volume/
+    """
+    from clinica.utils.exceptions import ClinicaException
+
+    parameters = {
+        # Clinica compulsory arguments
+        "group_label": group_label,
+        "orig_input_data_volume": orig_input_data_volume,
+        "contrast": contrast,
+        # Optional arguments
+        "group_label_dartel": group_label_dartel,
+        "full_width_at_half_maximum": full_width_at_half_maximum,
+        # Optional arguments for inputs from pet-volume pipeline
+        "acq_label": acq_label,
+        "use_pvc_data": use_pvc_data,
+        "suvr_reference_region": suvr_reference_region,
+        # Optional arguments for custom pipeline
+        "measure_label": measure_label,
+        "custom_file": custom_file,
+        # Advanced arguments
+        "cluster_threshold": cluster_threshold,
+        "tsv_file": subject_visits_with_covariates_tsv,
+        "base_dir": working_directory,
+    }
+
+    statistics_volume_pipeline = pydra_statistics_volume.build_core_workflow(
+        name="pydra-statistics-volume",
+        input_dir=None,
+        output_dir=caps_directory,
+        parameters=parameters,
+    )
+    pydra_utils.run(statistics_volume_pipeline, n_procs=n_procs)
+
+
+if __name__ == "__main__":
+    cli()
