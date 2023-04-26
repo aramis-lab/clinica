d80de71dc6724b3f9edc78a52e8e5e2cd0dea2ce
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2023-04-26 14:21:58 +0200

    change unit test following the suggestion

--- test/unittests/iotools/converters/genfi_to_bids/test_genfi_to_bids_utils.py
+++ test/unittests/iotools/converters/genfi_to_bids/test_genfi_to_bids_utils.py
@@ -32,106 +32,100 @@ def test_identify_modality_is_nan():
     assert math.isnan(identify_modality("blzflbzv"))
 
 
-@pytest.mark.parametrize(
-    "input, expected",
-    [
-        (
-            pd.DataFrame(
-                {
-                    "source_id": ["GRN001", "GRN001", "GRN001", "C9ORF001", "C9ORF001"],
-                    "source_ses_id": [1, 1, 1, 2, 2],
-                    "suffix": ["a", "a", "c", "a", "b"],
-                    "number_of_parts": [2, 2, 1, 1, 1],
-                    "dir_num": [10, 20, 30, 10, 20],
-                }
-            ),
-            pd.DataFrame(
-                {
-                    "source_id": ["C9ORF001", "C9ORF001", "GRN001", "GRN001", "GRN001"],
-                    "source_ses_id": [2, 2, 1, 1, 1],
-                    "suffix": ["a", "b", "a", "a", "c"],
-                    "number_of_parts": [1, 1, 2, 2, 1],
-                    "dir_num": [10, 20, 10, 20, 30],
-                    "run_01_dir_num": [10, 20, 10, 10, 30],
-                    "run": [False, False, False, True, False],
-                    "run_number": [1, 1, 1, 2, 1],
-                    "run_num": ["run-01", "run-01", "run-01", "run-02", "run-01"],
-                }
-            ),
-        )
-    ],
-)
-def test_compute_runs(input, expected):
-    assert_frame_equal(compute_runs(input), expected)
+@pytest.fixture
+def input_df_compute_runs():
+    return pd.DataFrame(
+        {
+            "source_id": ["GRN001", "GRN001", "GRN001", "C9ORF001", "C9ORF001"],
+            "source_ses_id": [1, 1, 1, 2, 2],
+            "suffix": ["a", "a", "c", "a", "b"],
+            "number_of_parts": [2, 2, 1, 1, 1],
+            "dir_num": [10, 20, 30, 10, 20],
+        }
+    )
 
 
-@pytest.mark.parametrize(
-    "input, expected",
-    [
-        (
-            pd.DataFrame(
-                {
-                    "source_id": [
-                        "sub-01",
-                        "sub-01",
-                        "sub-01",
-                        "sub-01",
-                        "sub-01",
-                        "sub-02",
-                        "sub-02",
-                        "sub-02",
-                    ],
-                    "source_ses_id": [1, 1, 1, 2, 2, 1, 2, 2],
-                    "suffix": ["dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi"],
-                    "manufacturer": [
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                    ],
-                    "dir_num": [10, 20, 30, 40, 50, 10, 20, 30],
-                }
-            ),
-            pd.DataFrame(
-                {
-                    "source_id": [
-                        "sub-01",
-                        "sub-01",
-                        "sub-01",
-                        "sub-01",
-                        "sub-01",
-                        "sub-02",
-                        "sub-02",
-                        "sub-02",
-                    ],
-                    "source_ses_id": [1, 1, 1, 2, 2, 1, 2, 2],
-                    "suffix": ["dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi"],
-                    "manufacturer": [
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                        "philips",
-                    ],
-                    "dir_num": [10, 20, 30, 40, 50, 10, 20, 30],
-                    "part_01_dir_num": [10, 10, 10, 40, 40, 10, 20, 20],
-                    "run": [False, True, True, False, True, False, False, True],
-                    "part_number": [1, 2, 3, 1, 2, 1, 1, 2],
-                    "number_of_parts": [3, 3, 3, 2, 2, 1, 2, 2],
-                }
-            ),
-        )
-    ],
-)
-def test_compute_philips_parts(input, expected):
-    assert_frame_equal(compute_philips_parts(input), expected)
+def test_compute_runs(input_df_compute_runs):
+    expected = pd.DataFrame(
+        {
+            "source_id": ["C9ORF001", "C9ORF001", "GRN001", "GRN001", "GRN001"],
+            "source_ses_id": [2, 2, 1, 1, 1],
+            "suffix": ["a", "b", "a", "a", "c"],
+            "number_of_parts": [1, 1, 2, 2, 1],
+            "dir_num": [10, 20, 10, 20, 30],
+            "run_01_dir_num": [10, 20, 10, 10, 30],
+            "run": [False, False, False, True, False],
+            "run_number": [1, 1, 1, 2, 1],
+            "run_num": ["run-01", "run-01", "run-01", "run-02", "run-01"],
+        }
+    )
+    assert_frame_equal(compute_runs(input_df_compute_runs), expected)
+
+
+@pytest.fixture
+def input_df_compute_philips_parts():
+    return pd.DataFrame(
+        {
+            "source_id": [
+                "sub-01",
+                "sub-01",
+                "sub-01",
+                "sub-01",
+                "sub-01",
+                "sub-02",
+                "sub-02",
+                "sub-02",
+            ],
+            "source_ses_id": [1, 1, 1, 2, 2, 1, 2, 2],
+            "suffix": ["dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi"],
+            "manufacturer": [
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+            ],
+            "dir_num": [10, 20, 30, 40, 50, 10, 20, 30],
+        }
+    )
+
+
+def test_compute_philips_parts(input_df_compute_philips_parts):
+    expected = pd.DataFrame(
+        {
+            "source_id": [
+                "sub-01",
+                "sub-01",
+                "sub-01",
+                "sub-01",
+                "sub-01",
+                "sub-02",
+                "sub-02",
+                "sub-02",
+            ],
+            "source_ses_id": [1, 1, 1, 2, 2, 1, 2, 2],
+            "suffix": ["dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi", "dwi"],
+            "manufacturer": [
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+                "philips",
+            ],
+            "dir_num": [10, 20, 30, 40, 50, 10, 20, 30],
+            "part_01_dir_num": [10, 10, 10, 40, 40, 10, 20, 20],
+            "run": [False, True, True, False, True, False, False, True],
+            "part_number": [1, 2, 3, 1, 2, 1, 1, 2],
+            "number_of_parts": [3, 3, 3, 2, 2, 1, 2, 2],
+        }
+    )
+    assert_frame_equal(compute_philips_parts(input_df_compute_philips_parts), expected)
 
 
 @pytest.mark.parametrize(
@@ -143,12 +137,12 @@ def test_compute_philips_parts(input, expected):
         ),
         ([True, True, True], [1, 2, 3]),
         ([False, False, False], [1, 1, 1]),
-        ([], ValueError),
     ],
 )
 def test_compute_scan_sequence_numbers(input: List[bool], expected):
-    if expected == ValueError:
-        with pytest.raises(ValueError):
-            _compute_scan_sequence_numbers(input)
-    else:
-        assert _compute_scan_sequence_numbers(input) == expected
+    assert _compute_scan_sequence_numbers(input) == expected
+
+
+def test_compute_scan_sequence_numbers_error():
+    with pytest.raises(ValueError):
+        _compute_scan_sequence_numbers([])
