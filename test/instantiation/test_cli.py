b21a2651b3bdf506ebdb59300b67f0a3202b9cce
JOULOT Matthieu <matthieu.joulot@icm-institute.org>
2023-05-23 11:40:21 +0200

    update instatiation test name

--- test/instantiation/test_cli.py
+++ test/instantiation/test_cli.py
@@ -107,7 +107,7 @@ def test_second_lv_iotools(generate_cli_second_lv_iotools):
         "pydra-t1-volume-create-dartel",
         "pydra-t1-volume-register-dartel",
         "pydra-t1-volume-tissue-segmentation",
-        "pydra-machinelearning-prepare-spatial-svm",
+        "pydra-machine-learning-prepare-spatial-svm",
     ]
 )
 def generate_cli_second_lv_run(request):
