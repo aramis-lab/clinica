5c7010ac2bfe00ab0d2a0132d8e5b28280325ade
Gensollen <nicolas.gensollen@gmail.com>
2023-06-05 14:04:50 +0200

    [TEST] Add unit tests for function `container_from_filename` (#933)
    * make docstring Numpydoc compliant
    
    * add unit tests

--- clinica/utils/nipype.py
+++ clinica/utils/nipype.py
@@ -7,18 +7,23 @@ In particular, this module currently contains functions used for Nipype DataSink
 def container_from_filename(bids_or_caps_filename: str) -> str:
     """Extract container from BIDS or CAPS file.
 
-    Args:
-       bids_or_caps_filename (str): full path to BIDS or CAPS filename.
+    Parameters
+    ----------
+    bids_or_caps_filename : str
+        Full path to BIDS or CAPS filename.
 
-    Returns:
-       Container path of the form "subjects/<participant_id>/<session_id>"
+    Returns
+    -------
+    str :
+        Container path of the form "subjects/<participant_id>/<session_id>".
 
-    Examples:
-       >>> from clinica.utils.nipype import container_from_filename
-       >>> container_from_filename('/path/to/bids/sub-CLNC01/ses-M000/anat/sub-CLNC01_ses-M000_T1w.nii.gz')
-               'subjects/sub-CLNC01/ses-M000'
-       >>> container_from_filename('caps/subjects/sub-CLNC01/ses-M000/dwi/preprocessing/sub-CLNC01_ses-M000_preproc.nii')
-               'subjects/sub-CLNC01/ses-M000'
+    Examples
+    --------
+    >>> from clinica.utils.nipype import container_from_filename
+    >>> container_from_filename('/path/to/bids/sub-CLNC01/ses-M000/anat/sub-CLNC01_ses-M000_T1w.nii.gz')
+    'subjects/sub-CLNC01/ses-M000'
+    >>> container_from_filename('caps/subjects/sub-CLNC01/ses-M000/dwi/preprocessing/sub-CLNC01_ses-M000_preproc.nii')
+    'subjects/sub-CLNC01/ses-M000'
     """
     import os
     import re
