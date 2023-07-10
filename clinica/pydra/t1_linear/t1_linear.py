73b1247dd5802e6dc0cdaf3a17f740666ce20817
Ghislain Vaillant <ghislain.vaillant@icm-institute.org>
2023-07-10 14:42:00 +0200

    REF: Use pydra-ants in t1-linear pipeline

--- clinica/pydra/t1_linear/t1_linear.py
+++ clinica/pydra/t1_linear/t1_linear.py
@@ -1,33 +1,13 @@
-import typing as ty
 from os import PathLike
 from pathlib import PurePath
+from typing import Any
 
 import pydra
-from nipype.interfaces.ants import N4BiasFieldCorrection, RegistrationSynQuick
 from pydra import Workflow
 from pydra.mark import annotate, task
 
 from clinica.pydra.engine import clinica_io
 from clinica.pydra.tasks import download_mni_template_2009c, download_ref_template
-
-n4_bias_field_correction = N4BiasFieldCorrection(bspline_fitting_distance=300)
-registration_syn_quick = RegistrationSynQuick(transform_type="a")
-
-
-@task
-@annotate({"return": {"cropped_image": PurePath}})
-def crop_image(input_image: PathLike, template_image: PathLike) -> PurePath:
-    from pathlib import Path
-
-    from nilearn.image import resample_to_img
-
-    cropped_image = Path(input_image).name.replace(".nii.gz", "_cropped.nii.gz")
-
-    resample_to_img(
-        source_img=str(input_image), target_img=str(template_image), force_resample=True
-    ).to_filename(cropped_image)
-
-    return cropped_image
 
 
 @clinica_io
@@ -44,52 +24,56 @@ def build_core_workflow(name: str = "core", parameters={}) -> Workflow:
     Workflow
         The core workflow.
     """
-    from pydra.tasks.nipype1.utils import Nipype1Task
+    from pydra.tasks import ants
 
     input_spec = pydra.specs.SpecInfo(
         name="Input",
-        fields=[("T1w", str, {"mandatory": True})],
+        fields=[
+            ("_graph_checksums", Any),
+            ("T1w", str, {"mandatory": True}),
+        ],
         bases=(pydra.specs.BaseSpec,),
     )
 
-    wf = Workflow(name, input_spec=input_spec)
+    wf = Workflow(name=name, input_spec=input_spec)
 
     wf.add(download_mni_template_2009c(name="download_mni_template"))
 
     wf.add(download_ref_template(name="download_ref_template"))
 
     wf.add(
-        Nipype1Task(
+        ants.N4BiasFieldCorrection(
             name="n4_bias_field_correction",
-            interface=n4_bias_field_correction,
+            dimensionality=3,
             input_image=wf.lzin.T1w,
+            spline_distance=600,
         ).split("input_image")
     )
 
     wf.add(
-        Nipype1Task(
+        ants.registration_syn_quick(
             name="registration_syn_quick",
-            interface=registration_syn_quick,
+            dimensionality=3,
+            transform_type="a",
             fixed_image=wf.download_mni_template.lzout.mni_template_file,
             moving_image=wf.n4_bias_field_correction.lzout.output_image,
         )
     )
 
     wf.add(
-        crop_image(
-            name="crop_image",
-            interface=crop_image,
-            input_image=wf.registration_syn_quick.lzout.warped_image,
-            template_image=wf.download_ref_template.lzout.ref_template_file,
+        ants.ApplyTransforms(
+            name="apply_transforms",
+            dimensionality=3,
+            fixed_image=wf.download_ref_template.lzout.ref_template_file,
+            moving_image=wf.registration_syn_quick.lzout.warped_moving_image,
+            output_datatype="short",
         )
     )
 
     wf.set_output(
         [
-            ("t1w_corrected_file", wf.n4_bias_field_correction.lzout.output_image),
-            ("t1w_registered_file", wf.registration_syn_quick.lzout.warped_image),
-            ("t1w_cropped_file", wf.crop_image.lzout.cropped_image),
-            ("xfm_file", wf.registration_syn_quick.lzout.out_matrix),
+            ("t1w_file", wf.apply_transforms.lzout.output_image),
+            ("xfm_file", wf.registration_syn_quick.lzout.affine_transform),
         ]
     )
 
