78bb4220592813dc2c8c560b5ba5050f96266774
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-08-18 17:26:55 +0200

    update existing unit test

--- test/unittests/iotools/converters/aibl_to_bids/test_aibl_utils.py
+++ test/unittests/iotools/converters/aibl_to_bids/test_aibl_utils.py
@@ -1,15 +1,9 @@
-from clinica.iotools.converters.aibl_to_bids import aibl_utils
+def test_listdir_nohidden(tmp_path):
+    from clinica.iotools.converters.aibl_to_bids.aibl_utils import _listdir_nohidden
 
+    (tmp_path / "file").touch()
+    (tmp_path / ".hidden_file").touch()
+    (tmp_path / "dir").mkdir()
+    (tmp_path / ".hidden_dir").mkdir()
 
-def test_listdir_nohidden():
-    from pathlib import Path
-    from tempfile import TemporaryDirectory
-
-    with TemporaryDirectory() as tmpdir:
-        path = Path(tmpdir)
-        (path / "file").touch()
-        (path / ".hidden_file").touch()
-        (path / "dir").mkdir()
-        (path / ".hidden_dir").mkdir()
-        result = aibl_utils.listdir_nohidden(str(path))
-        assert result == ["dir"]
+    assert _listdir_nohidden(tmp_path) == ["dir"]
