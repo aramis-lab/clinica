9f83e21af8ed0a99394873d0f9353231cbc68206
HuguesRoy <hugues.roy@gadz.org>
2024-04-26 10:51:13 +0200

    Linter word-list exception update

--- clinica/iotools/converters/adni_to_bids/adni_modalities/adni_fdg_pet.py
+++ clinica/iotools/converters/adni_to_bids/adni_modalities/adni_fdg_pet.py
@@ -18,7 +18,7 @@ class ADNIPreprocessingStep(Enum):
     STEP1 = "Co-registered Dynamic"
     STEP2 = "Co-registered, Averaged"
     STEP3 = "Coreg, Avg, Standardized Image and Voxel Size"
-    STEP4_8MM = "Coreg, Avg, Std Img and Vox Siz, Uniform Resolution"   # noqa
+    STEP4_8MM = "Coreg, Avg, Std Img and Vox Siz, Uniform Resolution"  # noqa
     STEP4_6MM = "Coreg, Avg, Std Img and Vox Siz, Uniform 6mm Res"  # noqa
 
     @classmethod
