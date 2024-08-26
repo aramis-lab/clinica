b494b68efce4c5411b34436f71d9e8b5b6e9f7bc
NicolasGensollen <nicolas.gensollen@gmail.com>
2024-08-26 09:31:44 +0200

    add unit tests

--- test/unittests/utils/test_check_dependency.py
+++ test/unittests/utils/test_check_dependency.py
@@ -227,28 +227,124 @@ def test_check_spm_alone(tmp_path, mocker):
 
 
 @pytest.mark.parametrize(
-    "version,expected",
+    "software,expected",
     [
-        ("2024a", Version("24.1")),
-        ("2023b", Version("23.2")),
-        ("2023a", Version("9.14")),
-        ("2022b", Version("9.13")),
-        ("2022a", Version("9.12")),
-        ("2021b", Version("9.11")),
-        ("2021a", Version("9.10")),
-        ("2020b", Version("9.9")),
-        ("2020a", Version("9.8")),
-        ("2019b", Version("9.7")),
-        ("2019a", Version("9.6")),
-        ("2018b", Version("9.5")),
-        ("2018a", Version("9.4")),
-        ("2017b", Version("9.3")),
-        ("2017a", Version("9.2")),
-        ("2016b", Version("9.1")),
-        ("2016a", Version("9.0.1")),
+        (ThirdPartySoftware.FREESURFER, Version("6.0.0")),
+        (ThirdPartySoftware.FSL, Version("5.0.5")),
+        (ThirdPartySoftware.ANTS, Version("2.5.0")),
+        (ThirdPartySoftware.DCM2NIIX, Version("1.0.20240202")),
+        (ThirdPartySoftware.MRTRIX, Version("3.0.3")),
+        (ThirdPartySoftware.CONVERT3D, Version("1.0.0")),
+        (ThirdPartySoftware.MATLAB, Version("9.2.0.556344")),
+        (ThirdPartySoftware.SPM, Version("12.7219")),
+        (ThirdPartySoftware.MCR, Version("9.0.1")),
+        (ThirdPartySoftware.SPMSTANDALONE, Version("12.7219")),
+        (ThirdPartySoftware.PETPVC, Version("0.0.0")),
     ],
 )
+def test_get_software_min_version_supported(software: str, expected: Version):
+    from clinica.utils.check_dependency import get_software_min_version_supported
+
+    assert get_software_min_version_supported(software) == expected
+
+
+def test_get_freesurfer_version(mocker):
+    from clinica.utils.check_dependency import get_software_version
+
+    mocker.patch("nipype.interfaces.freesurfer.Info.looseversion", return_value="1.2.3")
+
+    assert get_software_version("freesurfer") == Version("1.2.3")
+
+
+def test_get_spm_version():
+    from clinica.utils.check_dependency import get_software_version
+
+    class SPMVersionMock:
+        version: str = "12.6789"
+
+    with mock.patch(
+        "nipype.interfaces.spm.SPMCommand", wraps=SPMVersionMock
+    ) as spm_mock:
+        assert get_software_version("spm") == Version("12.6789")
+        spm_mock.assert_called_once()
+
+
+def test_get_spm_standalone_version(tmp_path):
+    from clinica.utils.check_dependency import get_software_version
+
+    class SPMStandaloneVersionMock:
+        version: str = "13.234"
+
+        def set_mlab_paths(self, matlab_cmd: str, use_mcr: bool):
+            pass
+
+    with mock.patch.dict(
+        os.environ,
+        {
+            "SPM_HOME": str(tmp_path / "spm_home_mock"),
+            "MCR_HOME": str(tmp_path / "mcr_home_mock"),
+        },
+    ):
+        with mock.patch(
+            "nipype.interfaces.spm.SPMCommand", wraps=SPMStandaloneVersionMock
+        ) as spm_mock:
+            with mock.patch.object(
+                SPMStandaloneVersionMock, "set_mlab_paths", return_value=None
+            ) as mock_method:
+                assert get_software_version("spm standalone") == Version("13.234")
+                spm_mock.set_mlab_paths.assert_called()
+                mock_method.assert_called_once_with(
+                    matlab_cmd=f"{tmp_path / 'spm_home_mock' / 'run_spm12.sh'} {tmp_path / 'mcr_home_mock'} script",
+                    use_mcr=True,
+                )
+
+
+def test_get_fsl_version(mocker):
+    from clinica.utils.check_dependency import get_software_version
+
+    mocker.patch("nipype.interfaces.fsl.Info.version", return_value="3.2.1:9e026117")
+
+    assert get_software_version("fsl") == Version("3.2.1")
+
+
+mcr_version_test_suite = [
+    ("2024a", Version("24.1")),
+    ("2023b", Version("23.2")),
+    ("2023a", Version("9.14")),
+    ("2022b", Version("9.13")),
+    ("2022a", Version("9.12")),
+    ("2021b", Version("9.11")),
+    ("2021a", Version("9.10")),
+    ("2020b", Version("9.9")),
+    ("2020a", Version("9.8")),
+    ("2019b", Version("9.7")),
+    ("2019a", Version("9.6")),
+    ("2018b", Version("9.5")),
+    ("2018a", Version("9.4")),
+    ("2017b", Version("9.3")),
+    ("2017a", Version("9.2")),
+    ("2016b", Version("9.1")),
+    ("2016a", Version("9.0.1")),
+    ("2015b", Version("0.0.0")),
+    ("1789a", Version("0.0.0")),
+]
+
+
+@pytest.mark.parametrize("version,expected", mcr_version_test_suite)
 def test_map_mcr_release_to_version_number(version, expected):
     from clinica.utils.check_dependency import _map_mcr_release_to_version_number
 
     assert _map_mcr_release_to_version_number(version) == expected
+
+
+@pytest.mark.parametrize("version,expected", mcr_version_test_suite)
+def test_get_mcr_version(tmp_path, version, expected):
+    from clinica.utils.check_dependency import get_software_version
+
+    with mock.patch.dict(
+        os.environ,
+        {
+            "MCR_HOME": str(tmp_path / "mcr_home_mock" / version / "v95"),
+        },
+    ):
+        assert get_software_version("MCR") == expected
