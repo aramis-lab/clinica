962d994f867d7323d192a5a2dd7fc6d3b2dbe2a9
Gensollen <nicolas.gensollen@gmail.com>
2024-08-28 16:27:31 +0200

    [ENH] Implement `LoggingLevel` as an `IntEnum` (#1267)
    * implement LoggingLevel as an IntEnum
    
    * add unit tests

--- clinica/utils/stream.py
+++ clinica/utils/stream.py
@@ -1,41 +1,74 @@
 """This module handles stream and log redirection."""
 import warnings
-from enum import Enum
-from typing import Type
+from enum import IntEnum
+from typing import Optional, Type, Union
+
+__all__ = [
+    "LoggingLevel",
+    "get_logging_level",
+    "cprint",
+    "log_and_raise",
+    "log_and_warn",
+]
 
 
-class LoggingLevel(str, Enum):
-    debug = "debug"
-    info = "info"
-    warning = "warning"
-    error = "error"
-    critical = "critical"
+class LoggingLevel(IntEnum):
+    DEBUG = 1
+    INFO = 2
+    WARNING = 3
+    ERROR = 4
+    CRITICAL = 5
 
 
-def cprint(msg: str, lvl: str = "info") -> None:
-    """
-    Print message to the console at the desired logging level.
+def get_logging_level(level: Union[str, int, LoggingLevel]) -> LoggingLevel:
+    if isinstance(level, str):
+        if level == "debug":
+            return LoggingLevel.DEBUG
+        if level == "info":
+            return LoggingLevel.INFO
+        if level == "warning":
+            return LoggingLevel.WARNING
+        if level == "error":
+            return LoggingLevel.ERROR
+        if level == "critical":
+            return LoggingLevel.CRITICAL
+        raise ValueError(f"Logging level {level} is not valid.")
+    return LoggingLevel(level)
 
-    Args:
-        msg (str): Message to print.
-        lvl (str): Logging level between "debug", "info", "warning", "error" and "critical".
-                   The default value is "info".
+
+def cprint(msg: str, lvl: Optional[Union[str, int, LoggingLevel]] = None) -> None:
+    """Print message to the console at the desired logging level.
+
+    Parameters
+    ----------
+    msg : str
+        The message to log.
+
+    lvl : str or int, or LoggingLevel, optional
+        The logging level:
+            - 1 = "debug"
+            - 2 = "info"
+            - 3 = "warning"
+            - 4 = "error"
+            - 5 = "critical"
+        The default value is "info".
     """
     from logging import getLogger
 
+    lvl = lvl or LoggingLevel.INFO
     # Use the package level logger.
     logger = getLogger("clinica")
 
     # Log message as info level.
-    if lvl == LoggingLevel.debug:
+    if lvl == LoggingLevel.DEBUG:
         logger.debug(msg=msg)
-    elif lvl == LoggingLevel.info:
+    elif lvl == LoggingLevel.INFO:
         logger.info(msg=msg)
-    elif lvl == LoggingLevel.warning:
+    elif lvl == LoggingLevel.WARNING:
         logger.warning(msg=msg)
-    elif lvl == LoggingLevel.error:
+    elif lvl == LoggingLevel.ERROR:
         logger.error(msg=msg)
-    elif lvl == LoggingLevel.critical:
+    elif lvl == LoggingLevel.CRITICAL:
         logger.critical(msg=msg)
     else:
         pass
@@ -52,7 +85,7 @@ def log_and_raise(message: str, error_type: Type[Exception]):
     error_type : Exception
         The error type to raise.
     """
-    cprint(message, lvl="error")
+    cprint(message, lvl=LoggingLevel.ERROR)
     raise error_type(message)
 
 
@@ -67,5 +100,5 @@ def log_and_warn(message: str, warning_type: Type[Warning]):
     warning_type : Warning
         The warning type to use.
     """
-    cprint(message, lvl="warning")
+    cprint(message, lvl=LoggingLevel.WARNING)
     warnings.warn(message, warning_type)
