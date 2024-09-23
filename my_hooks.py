19153c1f998998de8cdb74456252d0417dd552bc
AliceJoubert <alice00joubert@gmail.com>
2024-09-23 15:01:26 +0200

    Test hooks on startup

--- my_hooks.py
+++ my_hooks.py
@@ -1,2 +1,12 @@
-def on_page_markdown(markdown, *, page, config, files):
-    return markdown.replace("__DOCS_DIR__", config.docs_dir)
+def on_startup(markdown, *, page, config, files):
+    return markdown.replace("__DOCS_DIR__", "startup")
+
+
+# def on_serve(markdown, *, page, config, files):
+#     return markdown.replace("__DOCS_DIR__", "serve")
+#
+# def on_page_markdown(markdown, *, page, config, files):
+#     return markdown.replace("__DOCS_DIR__", "page")
+#
+# def on_pre_build(markdown, *, page, config, files):
+#     return markdown.replace("__DOCS_DIR__", "pre_build")
