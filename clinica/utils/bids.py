b060eead893907828265c93f4db045ce60e92f00
NicolasGensollen <nicolas.gensollen@gmail.com>
2024-11-29 08:58:22 +0100

    Implement hashing and ordering for Visit class

--- clinica/utils/bids.py
+++ clinica/utils/bids.py
@@ -18,10 +18,20 @@ __all__ = [
 BIDS_VERSION = Version("1.7.0")
 
 
-@dataclass
+@dataclass(frozen=True)
 class Visit:
     subject: str
     session: str
+
+    def __lt__(self, obj):
+        return (self.subject < obj.subject) or (
+            self.subject == obj.subject and self.session < obj.session
+        )
+
+    def __gt__(self, obj):
+        return (self.subject > obj.subject) or (
+            self.subject == obj.subject and self.session > obj.session
+        )
 
     def __str__(self) -> str:
         return f"{self.subject} {self.session}"
