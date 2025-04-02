94698cb1545b58f13e49fab85217b7c5f8cb7e83
NicolasGensollen <nicolas.gensollen@gmail.com>
2025-04-02 11:48:42 +0200

    Make tests more complete

--- test/unittests/iotools/test_iotools_cli.py
+++ test/unittests/iotools/test_iotools_cli.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+from typing import Optional
 from unittest.mock import patch
 
+import pytest
 from click.testing import CliRunner
 
 from clinica.iotools.cli import check_missing_modalities, check_missing_processing
@@ -26,20 +29,55 @@ def test_check_missing_processing(mock_compute_missing_processing, tmp_path):
     )
 
 
-@patch("clinica.iotools.data_handling.compute_missing_mods")
-def test_check_missing_modalities(mock_compute_missing_mods, tmp_path):
-    (tmp_path / "bids").mkdir()
-    runner = CliRunner()
-    result = runner.invoke(
-        check_missing_modalities,
-        [
-            str(tmp_path / "bids"),
-            str(tmp_path / "output"),
-        ],
-    )
-    assert result.exit_code == 0
-    mock_compute_missing_mods.assert_called_once_with(
+@pytest.fixture
+def input_argument_list(
+    tmp_path: Path, option_name: Optional[str], option_value: Optional[str]
+) -> list:
+    input_argument_list = [
         str(tmp_path / "bids"),
         str(tmp_path / "output"),
-        "missing_mods",
+    ]
+    if option_name:
+        input_argument_list.extend([option_name, option_value])
+    return input_argument_list
+
+
+@pytest.fixture
+def expected_arguments_passed_to_mock(
+    tmp_path: Path, option_name: Optional[str], option_value: Optional[str]
+) -> list:
+    expected_arguments_passed_to_mock = [
+        str(tmp_path / "bids"),
+        str(tmp_path / "output"),
+    ]
+    if option_name:
+        expected_arguments_passed_to_mock.append(option_value)
+    else:
+        expected_arguments_passed_to_mock.append("missing_mods")
+    return expected_arguments_passed_to_mock
+
+
+@pytest.mark.parametrize(
+    "option_name,option_value",
+    [
+        (None, None),
+        ("--output_prefix", "foobar"),
+        ("--output_prefix", ""),
+    ],
+)
+@patch("clinica.iotools.data_handling.compute_missing_mods")
+def test_check_missing_modalities(
+    mock_compute_missing_mods,
+    tmp_path,
+    option_name,
+    option_value,
+    input_argument_list,
+    expected_arguments_passed_to_mock,
+):
+    (tmp_path / "bids").mkdir()
+    runner = CliRunner()
+    result = runner.invoke(check_missing_modalities, input_argument_list)
+    assert result.exit_code == 0
+    mock_compute_missing_mods.assert_called_once_with(
+        *expected_arguments_passed_to_mock
     )
