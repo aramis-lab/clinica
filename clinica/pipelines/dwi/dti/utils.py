c21858dc774c601639a39c52b01a333812e6527d
AliceJoubert <158147135+AliceJoubert@users.noreply.github.com>
2025-06-11 11:50:02 +0200

    [FIX] DWI-DTI : fix import in utils (#1523)

--- clinica/pipelines/dwi/dti/utils.py
+++ clinica/pipelines/dwi/dti/utils.py
@@ -39,7 +39,7 @@ def compute_statistics_on_atlases(
     """
     from pathlib import Path
 
-    from clinica.bids import BIDSFileName
+    from clinica.dataset.bids import BIDSFileName
     from clinica.utils.atlas import atlas_factory
     from clinica.utils.statistics import statistics_on_atlas
 
