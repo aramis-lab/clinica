51c46d7b407541c873ac7c1b15b5ce8b9747f781
AliceJoubert <alice00joubert@gmail.com>
2025-12-30 15:13:39 +0100

    Modify m file

--- clinica/pipelines/pet_surface/applyInverseDeformationField.m
+++ clinica/pipelines/pet_surface/applyInverseDeformationField.m
@@ -7,12 +7,12 @@ function applyInverseDeformationField(target, deformation_field, img, output_fol
     fprintf('SPM version: %s Release: %s\n',name, version);
     fprintf('SPM path: %s\n', which('spm'));
     spm('Defaults','fMRI');
-    
-    if strcmp(name, 'SPM8') || strcmp(name(1:5), 'SPM12')
-        spm_jobman('initcfg');
-        spm_get_defaults('cmdline', 1);
+
+    spm_jobman('initcfg');
+    spm_get_defaults('cmdline', 1);
+
     end
-    
+
 jobs{1}.spm.util.defs.comp{1}.inv.comp{1}.def = {deformation_field};
 jobs{1}.spm.util.defs.comp{1}.inv.space = {target};
 jobs{1}.spm.util.defs.out{1}.pull.fnames = {img};
@@ -21,6 +21,6 @@ jobs{1}.spm.util.defs.out{1}.pull.interp = 4;
 jobs{1}.spm.util.defs.out{1}.pull.mask = 1;
 jobs{1}.spm.util.defs.out{1}.pull.fwhm = [0 0 0];
 jobs{1}.spm.util.defs.out{1}.pull.prefix = output_prefix;
-    
+
             spm_jobman('run', jobs);
 end
