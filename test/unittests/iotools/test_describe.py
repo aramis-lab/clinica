2eedb5de699d5334b80939f207eb2c76a8894230
AliceJoubert <alice00joubert@gmail.com>
2026-01-28 17:44:42 +0100

    Update test_describe

--- test/unittests/iotools/test_describe.py
+++ test/unittests/iotools/test_describe.py
@@ -137,6 +137,6 @@ def test_build_processing_table_caps(tmp_path):
     for i, value in enumerate(("ants", ">=2.2.0", "2.3.5")):
         assert dependency_table_t1_linear.columns[i]._cells[0] == value
     for i, value in enumerate(
-        (["ants", "spm"], [">=2.3.0", ">=12"], ["2.5.1", "12.2.1"])
+        (["ants", "spm standalone"], [">=2.3.0", ">=25"], ["2.5.1", "25.2.1"])
     ):
         assert dependency_table_pet_linear.columns[i]._cells == value
