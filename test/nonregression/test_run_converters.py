2e913f4860180355c15cc002562c85a16f83a70d
ISMAILI Adam <Adam ISMAILI>
2026-03-04 16:33:05 +0100

    new nonregression tests added for genfi

--- test/nonregression/test_run_converters.py
+++ test/nonregression/test_run_converters.py
@@ -45,3 +45,44 @@ def test_converters(cmdopt, tmp_path, study: StudyName):
         StudyName.UKB,
     ):
         compare_bids_tsv(output_dir, ref_dir / "bids")
+
+
+@pytest.mark.parametrize(
+    "gif,full",
+    [
+        (False, False),
+        (True, False),
+        (False, True),
+        (True, True),
+    ],
+)
+def test_genfi_converter_using_options(cmdopt, tmp_path, gif, full):
+    from clinica.converters.factory import convert, get_converter_name
+
+    base_dir = Path(cmdopt["input"])
+    input_dir, tmp_dir, ref_dir = configure_paths(
+        base_dir, tmp_path, get_converter_name(StudyName.GENFI)
+    )
+    output_dir = tmp_path / "bids"
+
+    kwargs = dict(
+        path_to_dataset=input_dir / "unorganized",
+        bids_dir=output_dir,
+        path_to_clinical=input_dir / "clinical_data",
+        subjects=input_dir / "subjects.txt",
+        path_to_clinical_txt=input_dir / "additional_clinical_data.txt",
+        gif=gif,
+        full=full,
+    )
+
+    convert(StudyName.GENFI, **kwargs)
+
+    if full:
+        compare_folders(output_dir, ref_dir / "bids_with_options/bids_full", output_dir)
+        compare_bids_tsv(output_dir, ref_dir / "bids_with_options/bids_full")
+    elif gif:
+        compare_folders(output_dir, ref_dir / "bids_with_options/bids_gif", output_dir)
+        compare_bids_tsv(output_dir, ref_dir / "bids_with_options/bids_gif")
+    else:
+        compare_folders(output_dir, ref_dir / "bids_with_options/bids_cdt", output_dir)
+        compare_bids_tsv(output_dir, ref_dir / "bids_with_options/bids_cdt")
